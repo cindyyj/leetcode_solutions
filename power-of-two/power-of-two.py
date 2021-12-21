1e2387b50deedd6e9da1d4036266522f9edca845
Jing <jyang.ustc@gmail.com>
2021-12-21 10:49:06 -0600

    Time: 24 ms (96.97%), Space: 14.2 MB (69.98%) - LeetHub

--- power-of-two/power-of-two.py
+++ power-of-two/power-of-two.py
@@ -1,14 +1,19 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
-        if n <= 0:
+        # if n <= 0:
+        #     return False
+        # elif n == 1:
+        #     return True
+        # else:
+        #     while n / 2 >= 1:
+        #         n = n/2
+        #         if n == 1: 
+        #             return True
+        #     return False
+
+        # math Logaritmic solution
+        if n < 1:
             return False
-        elif n == 1:
-            return True
-        else:
-            while n / 2 >= 1:
-                n = n/2
-                if n == 1: 
-                    return True
-            return False
-        
+        import math
+        return True if math.log2(n) % 1 == 0 else False        
