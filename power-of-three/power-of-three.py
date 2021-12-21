6dc68217b050b45b27f4242c5ffc951680300ffd
Jing <jyang.ustc@gmail.com>
2021-12-21 13:41:54 -0600

    Time: 80 ms (61.82%), Space: 14.4 MB (14.79%) - LeetHub

--- power-of-three/power-of-three.py
+++ power-of-three/power-of-three.py
@@ -1,10 +1,12 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        
-        while n % 3 == 0:
+class Solution:    
+#     BIG = 3**19
+    
+#     def isPowerOfThree(self, n: int) -> bool:    
+#         # method 2: divisor of 3 power 19
+#         return (n > 0) and (Solution.BIG % n == 0)        
+
+    def isPowerOfThree(self, n: int) -> bool:   
+        while n and (n % 3 == 0):
             n /= 3
         
-        return n == 1
-        
+        return n == 1         
