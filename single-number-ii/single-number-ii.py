4e616340a54fcf5b6fe8dbf37b5b739045c80a21
Jing <jyang.ustc@gmail.com>
2021-12-27 16:31:34 -0600

    Time: 56 ms (78.56%), Space: 16.2 MB (23.53%) - LeetHub

--- single-number-ii/single-number-ii.py
+++ single-number-ii/single-number-ii.py
@@ -1,6 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        for num in nums:
-            if nums.count(num) == 1:
-                return num      
+        freq = collections.Counter(nums)
+        ans = [num for num, occ in freq.items() if occ == 1][0]
+        return ans
+       
+        # # super slow solution
+        # for num in nums:
+        #     if nums.count(num) == 1:
+        #         return num      
