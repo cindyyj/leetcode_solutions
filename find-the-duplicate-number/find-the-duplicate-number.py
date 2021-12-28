7d60f4d9ee322f23cf83800c392c5bc62802d58a
Jing <jyang.ustc@gmail.com>
2021-12-28 10:42:32 -0600

    Update find-the-duplicate-number.py

--- find-the-duplicate-number/find-the-duplicate-number.py
+++ find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,8 +1,26 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # https://leetcode.com/problems/find-the-duplicate-number/solution/
         
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                return nums[i]
+#         # Math solution: sum of 1 to n, len(nums) is n+1! n = (len(nums)-1)
+#         # O(1); repeated number can appear more than 1 time!!! so this doesn't work!
+#         series_sum = len(nums)*(len(nums)-1)/2
+#         nums_sum = sum(nums)
+        
+#         return int(nums_sum-series_sum)
+        
+        # https://leetcode.com/problems/find-the-duplicate-number/solution/
+        # 7 methods!!!
+        
+        # Time complexity: O(n**2): nested loop
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         return nums[i]
+        
+        # Time complexity: O(n), one loop; space: O(n)
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            else:
+                seen.add(num)
