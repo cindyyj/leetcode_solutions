6f3d43c6ce3eb81905f81f398ac561b4010bcf14
Jing <jyang.ustc@gmail.com>
2022-01-02 10:43:57 -0600

    Update two-sum-ii-input-array-is-sorted.py

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,25 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # hashmap, time O(n), space O(n)
         
-        l = 0
-        r = len(numbers)-1
+        hashdict = dict()
+        for i in range(len(numbers)):
+            complement = target - numbers[i]
+            if complement in hashdict:
+                return [hashdict[complement]+1, i+1]
+            hashdict[numbers[i]] = i
         
-        while l<r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-                break
-            elif numbers[l] + numbers[r] < target:
-                l += 1
-            else:
-                r -= 1        
-        
+        return [-1, -1]
+    
+#         # two pointer, time O(n), space O(1)
+#         l, r = 0, len(numbers)-1
+
+#         while l<r:
+#             s = numbers[l] + numbers[r]
+#             if s == target:
+#                 return [l+1, r+1]
+#             elif s < target:
+#                 l += 1
+#             else:
+#                 r -= 1
+#         return [-1, -1]
