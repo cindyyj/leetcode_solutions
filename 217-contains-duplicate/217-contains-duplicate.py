63a68ca95146314848754ccbdbe28e3c6f8b5b0a
Jing <jyang.ustc@gmail.com>
2022-01-07 15:51:00 -0600

    Time: 436 ms (45.06%), Space: 25.8 MB (41.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,25 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        return len(set(nums)) != len(nums)
         
+        # Method 1 -- Apply hashtable O(n)
+        hashNum = {}
+        for i in nums:
+            if i not in hashNum:
+                hashNum[i] = 1
+            else:
+                return True
+        return False
+
+        # Method 2 -- Sorting
+        # l =  len(nums)
+        # if l < 2:
+        #     return False
+        # nums.sort()
+        # for i in range(l-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        # return False
+
+    #     # Method 3 -- Set solution for python
+    #     return len(set(nums)) != len(nums)
