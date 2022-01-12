cf2d6287a9f06aada75b241bf662ef2bfdb8b89b
Jing <jyang.ustc@gmail.com>
2022-01-11 18:45:31 -0600

    Time: 6492 ms (36.52%), Space: 52.7 MB (87.78%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -1,7 +1,7 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
         
-                ## APPROACH :  Sieve of Eratosthenes ##
+## APPROACH :  Sieve of Eratosthenes ##
         
         #   1. Checking till sqrt(n) is enough for prime numbers i.e i*i < n
         #   2. mark all as prime.
@@ -11,6 +11,15 @@ class Solution:
 		## TIME COMPLEXITY : O(NLogN) ##
 		## SPACE COMPLEXITY : O(N) ##
         # Loop's ending condition is i * i < n instead of i < sqrt(n) to avoid repeatedly calling an expensive function sqrt().
+        # All the for construct does is loop over an iterable. If that iterable is empty, then the body of the for is never executed.
+
+        # All range does is return an interable containing all the values from start to stop (inclusive of start, exclusive of stop). If start is higher than stop, then the iterable is empty because there are no valid values between start and stop:
+
+        # >>> range(0, 2):
+        # [0, 1]
+        # >>> range(2, 0):
+        # []
+        # Putting this together means that you now have a for loop with an empty iterable - so it doesn't get iterated over at all.        
         
         if n <= 2: return 0
         
