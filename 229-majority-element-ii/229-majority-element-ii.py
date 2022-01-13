c83ca18283bfa7884f36785e54d7f27645b35684
Jing <jyang.ustc@gmail.com>
2022-01-13 15:59:29 -0600

    Time: 112 ms (89.63%), Space: 15.5 MB (31.90%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,21 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
-        # Brute Force:
-        majority_count = len(nums) // 3
-        seen = []
+        # HashMap
+        counts = collections.Counter(nums)
         ans = []
-        for num in nums:
-            if num not in seen:
-                count = sum([1 for elem in nums if elem == num])
-                if count > majority_count:
-                    ans.append(num)
-                    seen.append(num)
         
+        for key, value in counts.items():
+            if value > len(nums)/3:
+                ans.append(key)
+                
         return ans
-#         majority_count = len(nums) // 2
         
+#         # Brute Force: Time (O(n**2))
+#         majority_count = len(nums) // 3
+#         seen = []
+#         ans = []
 #         for num in nums:
-#             count = sum([1 for elem in nums if num == elem])
-#             if count > majority_count:
-#                 return num             
+#             if num not in seen:
+#                 count = sum([1 for elem in nums if elem == num])
+#                 if count > majority_count:
+#                     ans.append(num)
+#                     seen.append(num)
+        
+#         return ans
