d65adcf16ae4c10abf930e53622c50dccec2b69d
Jing <jyang.ustc@gmail.com>
2022-01-13 18:48:48 -0600

    Time: 31 ms (91.07%), Space: 14.1 MB (69.88%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # cannot use built-in function
+        # pow(x, 0.5) or x**0.5, math.sqrt(x)
+        
+        # binary search
+        
+        low, high = 0, x
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            if (mid*mid <= x < (mid+1)*(mid+1)):
+                return mid
+            elif mid*mid > x:
+                high = mid -1
+            else:
+                low = mid + 1
+        return low
