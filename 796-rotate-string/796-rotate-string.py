1addad191ad2891868bcf6416ef8ac0304160d2e
Jing <jyang.ustc@gmail.com>
2022-01-14 21:53:59 -0600

    Time: 47 ms (19.07%), Space: 14.1 MB (74.86%) - LeetHub

--- 796-rotate-string/796-rotate-string.py
+++ 796-rotate-string/796-rotate-string.py
@@ -1,16 +1,21 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         
-        # https://leetcode.com/problems/rotate-string/solution/
-        # 4 solutions
+#         # https://leetcode.com/problems/rotate-string/solution/
+#         # 4 solutions
         
-        # Brute Force 
-        if len(s) != len(goal):
-            return False
-        if len(s) == 0:
-            return True
+#         # Brute Force, Time O(N**2), Space: O(1)
+#         if len(s) != len(goal):
+#             return False
+#         if len(s) == 0:
+#             return True
         
-        for i in range(len(s)):
-            if all( s[(x + i) % len(s)] == goal[x] for x in range(len(goal))):
-                return True
+#         for i in range(len(s)):
+#             if all( s[(x + i) % len(s)] == goal[x] for x in range(len(goal))):
+#                 return True
+        
+        # Simple check
+        # All rotations of A are contained in A+A. Thus, we can simply check whether B is a substring of A+A
+        
+        return len(s) == len(goal) and goal in s + s
         
