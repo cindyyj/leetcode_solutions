84b6c1f84501994f04e684e3892800044650e3d5
Jing <jyang.ustc@gmail.com>
2022-01-14 21:55:15 -0600

    Time: 38 ms (31.77%), Space: 14.2 MB (74.86%) - LeetHub

--- 796-rotate-string/796-rotate-string.py
+++ 796-rotate-string/796-rotate-string.py
@@ -4,7 +4,7 @@ class Solution:
 #         # https://leetcode.com/problems/rotate-string/solution/
 #         # 4 solutions
         
-#         # Brute Force, Time O(N**2), Space: O(1)
+#         # 1. Brute Force, Time O(N**2), Space: O(1)
 #         if len(s) != len(goal):
 #             return False
 #         if len(s) == 0:
@@ -14,8 +14,10 @@ class Solution:
 #             if all( s[(x + i) % len(s)] == goal[x] for x in range(len(goal))):
 #                 return True
         
-        # Simple check
-        # All rotations of A are contained in A+A. Thus, we can simply check whether B is a substring of A+A
-        
+        # 2. Simple check
+        # All rotations of A are contained in A+A. Thus, we can simply check whether B is a substring of A+A      
         return len(s) == len(goal) and goal in s + s
         
+        # 3. rolling hash 
+        # 4. KMP to read 
+        # KMP use a dp table to calculate shift
