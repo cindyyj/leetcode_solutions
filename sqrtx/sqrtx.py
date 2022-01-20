451694346e1c2c9314b914890dc543eb0ba2f883
Jing <jyang.ustc@gmail.com>
2022-01-20 13:08:52 -0600

    Time: 62 ms, Memory: 14.1 MB - LeetHub

---
+++ sqrtx/sqrtx.py
@@ -0,0 +1,53 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        low, high = 0, x
+        
+        while low <= high:
+            mid = low + (high- low) // 2
+            if mid*mid <= x < (mid+1) * (mid+1):
+                return mid
+            elif mid*mid > x:
+                high = mid - 1 
+            else:
+                low = mid + 1
+        
+        return low
+        
+        
+#         if x == 0:
+#             return x
+        
+#         low, high = 0, x
+#         while low <= high:
+#             mid = low + (high - low) // 2
+#             if mid * mid <= x < (mid+1) * (mid+1):
+#                 return mid
+#             elif x >= (mid+1) * (mid+1):
+#                 mid = high
+#             else: 
+#                 mid = low
+#         return low
+
+#         # newton's method
+#         r = x
+#         while r * r > x:
+#             r = (r + x/r) // 2
+#         return int(r)
+        
+# #         # cannot use built-in function
+# #         # pow(x, 0.5) or x**0.5, math.sqrt(x)
+        
+# #         # binary search
+        
+# #         low, high = 0, x
+        
+# #         while low <= high:
+# #             mid = low + (high - low) // 2
+# #             if (mid*mid <= x < (mid+1)*(mid+1)):
+# #                 return mid
+# #             elif mid*mid > x:
+# #                 high = mid -1
+# #             else:
+# #                 low = mid + 1
+# #         return low
