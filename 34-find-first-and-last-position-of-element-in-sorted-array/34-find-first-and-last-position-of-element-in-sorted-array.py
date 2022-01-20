3e4e219228b5355d6770cff9f4e056021d63a71f
Jing <jyang.ustc@gmail.com>
2022-01-19 21:59:20 -0600

    Time: 84 ms (81.37%), Space: 15.5 MB (13.19%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,57 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-
-        # https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-w1h4/
-        # empty nums
-        if not nums:
-            return [-1, -1]
         
-        # boundry cases
-        if target < nums[0] or target > nums[-1]:
-            return [-1, -1]
+        start, end = self.left_bound(nums, target), self.right_bound(nums, target)
+        return [start, end] if start <= end else [-1, -1]
+    
+    # helper function
+    def left_bound(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if nums[mid] < target: 
+                l = mid + 1
+            else: 
+                r = mid - 1
+        return l
+    
+    def right_bound(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if nums[mid] <= target: 
+                l = mid + 1
+            else: 
+                r = mid - 1
+        return r     
+                
+#         # https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-w1h4/
+#         # empty nums
+#         if not nums:
+#             return [-1, -1]
         
-        def left_bound(nums, target):
-            l, r = 0, len(nums) - 1
+#         # boundry cases
+#         if target < nums[0] or target > nums[-1]:
+#             return [-1, -1]
+        
+#         def left_bound(nums, target):
+#             l, r = 0, len(nums) - 1
             
-            while l <= r:
-                mid = l + (r-l) // 2                
-                if nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+#             while l <= r:
+#                 mid = l + (r-l) // 2                
+#                 if nums[mid] < target:
+#                     l = mid + 1
+#                 else:
+#                     r = mid - 1
             
-            return l
+#             return l
         
-        l = left_bound(nums, target)
-        if l >= len(nums) or nums[l] != target:
-            return [-1, -1]
+#         l = left_bound(nums, target)
+#         if l >= len(nums) or nums[l] != target:
+#             return [-1, -1]
         
-        r = left_bound(nums, target + 1)
-        return [l, r-1]
+#         r = left_bound(nums, target + 1)
+#         return [l, r-1]
         
         
 #         # empty nums
