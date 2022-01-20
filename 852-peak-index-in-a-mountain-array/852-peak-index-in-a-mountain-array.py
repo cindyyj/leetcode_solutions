12f904a2a0ebbc41a0057a387c8f7256fb0546fb
Jing <jyang.ustc@gmail.com>
2022-01-20 10:42:40 -0600

    Time: 137 ms (11.18%), Space: 15.4 MB (62.62%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,17 +1,22 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         
-        l, r = 0, len(arr) - 1
-        while l <= r:
-            mid = l + (r-l) // 2
+        # linear scan
+        # https://leetcode.com/problems/peak-index-in-a-mountain-array/discuss/139848/C%2B%2BJavaPython-Better-than-Binary-Search
+        return arr.index(max(arr))
+        
+#         # binary search
+#         l, r = 0, len(arr) - 1
+#         while l <= r:
+#             mid = l + (r-l) // 2
             
-            if arr[mid] > arr[mid+1] and arr[mid-1] < arr[mid]:
-                return mid
+#             if arr[mid] > arr[mid+1] and arr[mid-1] < arr[mid]:
+#                 return mid
 
-            if arr[mid] < arr[mid+1]:
-                l = mid + 1
-            elif arr[mid] > arr[mid+1]:
-                r = mid - 1
+#             if arr[mid] < arr[mid+1]:
+#                 l = mid + 1
+#             elif arr[mid] > arr[mid+1]:
+#                 r = mid - 1
             
             
         
