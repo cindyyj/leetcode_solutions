16c3ade06d670f2951f08eb41985e0b5e7f1fff5
Jing <jyang.ustc@gmail.com>
2022-01-20 09:24:50 -0600

    Time: 80 ms (6.54%), Space: 14.7 MB (29.72%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,42 +1,40 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        # If the list has just one element then return that element.
-        if len(nums) == 1:
+        # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/solution/
+        # binary search
+        # one-element or no rotation (last elem > 1st elem)
+        if len(nums) == 1 or nums[-1] > nums[0]:
             return nums[0]
-
-        # left pointer
-        left = 0
-        # right pointer
-        right = len(nums) - 1
-
-        # if the last element is greater than the first element then there is no rotation.
-        # e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
-        # Hence the smallest element is first element. A[0]
-        if nums[right] > nums[0]:
-            return nums[0]
-
-        # Binary search way
-        while right >= left:
-            # Find the mid element
-            mid = left + (right - left) // 2
+        
+        l, r = 0, len(nums) - 1
+        
+        while r >= l:
+            mid = l + (r - l) // 2
+            
             # if the mid element is greater than its next element then mid+1 element is the smallest
             # This point would be the point of change. From higher to lower value.
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            # if the mid element is lesser than its previous element then mid element is the smallest
-            if nums[mid - 1] > nums[mid]:
-                return nums[mid]
+            # inflection point 
+            
+            # stop our search when we find the inflection point, when either of the two conditions is satisfied:
+            # nums[mid] > nums[mid + 1] Hence, mid+1 is the smallest.
+            # nums[mid - 1] > nums[mid] Hence, mid is the smallest.
 
-            # if the mid elements value is greater than the 0th element this means
-            # the least value is still somewhere to the right as we are still dealing with elements greater than nums[0]
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+            
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+            
+            # If mid element > first element of array this means that we need to look for the inflection point on the right of mid.
+            # If mid element < first element of array this that we need to look for the inflection point on the left of mid.
+            
             if nums[mid] > nums[0]:
-                left = mid + 1
-            # if nums[0] is greater than the mid value then this means the smallest value is somewhere to the left
-            else:
-                right = mid - 1
-                
-        
+                l = mid + 1
+            if nums[mid] < nums[0]:
+                r = mid - 1
+
+
 #         # 1- liner, time: O(N)
 #         # min, max have O(N) time complexity 
 #         # because they have to loop over the given list/string and check every index to find min/max.
