4bb7de29d5ea003cdb87392e44fdc21a902bbddb
Jing <jyang.ustc@gmail.com>
2022-01-21 10:36:09 -0600

    Time: 65 ms (7.05%), Space: 14.3 MB (14.88%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -7,28 +7,52 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-# --------------------------------------------        
-        # Stack (with dummy node)
+        # two pointers, fast and slow pointers, still requires dummy
         
         dummy = ListNode(0, head)
-        stack = list()
         
-        cur = dummy
-        # push all nodes in stack
-        while cur:            
-            stack.append(cur)
-            cur = cur.next
-            
-        # pop n elements
+        slow = fast = dummy
+        
         for i in range(n):
-            stack.pop()
+            fast = fast.next
         
-        stack[-1].next = stack[-1].next.next
+        # at last node 
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        
+        slow.next = slow.next.next
         
         return dummy.next
+        
+        
+        
+# # --------------------------------------------        
+#         # Stack (with dummy node)
+# # 时间复杂度：O(L)，其中 L 是链表的长度。
+# # 空间复杂度：O(L)，其中 L 是链表的长度。主要为栈的开销。
+        
+#         dummy = ListNode(0, head)
+#         stack = list()
+        
+#         cur = dummy
+#         # push all nodes in stack
+#         while cur:            
+#             stack.append(cur)
+#             cur = cur.next
+            
+#         # pop n elements
+#         for i in range(n):
+#             stack.pop()
+        
+#         stack[-1].next = stack[-1].next.next
+        
+#         return dummy.next
 
-# --------------------------------------------
+# # --------------------------------------------
 #     # 计算链表长度, 当遍历到第 L-n 个节点时, 它就是我们需要删除的节点。
+# # 时间复杂度：O(L)，其中 L 是链表的长度。
+# # 空间复杂度：O(1)。
 #     def get_length(self, head):
 #         length = 0
 #         while head:
