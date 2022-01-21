6ffb348442e24f7132adf25dedaa8a361279ae06
Jing <jyang.ustc@gmail.com>
2022-01-20 18:16:42 -0600

    Time: 208 ms (8.48%), Space: 14.8 MB (30.66%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,8 +1,24 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-
-        for c in letters:
-            if c > target:
-                return c
-        return letters[0]
+        
+        # binary search, right boundary
+        
+        l, r = 0, len(letters)-1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if letters[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        if r < 0 or l >= len(letters):
+            return letters[0]
+        else:
+            return letters[l]
+            
+        # # https://leetcode.com/problems/find-smallest-letter-greater-than-target/solution/
+        # for c in letters:
+        #     if c > target:
+        #         return c
+        # return letters[0]
         
