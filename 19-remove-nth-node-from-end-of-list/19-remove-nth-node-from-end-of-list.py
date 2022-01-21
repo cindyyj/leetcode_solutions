da5d2a27488f2cd6e07f83e054f59cf310ca6607
Jing <jyang.ustc@gmail.com>
2022-01-21 10:20:50 -0600

    Time: 32 ms (84.39%), Space: 14.3 MB (49.49%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,25 +5,42 @@
 #         self.next = next
 
 class Solution:
-    
-    # 计算链表长度, 当遍历到第 L-n 个节点时, 它就是我们需要删除的节点。
-    def get_length(self, head):
-        length = 0
-        while head:
-            length += 1
-            head = head.next
-        return length
-        
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Stack (with dummy node)
         
-        # in case deleted head node ! use a dummy node
         dummy = ListNode(0, head)
+        stack = list()
+        
         cur = dummy
-        
-        for i in range(self.get_length(head) - n):
+        # push all nodes in stack
+        while cur:
+            stack.append(cur)
             cur = cur.next
+            
+        # pop n elements
+        for i in range(n):
+            stack.pop()
         
-        cur.next = cur.next.next
+        stack[-1].next = stack[-1].next.next
         
         return dummy.next
+    
+#     # 计算链表长度, 当遍历到第 L-n 个节点时, 它就是我们需要删除的节点。
+#     def get_length(self, head):
+#         length = 0
+#         while head:
+#             length += 1
+#             head = head.next
+#         return length
+        
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+#         # in case deleted head node ! use a dummy node
+#         dummy = ListNode(0, head)
+#         cur = dummy        
+#         for i in range(self.get_length(head) - n):
+#             cur = cur.next        
+#         cur.next = cur.next.next
+        
+#         return dummy.next
             
