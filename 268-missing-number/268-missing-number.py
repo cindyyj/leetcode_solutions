b317e5cc8a5223e8464b52a75f65895c0d30e2d7
Jing <jyang.ustc@gmail.com>
2022-01-22 14:14:51 -0600

    Time: 4744 ms (5.01%), Space: 15.3 MB (80.25%) - LeetHub

---
+++ 268-missing-number/268-missing-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        # hashset
+        # time, O(n), traverse the list. Space: O(1)
+        
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i 
+        
+#         # expected sum (n+1)*n/2 - actual sum
+#         # 时间复杂度：O(n)，
+#         # The time complexity of len() is O(1) , sum(list) O(n)
+#         # The time complexity of Python sum() depends on your data structure. 
+#         # For a flat list, dict you cannot do better than O(n) because you have to look at each item in the list to add them up.
+#         # space: O(1)
+
+#         return int((len(nums) + 1) * len(nums) / 2 - sum(nums))
