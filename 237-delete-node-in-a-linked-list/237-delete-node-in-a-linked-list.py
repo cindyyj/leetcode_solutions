18941fe2a29eb61da829a1ecfdeea941df0d8d47
Jing <jyang.ustc@gmail.com>
2022-01-22 17:18:50 -0600

    Time: 40 ms (74.29%), Space: 14.9 MB (31.33%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.py
@@ -9,34 +9,7 @@ class Solution:
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
-                
-        goal : delete node 2.
-
-        1  ->  2 -> 3       -> 4
-           ^    ^
-           |    |
-          node  node.next
-        #step one:  change the node value to 3
-        1  ->  3   3           4
-           ^    ^           ^
-           |    |           |
-          node  node.next   node.next.next
-
-        #step two: change the next pointer to point to node.next.next
-        1  ->  3   ->          4
-           ^    ^           ^
-           |    |           |
-          node  node.next   node.next.next
-
-        1 ->3 ->4
-        
-        Complexity Analysis
-
-Time complexity: O(1)
-The algorithm only needs two assignment operations.
-Space complexity: O(1)
-The algorithm doesn't need extra memory.
         """
-        
+        # Copy next node value into current node value and then delete next node .
         node.val = node.next.val
         node.next = node.next.next
