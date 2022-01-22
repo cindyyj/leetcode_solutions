e30de365c2e8d014ecee2a7f1a120c503918baa3
Jing <jyang.ustc@gmail.com>
2022-01-22 17:28:17 -0600

    Time: 3309 ms (5.10%), Space: 60.9 MB (76.33%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -6,30 +6,20 @@
 
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        newList = ListNode()
-        newList.next = head 
-        slow = newList
-        fast = head
+
+        # 基本思路设计两个指针，fast一次走两步，slow一次走一步。当fast或fast->next为空时，slow必定指向中间节点
+        
+        if not head or not head.next:
+            return None
+        
+        dummy, dummy.next = ListNode(0), head
+        pre, slow, fast = dummy, head, head
         
         while fast and fast.next:
+            pre = pre.next
             slow = slow.next
             fast = fast.next.next
-        slow.next = slow.next.next
-        return newList.next
         
-#         # 基本思路设计两个指针，fast一次走两步，slow一次走一步。当fast或fast->next为空时，slow必定指向中间节点
+        pre.next = slow.next
         
-#         if not head or not head.next:
-#             return None
-        
-#         dummy, dummy.next = ListNode(0), head
-#         pre, slow, fast = dummy, head, head
-        
-#         while fast and fast.next:
-#             pre = pre.next
-#             slow = slow.next
-#             fast = fast.next.next
-        
-#         pre.next = slow.next
-        
-#         return head        
+        return head        
