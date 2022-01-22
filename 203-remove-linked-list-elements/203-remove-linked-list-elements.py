9b1144223497e1104bfae821ac9ff7ce00744c2d
Jing <jyang.ustc@gmail.com>
2022-01-22 17:13:17 -0600

    Time: 118 ms (12.02%), Space: 17.2 MB (69.33%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -6,12 +6,17 @@
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
+        # Iterative
+        # Time, O(n); S: O(1)
+        # （迭代-单独处理头节点）
+
+        while head and head.val == val:
+            head = head.next
+            
         if not head:
             return head
-        
-        dummy, dummy.next = ListNode(0), head
-        
-        pre, cur = dummy, head
+                
+        pre, cur = head, head.next
         while cur:
             if cur.val == val:
                 pre.next = cur.next
@@ -19,4 +24,4 @@ class Solution:
                 pre = cur                
             cur = cur.next
             
-        return dummy.next
+        return head
