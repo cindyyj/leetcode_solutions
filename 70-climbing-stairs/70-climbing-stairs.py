dac45f2a462bf3aacdb95264b552524c96824a1f
Jing <jyang.ustc@gmail.com>
2022-01-22 15:30:42 -0600

    Time: 32 ms (67.60%), Space: 14.3 MB (44.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -3,8 +3,18 @@ class Solution:
     @lru_cache()  # this decorator can speed up the program by 30%
     def climbStairs(self, n: int) -> int:
         
-        if n <= 2:
-            return n
+        dp = [0] * (n + 1)
         
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        for i in range(n+1):
+            if i <= 2:
+                dp[i] = i
+            else:
+                dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
+#         if n <= 2:
+#             return n
+        
+#         return self.climbStairs(n-1) + self.climbStairs(n-2)
         
