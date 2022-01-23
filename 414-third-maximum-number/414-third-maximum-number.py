19f0ef7491d27c69875df5ae48459544dcc961c3
Jing <jyang.ustc@gmail.com>
2022-01-23 16:13:22 -0600

    Time: 105 ms (9.63%), Space: 15.5 MB (25.59%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        # 时间复杂度：使用 Set 去重的复杂度为 O(n)；排序复杂度为 O(nlogn)。整体复杂度为 O(nlogn)
+        # 空间复杂度：O(n)
         
         n = sorted(set(nums))
         if not n:
