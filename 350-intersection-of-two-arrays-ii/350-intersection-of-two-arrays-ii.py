8d7fbab4ddab03243f0c6a337950e1beba3047fa
Jing <jyang.ustc@gmail.com>
2022-01-23 12:42:42 -0600

    Time: 69 ms (34.65%), Space: 14.5 MB (43.05%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,29 +1,34 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        if not nums1 or not nums2:
-            return None
-        
-        if len(nums1) > len(nums2):
-            return self.intersect(nums2, nums1)
-        
-        res = list()
-        n1, n2 = sorted(nums1), sorted(nums2)
-        p1 = p2 = 0
-        
-        while p1 < len(nums1) and p2 < len(nums2):
-            if n1[p1] == n2[p2]:
-                res.append(n1[p1])
-                p1 += 1
-                p2 += 1
-            elif n1[p1] < n2[p2]:
-                p1 += 1
-            else:
-                p2 += 1
-        
-        return res
+        # simple counter
+        from collections import Counter
+        return list((Counter(nums1) & Counter(nums2)).elements())
+
         
         
+#         # two pointers
+#         if not nums1 or not nums2:
+#             return None
+        
+#         if len(nums1) > len(nums2):
+#             return self.intersect(nums2, nums1)
+        
+#         res = list()
+#         n1, n2 = sorted(nums1), sorted(nums2)
+#         p1 = p2 = 0
+        
+#         while p1 < len(nums1) and p2 < len(nums2):
+#             if n1[p1] == n2[p2]:
+#                 res.append(n1[p1])
+#                 p1 += 1
+#                 p2 += 1
+#             elif n1[p1] < n2[p2]:
+#                 p1 += 1
+#             else:
+#                 p2 += 1
+        
+#         return res
         
         # https://leetcode.com/problems/intersection-of-two-arrays-ii/discuss/82247/Three-Python-Solutions
         # see the comments!!!
