c7c545721aa835019e249665122cd8c2936032d1
Jing <jyang.ustc@gmail.com>
2022-01-23 16:07:14 -0600

    Time: 108 ms (7.63%), Space: 14.6 MB (31.38%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # one-line
+        
+        # https://leetcode-cn.com/problems/valid-anagram/solution/pai-xu-bi-dui-ha-xi-ji-lu-ci-shu-by-cht_-4vex/
+        
         if len(s) != len(t):
             return False
         
-        return collections.Counter(s) == collections.Counter(t)
+        count = [0]*26 # count of appearance of a to z, 'a' = chr(ord('a'))
+        
+        for i in range(len(s)):
+            count[ord(s[i])-ord('a')] += 1
+        
+        for i in range(len(t)):
+            count[ord(t[i])-ord('a')] -= 1
+            
+        for i in range(26):
+            if count[i] != 0:
+                return False
+        
+        return True
+        
+#         # one-line
+#         if len(s) != len(t):
+#             return False
+        
+#         return collections.Counter(s) == collections.Counter(t)
