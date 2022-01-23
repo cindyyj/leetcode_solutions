8ff265e493661d0d9ec07ab08881f88043f5e22b
Jing <jyang.ustc@gmail.com>
2022-01-23 16:08:21 -0600

    Time: 44 ms (84.95%), Space: 15 MB (17.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,22 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # 3. sort
+        return sorted(s) == sorted(t)
         
-        # https://leetcode-cn.com/problems/valid-anagram/solution/pai-xu-bi-dui-ha-xi-ji-lu-ci-shu-by-cht_-4vex/
+#         # 2. dict to save count
+#         # https://leetcode-cn.com/problems/valid-anagram/solution/pai-xu-bi-dui-ha-xi-ji-lu-ci-shu-by-cht_-4vex/
         
-        if len(s) != len(t):
-            return False
+#         if len(s) != len(t):
+#             return False
         
-        count = [0]*26 # count of appearance of a to z, 'a' = chr(ord('a'))
+#         count = [0]*26 # count of appearance of a to z, 'a' = chr(ord('a'))
         
-        for i in range(len(s)):
-            count[ord(s[i])-ord('a')] += 1
+#         for i in range(len(s)):
+#             count[ord(s[i])-ord('a')] += 1
         
-        for i in range(len(t)):
-            count[ord(t[i])-ord('a')] -= 1
+#         for i in range(len(t)):
+#             count[ord(t[i])-ord('a')] -= 1
         
-        return count == [0]*26
+#         return count == [0]*26
 
-#         # one-line
+#         # 1. one-line
 #         if len(s) != len(t):
 #             return False
         
