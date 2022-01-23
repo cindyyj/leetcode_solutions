2f36724478ea81c4e41766df670b928f2f840c56
Jing <jyang.ustc@gmail.com>
2022-01-23 10:08:16 -0600

    Time: 156 ms (81.07%), Space: 14.2 MB (50.55%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.py
@@ -4,23 +4,30 @@ class Solution:
         # 5 solutions, include using Fermat Theorem (too complicated)
         # n**2 = 1+3+..+ (2n-1) = 2*(1+2+3+...+n) -n = 2*n(n+1)/2 - n = n**2
         
-        # Hashset
-        # https://leetcode.com/problems/sum-of-square-numbers/discuss/1424957/Python-3-solutions%3A-Sqrt-HashSet-Two-Pointers-Clean-and-Concise-O(sqrt(c)) 
-        # transforms this into two sum
-        # Complexity
-        # Time: O(sqrt(c)), where c <= 2^31-1
-        # Space: O(sqrt(c))
-        
-        squares = set()
-        for i in range(int(c**0.5)+1):
-            squares.add(i*i)
-            
-        for a_square in squares:
-            b_square = c - a_square
-            if b_square in squares:
+        # sqrt
+        for a in range(int(c**0.5)+1):
+            b = (c - a*a)**0.5
+            if b == int(b):
                 return True
-        
         return False
+        
+#         # Hashset
+#         # https://leetcode.com/problems/sum-of-square-numbers/discuss/1424957/Python-3-solutions%3A-Sqrt-HashSet-Two-Pointers-Clean-and-Concise-O(sqrt(c)) 
+#         # transforms this into two sum
+#         # Complexity
+#         # Time: O(sqrt(c)), where c <= 2^31-1
+#         # Space: O(sqrt(c))
+        
+#         squares = set()
+#         for i in range(int(c**0.5)+1):
+#             squares.add(i*i)
+            
+#         for a_square in squares:
+#             b_square = c - a_square
+#             if b_square in squares:
+#                 return True
+        
+#         return False
 
         
 #         # two pointer
