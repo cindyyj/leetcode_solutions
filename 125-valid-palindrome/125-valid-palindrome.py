586915f4176d1528d2c32f096eca5e7eef526ddd
Jing <jyang.ustc@gmail.com>
2022-01-23 10:28:26 -0600

    Time: 56 ms (50.54%), Space: 14.6 MB (60.76%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,9 +1,28 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        # time: O(len(s))
-        # space: O(len(s))
+        # two pointer
         
-        cleaned_s = "".join([char for char in s.lower() if char.isalnum()])
-        return cleaned_s == cleaned_s[::-1]
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].lower().isalnum():
+                l += 1
+            elif not s[r].lower().isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l += 1
+                    r -= 1
+        
+        return True
+            
+        
+        
+#         # time: O(len(s))
+#         # space: O(len(s))
+        
+#         cleaned_s = "".join([char for char in s.lower() if char.isalnum()])
+#         return cleaned_s == cleaned_s[::-1]
         
