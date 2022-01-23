6d04a476f82c8d1625a73935592ec74a69e0df49
Jing <jyang.ustc@gmail.com>
2022-01-22 18:40:21 -0600

    Time: 63 ms (12.16%), Space: 14.2 MB (48.58%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def getNext(self, n):
+        happy_sum = 0
+        
+        while n:            
+            # n, digit = (n // 10, n % 10) = divmod(n, 10)            
+            happy_sum += (n % 10) ** 2
+            n //= 10
+            
+        return happy_sum
+    
+    def isHappy(self, n: int) -> bool:
+        
+        seen = set()
+        
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = self.getNext(n)
+            
+        return n == 1
+            
+        # 将当前数替换为它每个位置上的数字的平方和。
+        # 如果为1，则是快乐数
+        # 如果替换后的数再之前出现过，则说明陷入无限循环，此数不是快乐数
+
+        # https://leetcode.com/problems/happy-number/solution/
+        # Determining the time complexity for this problem is challenging for an "easy" level question. If you're new to these problems, have a go at calculating the time complexity for just the getNext(n) function (don't worry about how many numbers will be in the chain).
+        # very hard to determine time complexity!!!     
