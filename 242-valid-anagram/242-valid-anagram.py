04ba245d49aae7373931eb979d9e064e082a6fd0
Jing <jyang.ustc@gmail.com>
2022-01-23 15:56:47 -0600

    Time: 67 ms (37.25%), Space: 14.4 MB (95.51%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,4 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # one-line
+        if len(s) != len(t):
+            return False
+        
         return collections.Counter(s) == collections.Counter(t)
