bd270d6a8f67950cd048d5e6abbd93671313c76f
Jing <jyang.ustc@gmail.com>
2022-01-23 10:24:37 -0600

    Time: 38 ms (92.61%), Space: 15.1 MB (34.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,5 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        sgood = "".join([char for char in s.lower() if char.isalnum()])
-        return sgood == sgood[::-1]
+        
+        # time: O(len(s))
+        # space: O(len(s))
+        
+        cleaned_s = "".join([char for char in s.lower() if char.isalnum()])
+        return cleaned_s == cleaned_s[::-1]
         
