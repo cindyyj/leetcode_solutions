0b2e2d3d5139150e9976756b6264589ec197d5d1
Jing <jyang.ustc@gmail.com>
2022-02-04 14:24:49 -0600

    Time: 81 ms (45.50%), Space: 13.9 MB (97.88%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,26 +1,26 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        # 
+        # --------------------------- METHOD 2 ---------------------------
         
-        neg_cnt = 0
+#         neg_cnt = 0
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                return 0
-            elif nums[i] < 0:
-                neg_cnt += 1
-        
-        return [1 if neg_cnt % 2 == 0 else -1][0]
-        
-
-#         # --------------------------- METHOD 1 ---------------------------
-#         sign = 1
-        
 #         for i in range(len(nums)):
 #             if nums[i] == 0:
 #                 return 0
 #             elif nums[i] < 0:
-#                 sign = - sign
+#                 neg_cnt += 1
+        
+#         return [1 if neg_cnt % 2 == 0 else -1][0]
+        
 
-#         return sign               
+        # --------------------------- METHOD 1: simplest ---------------------------
+        sign = 1
+        
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                return 0
+            elif nums[i] < 0:
+                sign = - sign
+
+        return sign               
         
