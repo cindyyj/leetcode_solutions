4ecf9201d31da33b288dd145de96bbca6d2bed4f
Jing <jyang.ustc@gmail.com>
2022-02-04 20:31:09 -0600

    Time: 208 ms (84.34%), Space: 16.1 MB (90.46%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.py
@@ -5,23 +5,15 @@ class Solution:
         # invariant: for every misplaced odd there is misplaced even
         # since there is just enough space for odds and evens
         
-        i = 0 # pointer for even misplaced
-        j = 1 # pointer for odd misplaced
-        n = len(nums)
-
-        while i < n and j < n:
+        i, j, n = 0, 1, len(nums)  # pointer for even misplaced, pointer for odd misplaced
+        while j < n and i < n:
             if nums[i] % 2 == 0:
                 i += 2
             elif nums[j] % 2 == 1:
                 j += 2
-            else:
-                # a[i] % 2 == 1 AND a[j] % 2 == 0
-                nums[i],nums[j] = nums[j],nums[i]
-                i += 2
-                j += 2
-
+            else: # a[i] % 2 == 1 AND a[j] % 2 == 0
+                nums[i], nums[j] = nums[j], nums[i]
         return nums
-    
     
 #         # change it using space O(n)
 #         even_idx, odd_idx = 0, 1
