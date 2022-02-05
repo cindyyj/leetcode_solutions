e5f19e7ae05282482126816c883e7c253bbaa964
Jing <jyang.ustc@gmail.com>
2022-02-04 19:10:38 -0600

    Time: 2625 ms (25.85%), Space: 45.2 MB (65.01%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,12 +1,29 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         
-        pos = [num for num in nums if num > 0]
-        neg = [num for num in nums if num < 0]
+        # two pointer
+        pos_idx = 0
+        neg_idx = 1
+        res = [0] * len(nums)
         
-        ans = []
-        for i in range(len(pos)):
-            ans.append(pos[i])
-            ans.append(neg[i])
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res[pos_idx] = nums[i]
+                pos_idx += 2
+            else:
+                res[neg_idx] = nums[i]
+                neg_idx += 2     
+                
+        return res
+            
+
+#         # list comprehension
+#         pos = [num for num in nums if num > 0]
+#         neg = [num for num in nums if num < 0]
         
-        return ans
+#         ans = []
+#         for i in range(len(pos)):
+#             ans.append(pos[i])
+#             ans.append(neg[i])
+        
+#         return ans
