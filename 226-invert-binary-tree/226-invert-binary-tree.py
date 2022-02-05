769ad0d7079145e582740fd40c6ae00946e4eed5
Jing <jyang.ustc@gmail.com>
2022-02-05 15:50:41 -0600

    Time: 43 ms (42.37%), Space: 13.6 MB (99.99%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -6,12 +6,26 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return root
+        # https://leetcode.com/problems/invert-binary-tree/discuss/62714/3-4-lines-Python
+        # recursive 3 line
+        # The assignment operation = is performed after the RHS of the statement is evaluated. 
+        # Here, the RHS (right hand side) of statement root.left, root.right = self.invertTree(root.right), self.invertTree(root.left) 
+        # is an implicit tuple object that has 2 elements namely self.invertTree(root.right) and self.invertTree(root.left). 
+        # Only after both of these function calls are evaluated, the assignment operation is performed.
+        # Because of this order, we don't lose any information.
         
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        
+        return root
+        
+#         # recursive
+#         if not root:
+#             return root
+        
+#         root.left, root.right = root.right, root.left
+#         self.invertTree(root.left)
+#         self.invertTree(root.right)
 
-        return root       
+#         return root       
         
