ec6dbb3b3d8d8319f39a19146843614a38e84b9d
Jing <jyang.ustc@gmail.com>
2022-02-05 11:18:25 -0600

    Time: 24 ms (97.49%), Space: 13.9 MB (95.38%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -5,13 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, res):
-        if root:
-            self.dfs(root.left, res)
-            self.dfs(root.right, res)
-            res.append(root.val)
+#     def dfs(self, root, res):
+#         if root:
+#             self.dfs(root.left, res)
+#             self.dfs(root.right, res)
+#             res.append(root.val)
     
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        self.dfs(root, res)
-        return res
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+#         self.dfs(root, res)
+#         return res
+
+# iteratively        
+    def postorderTraversal(self, root):
+        res, stack = [], [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
