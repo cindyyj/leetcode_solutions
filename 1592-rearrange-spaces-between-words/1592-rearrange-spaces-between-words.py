a5faf45c811e2388271612f6fbe7214670e29408
Jing <jyang.ustc@gmail.com>
2022-02-04 19:30:25 -0600

    Time: 88 ms (5.06%), Space: 13.8 MB (99.91%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -1,9 +1,18 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
+        
+        # good review on string operations! 
+        
         words = text.split() # split(sep=None) will discard empty strings.
+        
+        # word count
         cnt = len(words)
+        
+        # space count
         spaces = text.count(' ')
+        
         gap = 0 if cnt == 1 else spaces // (cnt - 1)
       # trailing_spaces = spaces if gap == 0 else spaces % (cnt - 1)
         trailing_spaces = spaces - gap * (cnt - 1) # credit to @madno
+        
         return (' ' * gap).join(words) + ' ' * trailing_spaces                
