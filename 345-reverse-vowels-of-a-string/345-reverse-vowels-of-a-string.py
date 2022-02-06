496acbc6c8d215972a456eecaa260153a5eaf84f
Jing <jyang.ustc@gmail.com>
2022-02-06 17:44:21 -0600

    Time: 52 ms (85.59%), Space: 16.2 MB (17.63%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -1,23 +1,30 @@
 class Solution:
-    def isVowel(self, ch: str) -> bool:
-        return ch in 'aeiouAEIOU'
+#     def isVowel(self, ch: str) -> bool:
+#         return ch in 'aeiouAEIOU'
     
-    def reverseVowels(self, s: str) -> str:
+#     def reverseVowels(self, s: str) -> str:
         
-        n = len(s)
-        s = list(s)
-        l, r = 0, n - 1
-        while l < r:
-            while l < r and not self.isVowel(s[l]):
-                l += 1
-            while l < r and not self.isVowel(s[r]):
-                r -= 1
+#         n = len(s)
+#         s = list(s)
+#         l, r = 0, n - 1
+#         while l < r:
+#             while l < r and not self.isVowel(s[l]):
+#                 l += 1
+#             while l < r and not self.isVowel(s[r]):
+#                 r -= 1
                 
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
+#             s[l], s[r] = s[r], s[l]
+#             l += 1
+#             r -= 1
         
-        return "".join(s)
+#         return "".join(s)
 
 
 # 链接：https://leetcode-cn.com/problems/reverse-vowels-of-a-string/solution/fan-zhuan-zi-fu-chuan-zhong-de-yuan-yin-2bmos/
+
+    # classic regex
+    # regular expression
+    def reverseVowels(self, s: str) -> str:
+        # the (?i) is an inline flag corresponding to re.IGNORECASE. In other words (?i)[aeiou] is equivalent to [aeiouAEIOU].
+        vowels = re.findall('(?i)[aeiou]', s)
+        return re.sub('(?i)[aeiou]', lambda m: vowels.pop(), s)
