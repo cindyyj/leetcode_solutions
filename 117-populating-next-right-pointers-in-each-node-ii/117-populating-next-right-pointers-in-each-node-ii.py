20dfe688b3d64f308d2a34d9397e2c7bd2d23c9b
Jing <jyang.ustc@gmail.com>
2022-02-06 13:03:31 -0600

    Time: 52 ms (72.54%), Space: 15.3 MB (78.78%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -16,54 +16,55 @@ class Solution:
         # Connect the tail with every one of the possible nodes in child level, update it only if the connected node is not nil.
         # Do this one level by one level. The whole thing is quite straightforward.
 
+        
+#         if not root:
+#             return root
+        
+#         from collections import deque
+        
+#         queue = deque([root, None])
+        
+#         while queue:
+#             node = queue.popleft()              
+
+#             if node is None: # end of curr level
+#                 if len(queue) >= 1:
+#                     queue.append(None)
+
+#             else: # if node
+#                 node.next = queue[0]
+
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+
+#         return root
+  
         if not root:
             return root
         
         from collections import deque
+        curr_level = deque([root, None])
         
-        queue = deque([root, None])
-        
-        while queue:
-            node = queue.popleft()              
-
-            if node is None: # end of curr level
-                if len(queue) >= 1:
-                    queue.append(None)
-
-            else: # if node
-                node.next = queue[0]
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-
-        return root
-  
-         # if not root:
-#             return root
-        
-#         from collections import deque
-#         curr_level = deque([root, None])
-        
-#         while curr_level:
-#             next_level = []
+        while curr_level:
+            next_level = deque([])
             
-#             for _ in range(len(curr_level)):
+            for _ in range(len(curr_level)):
                 
-#                 node = curr_level.pop()
-#                 if not node:
-#                     if len(next_level) != 0:
-#                         next_level.append(None)
+                node = curr_level.popleft()
+                if not node:
+                    if len(next_level) != 0:
+                        next_level.append(None)
                 
-#                 else: 
-#                     node.next = curr_level[0] # error, list index out of range!!
+                else: 
+                    node.next = curr_level[0] # error, list index out of range!!
 
-#                     if node.left:
-#                         next_level.append(node.left)
-#                     if node.right:
-#                         next_level.append(node.right)
+                    if node.left:
+                        next_level.append(node.left)
+                    if node.right:
+                        next_level.append(node.right)
                     
-#             curr_level = next_level
+            curr_level = next_level
         
-#         return root
+        return root
