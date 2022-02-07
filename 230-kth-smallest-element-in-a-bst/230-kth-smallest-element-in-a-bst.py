6755ffbb9d659cc023c0c069bd26da0ffcbf7905
Jing <jyang.ustc@gmail.com>
2022-02-07 12:52:19 -0600

    Time: 52 ms (79.64%), Space: 18.2 MB (14.99%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -13,16 +13,30 @@ class TreeNode:
         
 class Solution:
     
-    def inorder(self, root, nums):
-        if not root:
-            return 
+#     def inorder(self, root, nums):
+#         if not root:
+#             return 
         
-        self.inorder(root.left, nums)
-        nums.append(root.val)
-        self.inorder(root.right, nums)
+#         self.inorder(root.left, nums)
+#         nums.append(root.val)
+#         self.inorder(root.right, nums)
+        
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         nums = []
+#         self.inorder(root, nums)
+        
+#         return nums[k-1]
+    
+
+    # leetcode official solution
+    # great overview of tree reversal
+    # https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/
+    # Time complexity : O(N) to build a traversal.
+    # Space complexity : O(N) to keep an inorder traversal.
         
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nums = []
-        self.inorder(root, nums)
         
-        return nums[k-1]
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        
+        return inorder(root)[k - 1]
