9b93f873b342104fc249cd8f7396f75064ef5e78
Jing <jyang.ustc@gmail.com>
2022-02-06 18:22:26 -0600

    Time: 130 ms (35.21%), Space: 15.6 MB (32.94%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -7,9 +7,16 @@
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        # official leetcode solution
-        # https://leetcode.com/problems/merge-two-binary-trees/solution/
-        
+#         official leetcode solution
+#         https://leetcode.com/problems/merge-two-binary-trees/solution/
+#         时间复杂度：O(min(m,n))，其中 m 和 n 分别是两个二叉树的节点个数。对两个二叉树同时进行深度优先搜索，
+#         只有当两个二叉树中的对应节点都不为空时才会对该节点进行显性合并操作，因此被访问到的节点数不会超过较小的二叉树的节点数。
+#         比如一个很矮的树和很长的树组合， 多的那部分相当于剪树枝直接连接，所以时间复杂度是O（min（m,n))
+#         空间复杂度：O(min(m,n))，其中 m 和 n 分别是两个二叉树的节点个数。
+#         空间复杂度取决于递归调用的层数，递归调用的层数不会超过较小的二叉树的最大高度，最坏情况下，二叉树的高度等于节点数。
+
+#         链接：https://leetcode-cn.com/problems/merge-two-binary-trees/solution/he-bing-er-cha-shu-by-leetcode-solution/
+
         if not root1:
             return root2
         if not root2:
