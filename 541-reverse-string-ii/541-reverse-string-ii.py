4ab4598287ff875a6adfca911b120d7f187655b2
Jing <jyang.ustc@gmail.com>
2022-02-07 14:04:12 -0600

    Time: 33 ms (73.14%), Space: 14 MB (93.34%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        # https://leetcode.com/problems/reverse-string-ii/solution/
+        chars = list(s)
+        for i in range(0, len(chars), 2 * k):
+            chars[i:i+k] = reversed(chars[i:i+k])
+        
+        return "".join(chars)
+        
