48c5a0a7d1e5399c591a2c2c72d6dccbf449916c
Jing <jyang.ustc@gmail.com>
2022-02-07 14:07:54 -0600

    Time: 24 ms (98.52%), Space: 14.1 MB (84.35%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.py
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -1,10 +1,21 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         
-        # https://leetcode.com/problems/reverse-string-ii/solution/
-        chars = list(s)
-        for i in range(0, len(chars), 2 * k):
-            chars[i:i+k] = reversed(chars[i:i+k])
+        s = [ s[i:i+k] for i in range(0, len(s), k)]
         
-        return "".join(chars)
+        for i in range(0, len(s), 2):
+            s[i] = s[i][::-1]
+            
+        return ''.join(s)
+		# Divide s into an array of substrings length k
+		# Reverse every other substring, beginning with s[0]
+		# Join array of substrings into one string and return 
         
+        
+#         # https://leetcode.com/problems/reverse-string-ii/solution/
+#         chars = list(s)
+#         for i in range(0, len(chars), 2 * k):
+#             chars[i:i+k] = reversed(chars[i:i+k])
+        
+#         return "".join(chars)
+
