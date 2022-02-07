d356dc7ac22abdab36b06ae0adbb67ad5759f805
Jing <jyang.ustc@gmail.com>
2022-02-07 16:30:13 -0600

    Time: 59 ms (12.41%), Space: 13.7 MB (100.00%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.py
+++ 917-reverse-only-letters/917-reverse-only-letters.py
@@ -1,17 +1,31 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
         
-        l, r = 0, len(s) - 1
-        s = list(s)
-        while l < r:
-            if not s[l].isalpha():
-                l += 1
-            elif not s[r].isalpha():
-                r -= 1
+        # stack
+        letters = [c for c in s if c.isalpha()]
+        
+        ans = []
+        for c in s:
+            if c.isalpha():
+                ans.append(letters.pop())
             else:
-                # string doesn't support item assignment!!!
-                s[l], s[r] = s[r], s[l]
-                l += 1
-                r -= 1
+                ans.append(c)
+        
+        return ''.join(ans)
+        
+        
+#         # two pointers
+#         l, r = 0, len(s) - 1
+#         s = list(s)
+#         while l < r:
+#             if not s[l].isalpha():
+#                 l += 1
+#             elif not s[r].isalpha():
+#                 r -= 1
+#             else:
+#                 # string doesn't support item assignment!!!
+#                 s[l], s[r] = s[r], s[l]
+#                 l += 1
+#                 r -= 1
                 
-        return ''.join(s)
+#         return ''.join(s)
