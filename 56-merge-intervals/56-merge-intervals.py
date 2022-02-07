3c8cead815c7745290bbf57a18739e18510e40c4
Jing <jyang.ustc@gmail.com>
2022-02-06 19:31:39 -0600

    Time: 241 ms (22.12%), Space: 18 MB (61.02%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,19 +3,18 @@ class Solution:
         # https://leetcode.com/problems/merge-intervals/solution/
         # official leetcode solution 
         # classic!!! 
+        
         intervals.sort(key=lambda x: x[0])
         
         merged = []
-        
         for interval in intervals:
-            # 如果列表为空，或者当前区间与上一区间不重合，直接添加
+            
             if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
                 
             else:
-                #  否则的话，我们就可以与上一区间进行合并
-                merged[-1][1] = max(merged[-1][1], interval[1])
-
+                # updated right end of last interval
+                merged[-1][1] = max(interval[1], merged[-1][1])
+        
         return merged
-
 # # 链接：https://leetcode-cn.com/problems/merge-intervals/solution/he-bing-qu-jian-by-leetcode-solution/
