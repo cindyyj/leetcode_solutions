565bb85725ac56fa0993e57f3d4fb4bac5937553
Jing <jyang.ustc@gmail.com>
2022-02-06 19:55:18 -0600

    Time: 135 ms (26.21%), Space: 17.6 MB (31.43%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,15 +1,37 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # optimized
+        # start, end
+        s, e = newInterval[0], newInterval[1]
         
-        intervals.append(newInterval)
-        intervals.sort(key=lambda x : x[0])
+        left, right = [], []
+        #  WRONG!!!
+        # MISTAKE: Don't use multi-assignment for mutable object!!!
+        # left = right = []
         
-        merged = []
-        for interval in intervals:
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+        for i in intervals:
+            if i[1] < s:
+                left.append(i)
                 
+            elif i[0] > e:
+                right.append(i)
+            
             else:
-                merged[-1][1] = max(merged[-1][1], interval[1])
+                s = min(s, i[0])
+                e = max(e, i[1])
         
-        return merged
+        return left + [[s,e]] + right
+        
+#         # not optimized
+#         intervals.append(newInterval)
+#         intervals.sort(key=lambda x : x[0])
+        
+#         merged = []
+#         for interval in intervals:
+#             if not merged or merged[-1][1] < interval[0]:
+#                 merged.append(interval)
+                
+#             else:
+#                 merged[-1][1] = max(merged[-1][1], interval[1])
+        
+#         return merged
