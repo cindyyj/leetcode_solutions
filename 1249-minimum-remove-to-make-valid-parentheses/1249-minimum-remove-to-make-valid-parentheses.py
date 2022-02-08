2bbf2db9b8e25e776134c3db3e84c9c74bb9d139
Jing <jyang.ustc@gmail.com>
2022-02-08 11:16:17 -0600

    Time: 118 ms (76.30%), Space: 15.9 MB (59.26%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -5,28 +5,55 @@ class Solution:
         # Convert string to list, 
         # because String is an immutable data structure in Python and it's much easier and memory-efficient to deal with a list for this task.
         
-        indexes_to_remove = set()
-        stack = []
         s = list(s)
+        stack = []
         
         for i, c in enumerate(s):
-            if c not in '()':
-                continue
-            elif c == '(':
+            if c == '(':
                 stack.append(i)
-            # c == ) and stack not empty
-            elif stack:
-                stack.pop()
-            # c == ) and stack empty
-            else:
-                indexes_to_remove.add(i)
-            
-        indexes_to_remove = indexes_to_remove.union(stack)
+            if c == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    # if no matching (, remove this unmatched )
+                    s[i] =''
+        
+        while stack:
+            i = stack.pop()
+            s[i] = ''
+        
+        return ''.join(s)
+        
 
-        string = []
-        for i, c in enumerate(s):
-            if i not in indexes_to_remove:
-                string.append(c)
+# # Iterate through list
+# # Keep track of indices with open parentheses in the stack. In other words, when we come across open parenthesis we add an index to the stack.
+# # When we come across close parenthesis we pop an element from the stack. 
+# # If the stack is empty we replace current list element with an empty string
+# # After iteration, we replace all indices we have in the stack with empty strings, because we don't have close parentheses for them.
+# # Convert list to string and return
+
+#         indexes_to_remove = set()
+#         stack = []
+#         s = list(s)
+        
+#         for i, c in enumerate(s):
+#             if c not in '()':
+#                 continue
+#             elif c == '(':
+#                 stack.append(i)
+#             # c == ) and stack not empty
+#             elif stack:
+#                 stack.pop()
+#             # c == ) and stack empty
+#             else:
+#                 indexes_to_remove.add(i)
             
-        return ''.join(string)
+#         indexes_to_remove = indexes_to_remove.union(stack)
+
+#         string = []
+#         for i, c in enumerate(s):
+#             if i not in indexes_to_remove:
+#                 string.append(c)
+            
+#         return ''.join(string)
         
