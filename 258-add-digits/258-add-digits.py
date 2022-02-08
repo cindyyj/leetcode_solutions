bb20f53211a3cfd434fb5677243163458bac219d
Jing <jyang.ustc@gmail.com>
2022-02-07 18:28:26 -0600

    Time: 99 ms (5.68%), Space: 13.9 MB (81.53%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,15 +1,31 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         
-        digital_root = 0
+        if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
         
-        while num > 0:
-            digital_root += num % 10 
-            num //= 10
+        else:
+            return num % 9
+        
+        
+#         digital_root = 0
+        
+#         while num > 0:
+#             digital_root += num % 10 
+#             num //= 10
             
-            if num == 0 and digital_root >= 10:
-                num = digital_root
-                digital_root = 0
+#             if num == 0 and digital_root >= 10:
+#                 num = digital_root
+#                 digital_root = 0
         
-        return digital_root
-        
+#         return digital_root
+
+
+#         当 num 是 0 时，各位之和是自身 0；
+#         当 num 能被 9 整除时，各位之和是 9；
+#         当 num 不能被 9 整除时，各位之和是对 9 取余的结果。
+
+#         作者：Dine
+#         链接：https://leetcode-cn.com/problems/add-digits/solution/258-ge-wei-xiang-jia-by-dine-s69o/
