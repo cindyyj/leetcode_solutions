f0a5f77015c3bb1c7c90ec3ea25ad4581ffd19bf
Jing <jyang.ustc@gmail.com>
2022-02-08 15:43:20 -0600

    Time: 117 ms (43.51%), Space: 15.5 MB (90.40%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -1,15 +1,29 @@
+from collections import Counter
+
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        res = set()
-        seen = set()
+        res = 0
+        counter = Counter(nums)
         
-        for num in nums:
-            if num - k in seen:
-                res.add((num - k, num))
-            if num + k in seen:
-                res.add((num, num + k))
+        for num in counter:
+            if k > 0 and num + k in counter:
+                res += 1
+            elif k == 0 and counter[num] >= 2:
+                res += 1
+        
+        return res
+        
+        
+#         res = set()
+#         seen = set()
+        
+#         for num in nums:
+#             if num - k in seen:
+#                 res.add((num - k, num))
+#             if num + k in seen:
+#                 res.add((num, num + k))
                 
-            seen.add(num)
+#             seen.add(num)
             
-        return len(res)
+#         return len(res)
                 
