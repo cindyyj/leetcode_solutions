8995345db363a6279c10c3b4607ba5862c787798
Jing <jyang.ustc@gmail.com>
2022-02-08 15:21:54 -0600

    Time: 73 ms (83.60%), Space: 13.9 MB (92.95%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -2,21 +2,33 @@ from collections import defaultdict
 
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
+        # nice usage of defaultdict
+        # https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/discuss/1471015/Python-Clean-and-concise.-Dictionary-T.C-O(N)        
         
-        # hashmap + dict
         seen = defaultdict(int)
-        
         res = 0
         
-        for i in range(len(nums)):
-            if (nums[i] - k) in seen:
-                res += seen[nums[i] - k]
-            if (nums[i] + k) in seen:
-                res += seen[nums[i] + k]
-            
-            seen[nums[i]] += 1
+        for num in nums:
+            # if such key doesn't exist, return 0
+            res += seen[num - k] + seen[num + k]
+            seen[num] += 1
         
         return res
+
+#         # hashmap + dict
+#         seen = defaultdict(int)
+        
+#         res = 0
+        
+#         for i in range(len(nums)):
+#             if (nums[i] - k) in seen:
+#                 res += seen[nums[i] - k]
+#             if (nums[i] + k) in seen:
+#                 res += seen[nums[i] + k]
+            
+#             seen[nums[i]] += 1
+        
+#         return res
             
 #         # brute force
 #         res, n = 0, len(nums)
