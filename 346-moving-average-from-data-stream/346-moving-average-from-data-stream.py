0df6cf2c9e3bc652dc6d24894e62c47f1078f9e6
Jing <jyang.ustc@gmail.com>
2022-02-08 14:12:35 -0600

    Time: 79 ms (73.00%), Space: 17.2 MB (57.42%) - LeetHub

--- 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
@@ -1,16 +1,37 @@
+from collections import deque
+
 class MovingAverage:
-
-    def __init__(self, size: int):
+    
+    def __init__(self, size:int):
         self.size = size
-        self.queue = [] #  initialize a variable queue to store the values from the data stream
+        self.q = deque()
+        self.window_sum = 0
+        self.total_len = 0
+    
+    def next(self, val: int) -> float:
+        self.total_len += 1
+        
+        self.q.append(val)
+        
+        tail = self.q.popleft() if self.total_len > self.size else 0
+        self.window_sum += (val - tail)
+        
+        return self.window_sum / min(self.total_len, self.size)
+
+#     # use queue
+#     # time: O(window_size), retrieve last n=window_size to calculate the moving average
+#     # space: O(M), total length of data feeds
+#     def __init__(self, size: int):  
+#         self.size = size
+#         self.queue = [] #  initialize a variable queue to store the values from the data stream
         
 
-    def next(self, val: int) -> float:
-        size, queue = self.size, self.queue
-        queue.append(val)
-        window_sum = sum(queue[-size:])
+#     def next(self, val: int) -> float:
+#         size, queue = self.size, self.queue
+#         queue.append(val)
+#         window_sum = sum(queue[-size:])  # -k will not make index out of range!!! 
         
-        return window_sum/ min(len(queue), size)
+#         return window_sum/ min(len(queue), size)
         
 
 
