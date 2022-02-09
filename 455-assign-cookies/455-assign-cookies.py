e7998da376743dbc150dd3d422af2c529da288ff
Jing <jyang.ustc@gmail.com>
2022-02-08 18:53:19 -0600

    Time: 168 ms (68.25%), Space: 15.8 MB (93.26%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -1,6 +1,12 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         # https://leetcode.com/problems/assign-cookies/discuss/210952/Python3
+        
+        # 先对g, s两个数组进行排序
+        # 贪心算法
+        # 贪心思想1 优先满足需求因子较小的孩子。因为如果较小需求的孩子无法被满足，则之后的较大的需求更不可能能被满足了。
+        # 贪心思想2 尽量用较小的糖果去优先满足孩子。
+        
         g.sort()    # 对需求因子进行排序，从小到大
         s.sort()    # 对糖果数组进行排序，从小到大
         child  = 0  # 记录可以被满足孩子数
