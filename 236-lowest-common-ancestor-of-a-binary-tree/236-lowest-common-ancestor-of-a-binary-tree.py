7a12369e80f97e2fb403ac1a07634a8c8f71144e
Jing <jyang.ustc@gmail.com>
2022-02-08 19:39:34 -0600

    Time: 165 ms (7.49%), Space: 26.3 MB (49.44%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,26 +6,41 @@
 #         self.right = None
 
 class Solution:
+
+    def DFS(self,root,p,q):
+        if root:
+            if root in (None,p,q):
+                return root
+            l = self.DFS(root.left,p,q)
+            r = self.DFS(root.right,p,q)
+            return root if l and r else l or r        
+        else:
+            return None
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.DFS(root,p,q)    
+    
+    
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        # https://www.hrwhisper.me/algorithm-lowest-common-ancestor-of-a-binary-tree/
-        # 二叉树公共祖先专题
-        """
-        Using a bottom-up approach, we can improve over the top-down approach by avoiding traversing the same nodes over and over again.
+#         # https://www.hrwhisper.me/algorithm-lowest-common-ancestor-of-a-binary-tree/
+#         # 二叉树公共祖先专题
+#         """
+#         Using a bottom-up approach, we can improve over the top-down approach by avoiding traversing the same nodes over and over again.
 
-        We traverse from the bottom, and once we reach a node which matches one of the two nodes, we pass it up to its parent. The parent would then test its left and right subtree if each contain one of the two nodes. If yes, then the parent must be the LCA and we pass its parent up to the root. If not, we pass the lower node which contains either one of the two nodes (if the left or right subtree contains either p or q), or NULL (if both the left and right subtree does not contain either p or q) up.
+#         We traverse from the bottom, and once we reach a node which matches one of the two nodes, we pass it up to its parent. The parent would then test its left and right subtree if each contain one of the two nodes. If yes, then the parent must be the LCA and we pass its parent up to the root. If not, we pass the lower node which contains either one of the two nodes (if the left or right subtree contains either p or q), or NULL (if both the left and right subtree does not contain either p or q) up.
 
-        Sounds complicated? Surprisingly the code appears to be much simpler than the top-down one.       
-        """
-        if not root or root == p or root == q:
-            return root
+#         Sounds complicated? Surprisingly the code appears to be much simpler than the top-down one.       
+#         """   
         
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+#         if not root or root == p or root == q:
+#             return root
         
-        if left and right:
-            return root
-        return left if left else right
+#         left = self.lowestCommonAncestor(root.left, p, q)
+#         right = self.lowestCommonAncestor(root.right, p, q)
+        
+#         if left and right:
+#             return root
+#         return left if left else right
         
 #         # if root in (None, p, q): return root
 #         # left, right = (
