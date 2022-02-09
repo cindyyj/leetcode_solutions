95f58e55d2bd45c5757fee9d7599ddaf047d1bd1
Jing <jyang.ustc@gmail.com>
2022-02-09 13:40:22 -0600

    Time: 199 ms (66.82%), Space: 14.4 MB (94.38%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -8,7 +8,8 @@ class Solution:
         
         bed = [0] + flowerbed + [0] # same as flowerbed.insert(0, 0), flowerbed.append(0)
         
-        for i in range(1, len(flowerbed)+1):
+        # bed length n+2 (idx 0 to n+1), traverse from bed[1] to bed[n]
+        for i in range(1, len(flowerbed)+1):  
             if bed[i - 1] == bed[i] == bed[i + 1] == 0:
                 bed[i] = 1
                 n -= 1
