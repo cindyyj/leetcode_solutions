9c0c744a68d53f6675a736b3ed397d83e70c6f12
Jing <jyang.ustc@gmail.com>
2022-02-09 16:14:17 -0600

    Time: 154 ms (56.38%), Space: 19.3 MB (14.64%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,59 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        
+        n = len(s)
+        if n == 0:
+            return ""
+        
+        count = [0] * n
+        
+        i = 0
+        s = list(s)
+        
+        for j in range(n):
+            s[i] = s[j]
+            if i >0 and s[i-1] == s[j]:
+                count[i] = count[i-1] + 1
+            else:
+                count[i] = 1
+                
+            if count[i] == k:
+                i -= k
+                
+            i += 1
+            
+        return "".join(s[:i])
+        
+        # two pointer
+#         n = len(s)
+#         if n == 0:
+#             return ""
+        
+#         count = [0] * n
+#         i = 0
+#         res = list(s)
+        
+#         for j in range(n):
+#             res[i] = s[j]
+            
+#             if i > 0 and s[i - 1] == s[j]:
+#                 count[i] = count[i-1] + 1
+#             else:
+#                 count[i] = 1
+            
+#             if count[i] == k:
+#                 i -= k
+            
+#             i += 1
+        
+#         return "".join(s[:i])
+        
+
+
+        
+        # # recursive, time exceeds
+        # # time complexity, o(n), worst O(n*n/k), e.g. ABCCBA, k=2, 3 times recursion
+        # for i in range(len(s)):
+        #     if i + k <= len(s) and s[i:i + k] == s[i] * k:
+        #         return self.removeDuplicates(s[:i] + s[(i + k):], k)           
+        # return s
