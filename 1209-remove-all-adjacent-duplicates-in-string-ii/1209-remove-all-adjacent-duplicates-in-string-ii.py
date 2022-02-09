c04ae11b540ddcde09a28786ee8912c174802ca6
Jing <jyang.ustc@gmail.com>
2022-02-09 16:15:03 -0600

    Time: 164 ms (50.45%), Space: 19.4 MB (14.64%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,52 +1,29 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         
+        # two pointer
         n = len(s)
         if n == 0:
             return ""
         
         count = [0] * n
-        
         i = 0
-        s = list(s)
+        res = list(s)
         
         for j in range(n):
-            s[i] = s[j]
-            if i >0 and s[i-1] == s[j]:
+            res[i] = s[j]
+            
+            if i > 0 and res[i - 1] == s[j]:
                 count[i] = count[i-1] + 1
             else:
                 count[i] = 1
-                
+            
             if count[i] == k:
                 i -= k
-                
+            
             i += 1
-            
-        return "".join(s[:i])
         
-        # two pointer
-#         n = len(s)
-#         if n == 0:
-#             return ""
-        
-#         count = [0] * n
-#         i = 0
-#         res = list(s)
-        
-#         for j in range(n):
-#             res[i] = s[j]
-            
-#             if i > 0 and s[i - 1] == s[j]:
-#                 count[i] = count[i-1] + 1
-#             else:
-#                 count[i] = 1
-            
-#             if count[i] == k:
-#                 i -= k
-            
-#             i += 1
-        
-#         return "".join(s[:i])
+        return "".join(res[:i])
         
 
 
