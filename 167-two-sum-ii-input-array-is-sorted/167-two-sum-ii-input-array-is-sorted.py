4da1b2b661bd0b7e0299772c7e2d415636f7b585
Jing <jyang.ustc@gmail.com>
2022-02-09 18:10:41 -0600

    Time: 192 ms (9.96%), Space: 15 MB (5.67%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,31 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
+        # 0-index or 1-index ? 
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l + 1, r + 1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+        
+        
+        
 #         # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/discuss/51249/Python-different-solutions-(two-pointer-dictionary-binary-search)
 #         # two-pointer
 #     def twoSum1(self, numbers, target):
-#         l, r = 0, len(numbers)-1
-#         while l < r:
-#             s = numbers[l] + numbers[r]
-#             if s == target:
-#                 return [l+1, r+1]
-#             elif s < target:
-#                 l += 1
-#             else:
-#                 r -= 1
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
 
 #     # dictionary           
 #     def twoSum2(self, numbers, target):
@@ -22,19 +35,19 @@ class Solution:
 #                 return [dic[target-num]+1, i+1]
 #             dic[num] = i
 
-#     # binary search        
-#     def twoSum(self, numbers, target):
-        for i in range(len(numbers)):
-            l, r = i+1, len(numbers)-1
-            tmp = target - numbers[i]
-            while l <= r:
-                mid = l + (r-l)//2
-                if numbers[mid] == tmp:
-                    return [i+1, mid+1]
-                elif numbers[mid] < tmp:
-                    l = mid+1
-                else:
-                    r = mid-1
+# #     # binary search        
+# #     def twoSum(self, numbers, target):
+#         for i in range(len(numbers)):
+#             l, r = i+1, len(numbers)-1
+#             tmp = target - numbers[i]
+#             while l <= r:
+#                 mid = l + (r-l)//2
+#                 if numbers[mid] == tmp:
+#                     return [i+1, mid+1]
+#                 elif numbers[mid] < tmp:
+#                     l = mid+1
+#                 else:
+#                     r = mid-1
                 
 #         # hashmap, time O(n), space O(n)
         
