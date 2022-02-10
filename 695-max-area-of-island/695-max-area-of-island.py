82db219c8912a754553e2c5eacecc7ab1a177c7b
Jing <jyang.ustc@gmail.com>
2022-02-10 11:36:55 -0600

    Time: 327 ms (6.48%), Space: 16.6 MB (53.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,17 +1,45 @@
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
-        # time complexity: O(R*C) , number of rows * number of columns
-        def area(row, col):
-            if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col]:
-                # mark as visited
-                grid[row][col] = 0
-                return 1 + area(row - 1, col) + area(row + 1, col) + area(row, col - 1) + area(row, col + 1)
+    def inarea(self, grid, r, c):
+        return (0 <= r < len(grid)) and (0 <= c < len(grid[0]))
+    
+    def area(self, grid, r, c):
+        if not self.inarea(grid, r, c):
             return 0
         
-        return max(area(r, c) 
-                   for r in range(len(grid)) 
-                   for c in range(len(grid[0])))
+        if grid[r][c] != 1:
+            return 0
+        else: # grid[r][c] == 1
+            grid[r][c] = 2
+            
+        return (1 + 
+                self.area(grid, r - 1, c) + 
+                self.area(grid, r + 1, c) + 
+                self.area(grid, r, c - 1) + 
+                self.area(grid, r, c + 1)               
+               )
+        
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxarea = 0 
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                maxarea = max(maxarea, self.area(grid, r, c))
+        
+        return maxarea
+    
+    
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+#         # time complexity: O(R*C) , number of rows * number of columns
+#         def area(row, col):
+#             if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col]:
+#                 # mark as visited
+#                 grid[row][col] = 0
+#                 return 1 + area(row - 1, col) + area(row + 1, col) + area(row, col - 1) + area(row, col + 1)
+#             return 0
+        
+#         return max(area(r, c) 
+#                    for r in range(len(grid)) 
+#                    for c in range(len(grid[0])))
         
 # 遍历二维数组，对于每块土地，去其前后左右找相邻土地，再去前后左右的土地找其前后左右的土地，直到周围没有土地
 # 对于每一块已找过的土地，为避免重复计算，将其置为0
