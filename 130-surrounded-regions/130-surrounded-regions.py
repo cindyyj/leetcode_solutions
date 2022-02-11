ab448a3d64a50e4104284a7118148be747077f77
Jing <jyang.ustc@gmail.com>
2022-02-10 19:48:47 -0600

    Time: 238 ms (33.81%), Space: 16.4 MB (26.94%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        if not board:
+            return 
+        
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] == "O":
+                board[r][c] = "A" # connected to 'O'            
+                list(map(dfs, [r + 1, r - 1, r, r], [c, c, c + 1, c - 1]))            
+        
+        # mark Os connected to board O
+        for r in range(rows):
+            dfs(r, 0)
+            dfs(r, cols - 1)
+        
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows - 1, c)
+            
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "A": 
+                    board[r][c] = "O"
+                elif board[r][c] == "O": 
+                    board[r][c] = "X"           
+        
