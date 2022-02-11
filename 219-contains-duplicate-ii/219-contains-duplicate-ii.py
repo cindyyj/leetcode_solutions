e64a24079e5d76a1711987ce2e890a30d2904725
Jing <jyang.ustc@gmail.com>
2022-02-11 16:25:35 -0600

    Time: 672 ms (63.46%), Space: 27.4 MB (59.16%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        # sliding window + hashmap
+        """
+        This will work  in case of multiple duplicates? 1,0,1,1,2 and k =1 ??
+        Because the index in the hashmap gets updated so the distance i - dic[j] will always reflect the shortest distance between deplicates.
+        """
+        n = len(nums)        
+        hashmap = {}
+        for i, num in enumerate(nums):
+            if num in hashmap and i - hashmap[num] <= k:
+                return True
+            hashmap[num] = i
+        
+        return False         
