56d9572c8dfc173d09dc8c00d4572ca2c8068a41
Jing <jyang.ustc@gmail.com>
2022-02-11 17:59:42 -0600

    Time: 182 ms (38.62%), Space: 13.9 MB (99.13%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,7 +2,6 @@ from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        # two pointer
         l1, l2 = len(s1), len(s2)
         if l1 > l2:
             return False
@@ -10,17 +9,44 @@ class Solution:
         count1 = Counter(s1)
         count2 = Counter()
         
-        for i in range(l2):
-            count2[s2[i]] += 1
-            if i >= l1: 
-                count2[s2[i - l1]] -= 1
+        start = 0
+        for end in range(l2):
+            count2[s2[end]] += 1
+            
+            if end >= l1:
+                count2[s2[start]] -= 1
+                if count2[s2[start]] == 0:
+                    del count2[s2[start]]
+                start += 1
             
             if count1 == count2:
                 return True
-            
+        
         return False
 
         
+# # --------------------------- METHOD 2 ---------------------------
+        
+#         # two pointer
+#         l1, l2 = len(s1), len(s2)
+#         if l1 > l2:
+#             return False
+        
+#         count1 = Counter(s1)
+#         count2 = Counter()
+        
+#         for i in range(l2):
+#             count2[s2[i]] += 1
+#             if i >= l1: 
+#                 count2[s2[i - l1]] -= 1
+            
+#             if count1 == count2:
+#                 return True
+            
+#         return False
+
+# # --------------------------- METHOD 1 ---------------------------
+#         # sliding window
 #         # https://leetcode.com/problems/minimum-window-substring/
 #         # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 #         # https://leetcode.com/problems/substring-with-concatenation-of-all-words/
@@ -36,4 +62,3 @@ class Solution:
 #                 return True
         
 #         return False
-        
