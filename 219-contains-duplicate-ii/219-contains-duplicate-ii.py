8f48ee68097e6647471ef24a52ea0c78d99b7991
Jing <jyang.ustc@gmail.com>
2022-02-11 16:25:44 -0600

    Time: 751 ms (52.60%), Space: 27.4 MB (59.16%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -6,7 +6,6 @@ class Solution:
         This will work  in case of multiple duplicates? 1,0,1,1,2 and k =1 ??
         Because the index in the hashmap gets updated so the distance i - dic[j] will always reflect the shortest distance between deplicates.
         """
-        n = len(nums)        
         hashmap = {}
         for i, num in enumerate(nums):
             if num in hashmap and i - hashmap[num] <= k:
