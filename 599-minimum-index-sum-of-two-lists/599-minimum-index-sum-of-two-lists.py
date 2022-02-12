711da6fc3a0736e1a3aa4ec958497fbd1fb55eb3
Jing <jyang.ustc@gmail.com>
2022-02-12 15:54:00 -0600

    Time: 170 ms (76.77%), Space: 14.4 MB (97.54%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.py
@@ -1,5 +1,29 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        
+        # time O(l1+l2), space(min(l1,l2)* x); x average string length
+        
+        if (len(list1) > len(list2)):
+            return self.findRestaurant(list2, list1)
+        
+        # restaurant:i
+        dict1 = {r : i for i, r in enumerate(list1)}
+        dict2 = {r: dict1[r] + i for i, r in enumerate(list2) if r in dict1}
+        
+        MIN = float('inf')
+        res = []
+        
+        for key, val in dict2.items():
+            if val < MIN:
+                res = [key]
+                MIN = val
+            elif val == MIN:
+                res.append(key)
+                
+        return res
+        
+        
+        # 2 line solution
         d = {x: list1.index(x) + list2.index(x) for x in set(list1) & set(list2)}
         return [x for x in d if d[x] == min(d.values())]
         
