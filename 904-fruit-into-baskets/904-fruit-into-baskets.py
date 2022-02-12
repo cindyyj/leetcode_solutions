26b7e82c458e62954afa472729e9bfe052cc902d
Jing <jyang.ustc@gmail.com>
2022-02-12 11:02:08 -0600

    Time: 820 ms (81.59%), Space: 20.6 MB (7.76%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # fruits[i] is the type of fruit the ith tree produces! not the number of fruits! 
+        # longest subsequence that have 2 distinct types
+        # pick one fruit from tree, --> maximum subarray length = max fruits
+        
+        maxl = 0
+        start = 0
+        d = defaultdict(int)
+        
+        for end, fruit in enumerate(fruits):
+            d[fruit] += 1
+            
+            if len(d) > 2:
+                d[fruits[start]] -= 1
+                if d[fruits[start]] == 0:
+                    del d[fruits[start]]
+                start += 1
+            
+            maxl = max(maxl, end - start + 1)
+        
+        return maxl
