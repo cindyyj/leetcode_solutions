fb9a8fa2d2ed08cb402f30a0a6eee987b1cf0fc0
Jing <jyang.ustc@gmail.com>
2022-02-12 14:11:37 -0600

    Time: 62 ms (42.79%), Space: 14.2 MB (95.35%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,6 +1,23 @@
 from collections import Counter
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+# --------------------------- METHOD 4 ---------------------------  
+        s2t, t2s = {}, {}
+        for c1, c2 in zip(s, t):
+            # Case 1: No mapping exists in either of the dictionaries
+            if c1 not in s2t and c2 not in t2s:
+                s2t[c1] = c2
+                t2s[c2] = c1
+                
+            # Case 2: Ether mapping doesn't exist in one of the dictionaries or Mapping exists and
+            # it doesn't match in either of the dictionaries or both            
+            
+            elif s2t.get(c1) != c2 or t2s.get(c2) != c1:
+                return False
+        
+        return True
+    
 
 # --------------------------- METHOD 3 ---------------------------  
         return list(map(s.find, s)) == list(map(t.find, t))
@@ -45,8 +62,6 @@ class Solution:
 #             d2[ord(val)].append(i)
 #         return sorted(d1) == sorted(d2)
     
-#     def isIsomorphic5(self, s, t):
-#         return map(s.find, s) == map(t.find, t)
 
 #     def isIsomorphic6(self, s, t):
 #         d1, d2 = [0 for _ in xrange(256)], [0 for _ in xrange(256)]
