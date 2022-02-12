6579ccaafec874aa0ae160d9d25da28d5a22ecd1
Jing <jyang.ustc@gmail.com>
2022-02-12 16:02:31 -0600

    Time: 127 ms (57.06%), Space: 18.6 MB (94.57%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # https://leetcode.com/problems/top-k-frequent-elements/solution/
+        # heap + Quickselect (Hoare's selection algorithm)
+        
+        freq = collections.Counter(nums)
+        # result = sorted(freq, key=lambda num: -freq[num])
+        # return result[:k]
+        
+        return [x[0] for x in freq.most_common()[:k]]
+        # c = Counter(nums)
+        # return [x[0] for x in c.most_common()[:k]]    
