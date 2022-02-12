e87ac4f84a0524d00128d0a73ca9c0033f343b40
Jing <jyang.ustc@gmail.com>
2022-02-11 18:29:27 -0600

    Time: 62 ms (15.47%), Space: 13.8 MB (97.56%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.py
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -14,3 +14,22 @@ class Solution:
                 l = 1
                 prev = char
         return maxlen            
+    
+#         # Time Complexity: O(n) - solved by keeping track of last visited character and 2 counters (1 global, 1 local)
+#         # Space Complexity: O(1)
+
+#         prev_char = None
+#         max_power = 0
+#         local_power = 0
+        
+#         for curr_char in s:
+#             if curr_char != prev_char:
+#                 local_power = 1
+#             else: 
+#                 local_power += 1 
+            
+#             max_power = max(max_power, local_power)
+            
+#             prev_char = curr_char
+        
+#         return max_power
