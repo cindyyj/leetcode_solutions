e937bb6267f672d9d211ba0b19a93d28fbd7bde2
Jing <jyang.ustc@gmail.com>
2022-02-12 13:09:09 -0600

    Time: 408 ms (44.81%), Space: 17.2 MB (87.20%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,17 +1,54 @@
-class MyHashMap:
 
+class MyHashMap:
+    # Hash + chaining
     def __init__(self):
-        N = 1000001
-        self.data = [-1] * N
+        self.buckets =  1000
+        # The alternative, [{}] * n creates a list of length n with only one dictionary, referenced n times. 
+        # This leads to nasty surprises when adding keys to the dictionary.
+        self.data = [{} for _ in range(self.buckets) ]
+        
+    def hash(self, key):
+        return key % self.buckets
         
     def put(self, key: int, value: int) -> None:
-        self.data[key] = value
+        hashkey = self.hash(key)
+        # update if exist
+        if key in self.data[hashkey]:
+            self.data[hashkey][key] = value
+        
+        # no add(), append(), or insert() method you can use to add an item to a dictionary in Python
+        self.data[hashkey][key] = value
         
     def get(self, key: int) -> int:
-        return self.data[key] if self.data[key] != -1 else -1 
-
+        hashkey = self.hash(key)
+        # if not exist
+        if key not in self.data[hashkey]:
+            return -1 
+        return self.data[hashkey][key]
+        
     def remove(self, key: int) -> None:
-        self.data[key] = -1
+        hashkey = self.hash(key)
+        # if not exist
+        if key not in self.data[hashkey]:
+            return -1 
+        del self.data[hashkey][key]
+        
+        
+
+# class MyHashMap:
+#     # Brute Force
+#     def __init__(self):
+#         N = 1000001
+#         self.data = [-1] * N
+        
+#     def put(self, key: int, value: int) -> None:
+#         self.data[key] = value
+        
+#     def get(self, key: int) -> int:
+#         return self.data[key] if self.data[key] != -1 else -1 
+
+#     def remove(self, key: int) -> None:
+#         self.data[key] = -1
 
 
 # Your MyHashMap object will be instantiated and called as such:
