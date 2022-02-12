c30dcec31fbeea03aa7198467bff920a7660eb23
Jing <jyang.ustc@gmail.com>
2022-02-11 18:22:39 -0600

    Time: 394 ms (56.89%), Space: 14.4 MB (65.68%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -3,14 +3,13 @@ class Solution:
         # # fancy one liner solution as shared by Stefan Pochmann.
         # return max(list(map(len, ''.join(map(str, nums)).split('0') )))
         
-        
         maxlen = l = 0
 
         for num in nums:
             if num == 1:
                 l += 1
                 maxlen = max(maxlen, l)
-            else:
+            else:           
                 l = 0
                 
         return maxlen
