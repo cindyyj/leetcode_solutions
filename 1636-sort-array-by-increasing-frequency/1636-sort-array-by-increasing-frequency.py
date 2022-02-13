5b44b0bf220db8cd129226a662a99a6451f890b3
Jing <jyang.ustc@gmail.com>
2022-02-12 19:58:47 -0600

    Time: 51 ms (82.27%), Space: 13.8 MB (91.13%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,15 +1,27 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        # # @v_paliy note that:
-        # # "count[x] sorts by frequency, and if two values are equal, it will sort the keys in decreasing order."
-        # counts = Counter(nums)
-        # return sorted(nums, key=lambda x: (counts[x], -x))
         
-        cnt = Counter(nums).most_common()
-        cnt.sort(key = lambda x: (x[1], -x[0]))
+        cnt = Counter(nums).items()
+        cnt = sorted(cnt, key=lambda x: (x[1], -x[0]))
         ans = []
+        
         for num, freq in cnt:
-            ans.extend([num]*freq)
-            
+            ans.extend([num] * freq)
+        
         return ans
+ 
+#     # --------------------------- METHOD 1 ---------------------------        
+#         # @v_paliy note that:
+#         # "count[x] sorts by frequency, and if two values are equal, it will sort the keys in decreasing order."
+#         counts = Counter(nums)
+#         return sorted(nums, key=lambda x: (counts[x], -x))
+
+#     # --------------------------- METHOD 2 ---------------------------        
+#         cnt = Counter(nums).most_common()
+#         cnt.sort(key = lambda x: (x[1], -x[0]))
+#         ans = []
+#         for num, freq in cnt:
+#             ans.extend([num]*freq)
+            
+#         return ans
         
