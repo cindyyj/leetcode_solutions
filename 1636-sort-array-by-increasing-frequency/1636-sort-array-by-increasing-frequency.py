f2570e8a117ff3f0db7d40b1f542e6306e89f192
Jing <jyang.ustc@gmail.com>
2022-02-12 19:47:38 -0600

    Time: 52 ms (81.82%), Space: 13.9 MB (76.00%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,8 +1,15 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        # @v_paliy note that:
-        # "count[x] sorts by frequency, and if two values are equal, it will sort the keys in decreasing order."
-        counts = Counter(nums)
-        return sorted(nums, key=lambda x: (counts[x], -x))
+        # # @v_paliy note that:
+        # # "count[x] sorts by frequency, and if two values are equal, it will sort the keys in decreasing order."
+        # counts = Counter(nums)
+        # return sorted(nums, key=lambda x: (counts[x], -x))
         
+        cnt = Counter(nums).most_common()
+        cnt.sort(key = lambda x: (x[1], -x[0]))
+        ans = []
+        for num, freq in cnt:
+            ans.extend([num]*freq)
+            
+        return ans
         
