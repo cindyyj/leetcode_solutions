ed99394b7c8c1dbd60b39fedda17b581f5f72803
Jing <jyang.ustc@gmail.com>
2022-02-13 10:32:27 -0600

    Time: 55 ms (29.27%), Space: 13.8 MB (91.96%) - LeetHub

--- 760-find-anagram-mappings/760-find-anagram-mappings.py
+++ 760-find-anagram-mappings/760-find-anagram-mappings.py
@@ -1,7 +1,14 @@
 class Solution:
     def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
+        # handle duplicate
+        # replace the Hashmap <int, int> to Hashmap <int, Hashset> and pop the elements on the second loop.
+        d = collections.defaultdict(list)
+        for i, num in enumerate(nums2):
+            d[num].append(i)
+        return [d[num].pop() for num in nums1]    
+        
+        # won't work for duplicates!!!
         if sorted(nums1) != sorted(nums2):
             return False
-        
         return [nums2.index(num) for num in nums1]
