d70ae890a66ee46545316fec3c3f87ff2c1954f7
Jing <jyang.ustc@gmail.com>
2022-02-12 18:18:26 -0600

    Time: 52 ms (80.42%), Space: 13.9 MB (90.98%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,18 +1,17 @@
 from collections import Counter
 from functools import reduce
 from operator import and_
-from collections import Counter
 
-class Solution:
-    def commonChars(self, A: List[str]) -> List[str]:
-        return list(reduce(and_, map(Counter, A)).elements())
-    
-    
 # class Solution:
-#     def commonChars(self, words: List[str]) -> List[str]:
+#     def commonChars(self, A: List[str]) -> List[str]:
+#         return list(reduce(and_, map(Counter, A)).elements())
+    
+    
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
         
-#         cnt = Counter(words[0])
-#         for word in words[1:]:
-#             cnt &= Counter(word)
-#         return list(cnt.elements())
+        cnt = Counter(words[0])
+        for word in words[1:]:
+            cnt &= Counter(word)
+        return list(cnt.elements())
         
