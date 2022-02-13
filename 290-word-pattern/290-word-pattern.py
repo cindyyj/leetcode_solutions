85e20024a3d8288ba8518f3e13901cbd16745bd5
Jing <jyang.ustc@gmail.com>
2022-02-12 18:37:21 -0600

    Time: 44 ms (49.88%), Space: 13.9 MB (91.14%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, p: str, s: str) -> bool: 
+        # p for pattern
+        
+        s = s.split(' ')
+        
+        p2s = {}
+        s2p = {}
+        
+        if len(p) != len(s):
+            return False
+        
+        # for char, word in zip(p, s):
+        for i in range(len(s)):
+            
+            if s[i] not in s2p and p[i] not in p2s:
+                p2s[p[i]] = s[i]
+                s2p[s[i]] = p[i]
+            
+            elif p2s.get(p[i]) != s[i] or s2p.get(s[i]) != p[i]:
+                return False
+            
+        return True
