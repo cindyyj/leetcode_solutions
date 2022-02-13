49d875e8adf50473b7648d59120a290a6e822c6a
Jing <jyang.ustc@gmail.com>
2022-02-12 18:43:15 -0600

    Time: 32 ms (83.35%), Space: 13.9 MB (91.14%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -1,23 +1,31 @@
 class Solution:
     def wordPattern(self, p: str, s: str) -> bool: 
-        # p for pattern
         
+        # zip
         s = s.split(' ')
-        
-        p2s = {}
-        s2p = {}
-        
         if len(p) != len(s):
             return False
+        return len(set(p)) == len(set(s)) == len(set(zip(p,s)))
         
-        # for char, word in zip(p, s):
-        for i in range(len(s)):
+        
+#         # 2 hashmap
+#         # p for pattern
+#         s = s.split(' ')
+        
+#         p2s = {}
+#         s2p = {}
+        
+#         if len(p) != len(s):
+#             return False
+        
+#         # for char, word in zip(p, s):
+#         for i in range(len(s)):
             
-            if s[i] not in s2p and p[i] not in p2s:
-                p2s[p[i]] = s[i]
-                s2p[s[i]] = p[i]
+#             if s[i] not in s2p and p[i] not in p2s:
+#                 p2s[p[i]] = s[i]
+#                 s2p[s[i]] = p[i]
             
-            elif p2s.get(p[i]) != s[i] or s2p.get(s[i]) != p[i]:
-                return False
+#             elif p2s.get(p[i]) != s[i] or s2p.get(s[i]) != p[i]:
+#                 return False
             
-        return True
+#         return True
