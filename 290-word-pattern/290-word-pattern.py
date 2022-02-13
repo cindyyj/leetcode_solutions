519a592336991597ec861aadb6b5bcb9787ab763
Jing <jyang.ustc@gmail.com>
2022-02-12 18:45:00 -0600

    Time: 56 ms (17.42%), Space: 13.8 MB (99.88%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -1,33 +1,35 @@
 class Solution:
     def wordPattern(self, p: str, s: str) -> bool: 
         
-        # # zip
-        # s = s.split(' ')
-        # if len(p) != len(s):
-        #     return False
-        # return len(set(p)) == len(set(s)) == len(set(zip(p,s)))
-        
-        
-        # 2 hashmap
-        # p for pattern
+        # zip
         s = s.split(' ')
         
-        p2s = {}
-        s2p = {}
-        
+        # import to ensure the string length are equal, otherwise fail for "aba" "cat cat cat dog"
         if len(p) != len(s):
             return False
-        if len(set(p)) != len(set(s)): 
-            return False # for the case w = ['dog', 'cat'] and p = 'aa'
+        return len(set(p)) == len(set(s)) == len(set(zip(p,s)))  
         
-        # for char, word in zip(p, s):
-        for i in range(len(s)):
+        
+#         # 2 hashmap
+#         # p for pattern
+#         s = s.split(' ')
+        
+#         p2s = {}
+#         s2p = {}
+        
+#         if len(p) != len(s):
+#             return False
+#         if len(set(p)) != len(set(s)): 
+#             return False # for the case w = ['dog', 'cat'] and p = 'aa'
+        
+#         # for char, word in zip(p, s):
+#         for i in range(len(s)):
             
-            if s[i] not in s2p and p[i] not in p2s:
-                p2s[p[i]] = s[i]
-                s2p[s[i]] = p[i]
+#             if s[i] not in s2p and p[i] not in p2s:
+#                 p2s[p[i]] = s[i]
+#                 s2p[s[i]] = p[i]
             
-            elif p2s.get(p[i]) != s[i] or s2p.get(s[i]) != p[i]:
-                return False
+#             elif p2s.get(p[i]) != s[i] or s2p.get(s[i]) != p[i]:
+#                 return False
             
-        return True
+#         return True
