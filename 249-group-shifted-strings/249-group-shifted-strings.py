1213199d614affe84e787c8025b5e23b49620f09
Jing <jyang.ustc@gmail.com>
2022-02-13 10:23:12 -0600

    Time: 47 ms (55.57%), Space: 14 MB (66.91%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,8 +1,10 @@
 class Solution:
     def groupStrings(self, strs: List[str]) -> List[List[str]]:
         
+        # time: O(N*k), N = len(strs), k = max(len(s))
+        # space: O(N*k)
         groups = defaultdict(list)
         for s in strs:
-            key = tuple((ord(c) - ord(s[0])) % 26 for c in s) 
+            key = tuple((ord(c) - ord(s[0])) % 26 for c in s)   # (a - z) -25 % 26 = 1 circular shift
             groups[key].append(s)
         return groups.values()
