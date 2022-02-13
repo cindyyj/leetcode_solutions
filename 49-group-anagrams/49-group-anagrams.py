c48015e69a95730c8fe1425ca8254f5a820052d1
Jing <jyang.ustc@gmail.com>
2022-02-13 09:48:01 -0600

    Time: 126 ms (59.61%), Space: 18.1 MB (45.54%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        # Categorize by Sorted String
-        # time: O(N*KlogK), k max length of string in strs. N, number of strings in strs
-        # space: O(N * k)
-        ans = defaultdict(list)
+        # # Categorize by Sorted String
+        # # time: O(N*KlogK), k max length of string in strs. N, number of strings in strs
+        # # space: O(N * k), total info stored in ans
+        # ans = defaultdict(list)
+        # for s in strs:
+        #     key = tuple(sorted(s))
+        #     ans[key].append(s)
+        # return ans.values()
+        
+        ans = {}
         for s in strs:
             key = tuple(sorted(s))
-            ans[key].append(s)
+            ans[key] = ans.get(key, []) + [s]
         return ans.values()
-        
