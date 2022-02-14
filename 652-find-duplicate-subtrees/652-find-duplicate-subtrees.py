10f7c759ba6b05a045d2e8c0693e6bac2dfdd1be
Jing <jyang.ustc@gmail.com>
2022-02-14 09:27:09 -0600

    Time: 91 ms (56.30%), Space: 23.4 MB (55.14%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+For those who're confused why pre and post order works but not in order. I think this clears my confusion: https://leetcode.com/problems/find-duplicate-subtrees/discuss/106011/Java-Concise-Postorder-Traversal-Solution/108467. 
+To be brief, it's because inorder can create same serialization for symmetric trees.
+"""
+
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        # Concept - We can use a hashmap to store the all the trees and their roots
+        # Also advantage of using hashmap is you can easily find for the repeated
+        # or duplicate trees
+        hashmap = {}
+        res = []
+        self.solve(root, hashmap)
+
+        # Once the hashmap is built, we can just travserse through the values 
+        # and find out the values that are greater than 1 are repeated
+        for val, node in hashmap.values():
+            if val > 1:
+                res.append(node)
+        return res
+    
+    def solve(self, root, hashmap):
+        if not root:
+            return 'X'
+        
+        left = self.solve(root.left, hashmap)
+        right = self.solve(root.right, hashmap)
+        
+        # Pre-order tree representation for storing the tree
+        temp = str(root.val) + ' ' + left + ' ' + right
+        
+        # Check if it is already in hashmap
+        if temp not in hashmap:
+            hashmap[temp] = [1, root]
+        
+        else:
+            hashmap[temp][0] += 1
+            
+        return temp        
