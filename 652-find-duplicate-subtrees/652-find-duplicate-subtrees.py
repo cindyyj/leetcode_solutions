531e3915846c64da3e1c32df33e9828f4eec73e3
Jing <jyang.ustc@gmail.com>
2022-02-14 09:35:06 -0600

    Time: 77 ms (67.84%), Space: 28.5 MB (17.17%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -11,36 +11,52 @@ To be brief, it's because inorder can create same serialization for symmetric tr
 """
 
 class Solution:
-    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        # Concept - We can use a hashmap to store the all the trees and their roots
-        # Also advantage of using hashmap is you can easily find for the repeated
-        # or duplicate trees
-        hashmap = {}
-        res = []
-        self.solve(root, hashmap)
-
-        # Once the hashmap is built, we can just travserse through the values 
-        # and find out the values that are greater than 1 are repeated
-        for val, node in hashmap.values():
-            if val > 1:
-                res.append(node)
-        return res
     
-    def solve(self, root, hashmap):
-        if not root:
-            return 'X'
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        def dfs(root):
+            if not root:
+                return "null"
+            key = str(root.val) + ' ' + dfs(root.left) + ' ' + dfs(root.right)
+            hashmap[key].append(root)
+            return key
         
-        left = self.solve(root.left, hashmap)
-        right = self.solve(root.right, hashmap)
-        
-        # Pre-order tree representation for storing the tree
-        temp = str(root.val) + ' ' + left + ' ' + right
-        
-        # Check if it is already in hashmap
-        if temp not in hashmap:
-            hashmap[temp] = [1, root]
-        
-        else:
-            hashmap[temp][0] += 1
+        hashmap = defaultdict(list)
+        dfs(root)
+        return [hashmap[i][0] for i in hashmap if len(hashmap[i]) > 1 ]
             
-        return temp        
+
+
+    
+#     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+#         # Concept - We can use a hashmap to store the all the trees and their roots
+#         # Also advantage of using hashmap is you can easily find for the repeated
+#         # or duplicate trees
+#         hashmap = {}
+#         res = []
+#         self.solve(root, hashmap)
+
+#         # Once the hashmap is built, we can just travserse through the values 
+#         # and find out the values that are greater than 1 are repeated
+#         for val, node in hashmap.values():
+#             if val > 1:
+#                 res.append(node)
+#         return res
+    
+#     def solve(self, root, hashmap):
+#         if not root:
+#             return 'X'
+        
+#         left = self.solve(root.left, hashmap)
+#         right = self.solve(root.right, hashmap)
+        
+#         # Pre-order tree representation for storing the tree
+#         temp = str(root.val) + ' ' + left + ' ' + right
+        
+#         # Check if it is already in hashmap
+#         if temp not in hashmap:
+#             hashmap[temp] = [1, root]
+        
+#         else:
+#             hashmap[temp][0] += 1
+            
+#         return temp
