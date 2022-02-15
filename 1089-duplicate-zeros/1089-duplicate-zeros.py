b402b0e3291c69f309c65b6081479d146a2e9b2a
Jing <jyang.ustc@gmail.com>
2022-02-15 16:15:44 -0600

    Time: 82 ms (65.02%), Space: 14.9 MB (64.38%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -3,17 +3,18 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        
         zeros = arr.count(0)
         n = len(arr)
+        
         for i in range(n-1, -1, -1):
             if i + zeros < n:
-                arr[i + zeros] = arr[i]
+                arr[i+zeros] = arr[i]
             if arr[i] == 0:
                 zeros -= 1
                 if i + zeros < n:
                     arr[i + zeros] = 0
-                    
+        
+        
 """
 challenge: change in place 
 Start from the back and adjust items to correct locations. If item is zero then duplicate it.
