173ac55c6cf8b64cd104d509b5296aa1098562d6
Jing <jyang.ustc@gmail.com>
2022-02-15 14:30:35 -0600

    Time: 85 ms, Memory: 13.9 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -1,19 +1,4 @@
-from math import log10, floor
-
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
+        return sum([len(str(num)) % 2 == 0 for num in nums])
         
-        """
-        Very common formula for return the number of digits
-        in a positive integer. Since we're guranteed there
-        are only positive numbers in the array, this works well. If
-        negative numbers were allowed, then use log10(abs(num)) instead. 
-        """
-        count = 0
-        
-        for num in nums:
-            digit_count = floor(log10(num))+1
-            if digit_count % 2 == 0:
-                count += 1
-        
-        return count
