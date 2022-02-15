f64aa034c50ef2d1444a610c9f40098add5b02a9
Jing <jyang.ustc@gmail.com>
2022-02-14 19:33:17 -0600

    Time: 42 ms (48.28%), Space: 13.9 MB (86.31%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -1,8 +1,25 @@
-# class Solution:
-#     def balancedStringSplit(self, s: str) -> int:
-        
-from itertools import accumulate
-
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        return list(accumulate(1 if c == "R" else -1 for c in s)).count(0)        
+        ans = cnt = 0
+        
+        for char in s:
+            cnt += 1 if char == 'R' else -1
+            if cnt == 0:
+                ans += 1
+        
+        return ans
+            
+        
+        
+        
+# from itertools import accumulate
+
+# class Solution:
+#     def balancedStringSplit(self, s: str) -> int:
+#         return list(accumulate(1 if c == "R" else -1 for c in s)).count(0)        
+
+"""
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+"""
