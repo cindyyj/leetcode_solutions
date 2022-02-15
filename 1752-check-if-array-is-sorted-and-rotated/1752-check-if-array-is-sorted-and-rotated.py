b6799129b465d2453d3e2246ad58df551df24a20
Jing <jyang.ustc@gmail.com>
2022-02-15 17:33:13 -0600

    Time: 36 ms (81.17%), Space: 13.9 MB (88.64%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,5 +1,6 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        
+        # https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/discuss/1053508/JavaC%2B%2BPython-Easy-and-Concise
+        # Very clever to use the fact that nums[0] < nums[-1] is also considered in this
         return sum( nums[i - 1] > nums[i] for i in range(len(nums)) ) <= 1
         
