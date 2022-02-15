f90bbd862261b47661acbc80c2375afc5f9a1b5a
Jing <jyang.ustc@gmail.com>
2022-02-15 14:26:10 -0600

    Time: 221 ms (60.29%), Space: 14.7 MB (75.17%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,4 +1,6 @@
 from collections import Counter
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
+        # c1 - c2 subtract (keeping only positive counts)
+        # min steps to make t as s, change t
         return sum((Counter(s) - Counter(t)).values())
