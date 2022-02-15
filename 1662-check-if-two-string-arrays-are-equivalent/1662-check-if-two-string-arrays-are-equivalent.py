99f6a164d2251a982287f61090d44c1f83752690
Jing <jyang.ustc@gmail.com>
2022-02-15 16:31:13 -0600

    Time: 36 ms (68.99%), Space: 14 MB (75.41%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -2,4 +2,23 @@ class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         
         return ''.join(word1) == ''.join(word2)
-        
+
+    
+    
+class Solution:
+    # Understanding generators and yield statement
+    # https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/discuss/1007878/Python-Understanding-generators-and-yield-statement
+    
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        for c1, c2 in zip(self.gen(word1), self.gen(word2)):
+            if c1 != c2:
+                return False
+        return True
+        # return all(c1 == c2 for c1, c2 in zip(self.gen(word1), self.gen(word2))
+
+    def gen(self, word: List[str]):
+        for s in word:
+            for c in s:
+                yield c
+        # Ensure False when len(word1) != len(word2) 
+        yield None
