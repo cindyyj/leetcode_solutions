6fb98b06ffe24ed8e21c67c40983ba26722fb834
Jing <jyang.ustc@gmail.com>
2022-02-14 18:02:43 -0600

    Time: 420 ms (16.31%), Space: 27.4 MB (46.40%) - LeetHub

--- 288-unique-word-abbreviation/288-unique-word-abbreviation.py
+++ 288-unique-word-abbreviation/288-unique-word-abbreviation.py
@@ -1,36 +1,20 @@
 class ValidWordAbbr:
 
-    def __init__(self, dictionary):
-        self.dic = collections.defaultdict(set)
-        for s in dictionary:
-            val = s
-            if len(s) > 2:
-                s = s[0]+str(len(s)-2)+s[-1]
-            self.dic[s].add(val)
+    def __init__(self, dictionary: List[str]):
+        self.d = collections.defaultdict(set)
+        for word in dictionary:
+            abbr = self.abbre(word)
+            self.d[abbr].add(word)
+
+    def abbre(self, word):
+        return word[0] + str(len(word) - 2) + word[-1] if len(word) > 2 else word
 
     def isUnique(self, word):
-        val = word 
-        if len(word) > 2:
-            word = word[0]+str(len(word)-2)+word[-1]
-        # if word abbreviation not in the dictionary, or word itself in the dictionary (word itself may 
-        # appear multiple times in the dictionary, so it's better using set instead of list)
-        return len(self.dic[word]) == 0 or (len(self.dic[word]) == 1 and val == list(self.dic[word])[0])
-
-
-
-#     def __init__(self, dictionary: List[str]):
-#         self.d = collections.defaultdict(set)
-#         for word in dictionary:
-#             abbr = self.abbre(word)
-#             self.d[abbr].add(word)
-            
+        abbr = self.abbre(word) 
+        if abbr not in self.d:
+            return True
+        return len(self.d[abbr]) == 1 and word == list(self.d[abbr])[0]
     
-#     def abbre(self, word):
-#         return word[0] + str(len(word) - 2) + word[-1] if len(word) > 2 else word
-    
-#     def isUnique(self, word):
-#         abbr = self.abbre(word) 
-#         return len(self.d[abbr]) == 0 or (len(self.d[abbr]) == 1 and set(word) == self.d[abbr])
 
 """
 https://leetcode.com/problems/unique-word-abbreviation/discuss/149925/Python-or-tm
