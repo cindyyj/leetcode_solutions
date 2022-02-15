d016baa973cab56ac366083ec6a4e07aa6c86116
Jing <jyang.ustc@gmail.com>
2022-02-14 18:51:37 -0600

    Time: 52 ms, Memory: 14.4 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        # two pointer
+        # time O(len(s))
+        # space O(1)
+        
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].lower().isalnum():
+                l += 1
+            elif not s[r].lower().isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l += 1
+                    r -= 1
+        
+        return True
+            
+        
+        
+#         # time: O(len(s))
+#         # space: O(len(s))
+        
+#         cleaned_s = "".join([char for char in s.lower() if char.isalnum()])
+#         return cleaned_s == cleaned_s[::-1]
+        
