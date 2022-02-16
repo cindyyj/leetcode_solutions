fe93bdb4fb242240d65d099ccc27537325fcd2be
Jing <jyang.ustc@gmail.com>
2022-02-16 11:44:10 -0600

    Time: 188 ms (64.20%), Space: 24.4 MB (20.41%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,8 +1,11 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         
-        # XOR
-        return reduce(xor, nums)
+        # # XOR
+        # return reduce(xor, nums)
+        
+        # set
+        return 2*sum(set(nums)) - sum(nums)
         
         # # Brute Force
         # for i in range(0, len(nums) - 2, 2):
