2244c9c7ea9efc8159b858e79075d88f0107c20c
Jing <jyang.ustc@gmail.com>
2022-02-16 12:55:36 -0600

    Time: 305 ms (64.25%), Space: 18.5 MB (85.49%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -7,28 +7,43 @@
 #         return random.choices(range(len(self.w)), weights=self.w)[0]
 
 import random
+from itertools import accumulate
 
 class Solution:
+
     def __init__(self, w: List[int]):
-        """
-        :type w: List[int]
-        """
-        self.prefix_sums = []
-        prefix_sum = 0
-        for weight in w:
-            prefix_sum += weight
-            self.prefix_sums.append(prefix_sum)
-        self.total_sum = prefix_sum
+        self.pre = list(accumulate(w))
+        self.total = self.pre[-1]
 
     def pickIndex(self) -> int:
-        """
-        :rtype: int
-        """
-        target = self.total_sum * random.random()
-        # run a linear search to find the target zone
-        for i, prefix_sum in enumerate(self.prefix_sums):
-            if target < prefix_sum:
-                return i
+        x = random.randint(1, self.total)
+        return bisect_left(self.pre, x)
+
+        # 作者：LeetCode-Solution
+        # 链接：https://leetcode-cn.com/problems/random-pick-with-weight/solution/an-quan-zhong-sui-ji-xuan-ze-by-leetcode-h13t/
+
+
+# class Solution:
+#     def __init__(self, w: List[int]):
+#         """
+#         :type w: List[int]
+#         """
+#         self.prefix_sums = []
+#         prefix_sum = 0
+#         for weight in w:
+#             prefix_sum += weight
+#             self.prefix_sums.append(prefix_sum)
+#         self.total_sum = prefix_sum
+
+#     def pickIndex(self) -> int:
+#         """
+#         :rtype: int
+#         """
+#         target = self.total_sum * random.random()
+#         # run a linear search to find the target zone
+#         for i, prefix_sum in enumerate(self.prefix_sums):
+#             if target < prefix_sum:
+#                 return i
         
 """
 sampling with weight
@@ -36,7 +51,10 @@ sampling important step for building decision tree model
 
 - random.random(): Return random number between 0.0 and 1.0:
 - random.choices(): returns a list with the randomly selected element from the specified sequence. Can weigh the possibility of each result with the weights parameter or the cum_weights parameter.
-- 
+- Random randint(start, stop): Return a integer between start and stop (both included)
+- same as randrange(start, stop+1)
+
+
 
 
 """
