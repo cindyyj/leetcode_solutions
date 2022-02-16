55e702849366cc7484d152c24fa5440d82853c69
Jing <jyang.ustc@gmail.com>
2022-02-16 12:58:13 -0600

    Time: 487 ms (29.25%), Space: 18.7 MB (73.83%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -16,8 +16,19 @@ class Solution:
         self.total = self.pre[-1]
 
     def pickIndex(self) -> int:
-        x = random.randint(1, self.total)
-        return bisect_left(self.pre, x)
+        target = random.randint(1, self.total)
+        
+        l, r = 0, len(self.pre) - 1
+        while l <= r:
+            mid = l + ((r - l) >> 1)
+            if target > self.pre[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+            
+        return l
+        
+        # return bisect_left(self.pre, x)
 
         # 作者：LeetCode-Solution
         # 链接：https://leetcode-cn.com/problems/random-pick-with-weight/solution/an-quan-zhong-sui-ji-xuan-ze-by-leetcode-h13t/
