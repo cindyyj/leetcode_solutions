2653d39213715836546efade733c780adf282cb2
Jing <jyang.ustc@gmail.com>
2022-02-16 11:49:51 -0600

    Time: 252 ms (41.94%), Space: 23.8 MB (81.02%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,11 +1,27 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         
+        # https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/1587270/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-%2B-Hashmap-%2B-XOR-%2B-Linear-%2B2-Binary-Search
+        # binary search
+        
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + ((r-l) >> 1)
+            mid -= mid & 1
+            if nums[mid] == nums[mid + 1]:
+                l = mid + 2
+            else:
+                r = mid
+        
+        return nums[l]
+            
+        
+        
         # # XOR
         # return reduce(xor, nums)
         
-        # set
-        return 2*sum(set(nums)) - sum(nums)
+        # # set
+        # return 2*sum(set(nums)) - sum(nums)
         
         # # Brute Force
         # for i in range(0, len(nums) - 2, 2):
