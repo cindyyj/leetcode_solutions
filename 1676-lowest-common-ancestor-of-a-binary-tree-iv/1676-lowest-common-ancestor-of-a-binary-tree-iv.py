3cd1cdcd6b870b2a24345159ca0d3ce0c49cd969
Jing <jyang.ustc@gmail.com>
2022-02-16 15:30:28 -0600

    Time: 221 ms (34.41%), Space: 19.8 MB (63.67%) - LeetHub

--- 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.py
+++ 1676-lowest-common-ancestor-of-a-binary-tree-iv/1676-lowest-common-ancestor-of-a-binary-tree-iv.py
@@ -11,6 +11,10 @@ class Solution:
         if not root:
             return root
         
+        # edge case
+        if len(nodes)==1:
+            return nodes[0]
+        
         nodes = set(nodes)
         if root in nodes:
             return root
@@ -20,4 +24,7 @@ class Solution:
         
         if left and right:
             return root
-        return left or right      
+        return left or right
+    
+    # same logic as L235, if some nodes in left subtree, others in right subtree, LCA must be root   
+    # else all nodes in one side of the tree, so the LCA is what ever returned from left and right that is not None
