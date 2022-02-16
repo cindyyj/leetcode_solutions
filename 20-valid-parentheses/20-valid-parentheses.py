a5f50d0eef112e61f006cecba99f8caf3644c8cb
Jing <jyang.ustc@gmail.com>
2022-02-16 16:34:09 -0600

    Time: 20 ms (99.62%), Space: 13.9 MB (75.48%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        
+        pairs = {
+            ")" : "(", 
+            "}" : "{", 
+            "]" : "[", 
+        }
+        
+        stack = list()
+                  
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            elif char in "]})":
+                # 右括号比左括号先出现, 不能闭合 not stack 
+                # 遇到右括号, 必然要与上一个左括号闭合, 如果不匹配就 False
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                else: 
+                    stack.pop()
+                    
+        # 正常闭合的情况下, 栈里面应该全都弹出去了, 所以应该是空的
+        return stack == []
