d3edf9e0be665dd2d897e8601898c15e6c0893b7
Jing <jyang.ustc@gmail.com>
2022-02-16 13:44:17 -0600

    Time: 355 ms (51.24%), Space: 18.7 MB (73.14%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -21,7 +21,7 @@ class Solution:
         l, r = 0, len(self.pre) - 1
         while l <= r:
             mid = l + ((r - l) >> 1)
-            if target > self.pre[mid]:
+            if self.pre[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
@@ -29,9 +29,6 @@ class Solution:
         return l
         
         # return bisect_left(self.pre, target)
-
-        # 作者：LeetCode-Solution
-        # 链接：https://leetcode-cn.com/problems/random-pick-with-weight/solution/an-quan-zhong-sui-ji-xuan-ze-by-leetcode-h13t/
 
 
 # class Solution:
@@ -65,8 +62,38 @@ sampling important step for building decision tree model
 - Random randint(start, stop): Return a integer between start and stop (both included)
 - same as randrange(start, stop+1)
 
+def bisect_right(a, x, lo=0, hi=None):
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if a[mid] <= x:    # <--- less than or equal to
+            lo = mid+1
+        else:
+            hi = mid
+    return lo
 
 
+def bisect_left(a, x, lo=0, hi=None):
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x:    # <--- less than
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
+
+
+pre[i]−w[i]+1≤x≤pre[i]
+
+的 ii 并将其作为答案返回。由于 \textit{pre}[i]pre[i] 是单调递增的，因此我们可以使用二分查找在 O(\log n)O(logn) 的时间内快速找到 ii，即找出最小的满足 x \leq \textit{pre}[i]x≤pre[i] 的下标 ii。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/random-pick-with-weight/solution/an-quan-zhong-sui-ji-xuan-ze-by-leetcode-h13t/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 """
 
