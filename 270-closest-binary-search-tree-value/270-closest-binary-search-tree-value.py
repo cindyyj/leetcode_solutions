fba3544483e46de4f2f48675ec95bf7e8d6e0d6b
Jing <jyang.ustc@gmail.com>
2022-02-16 10:26:15 -0600

    Time: 67 ms (30.04%), Space: 16.1 MB (53.12%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.py
@@ -6,8 +6,23 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        def inorder(r):
-            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
         
-        nums = inorder(root)
-        return min(nums, key = lambda x: abs(target - x))
+        cur = root
+        closest = cur.val
+
+        while cur:
+            closest = min(cur.val, closest, key = lambda x: abs(target-x))
+            cur = cur.left if target < cur.val else cur.right
+            # if cur.val > target:
+            #     cur = cur.right
+            # else:
+            #     cur = cur.left
+        
+        return closest
+        
+        
+#         def inorder(r):
+#             return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        
+#         nums = inorder(root)
+#         return min(nums, key = lambda x: abs(target - x))
