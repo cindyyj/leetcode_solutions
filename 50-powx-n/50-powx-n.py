a06b9950a42201994ee0648b72dba28b57ee71e6
Jing <jyang.ustc@gmail.com>
2022-02-17 10:07:07 -0600

    Time: 68 ms (5.04%), Space: 14 MB (65.35%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,25 +1,39 @@
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        # pos
-        # n odd, x**n = x*(x**2)**(n-1)/2
-        # n even, x**n = (x**2)**n/2
-        # x is called the base, and the number n is called the exponent.
-        # 0
-        # neg
-        
-        # recursive
-        # https://leetcode.com/problems/powx-n/discuss/19560/Shortest-Python-Guaranteed
-        
-        # 时间复杂度：O(logn)，即为递归的层数。
-        # 空间复杂度：O(\log n)O(logn)，即为递归的层数。这是由于递归的函数调用会使用栈空间。
+from functools import lru_cache
 
+class Solution:
+    
+    @lru_cache()
+    def myPow(self, x: float, n: int) -> float:
+        
+        # recursive 
         if n == 0:
-            return 1
-        
+            return 1        
         if n < 0:
-            return 1/self.myPow(x, -n)
-        
+            return 1/self.myPow(x, -n)        
         if n % 2:
-            return x * self.myPow(x, (n-1))
-        
+            return x * self.myPow(x, (n-1))        
         return self.myPow(x*x, n/2)
+
+"""
+> Base Case:  b == 0
+> Function: F(a ^ b) = F( (a*a) ^ b // 2) 
+
+return pow(x, n)
+# pos
+# n odd, x**n = x*(x**2)**(n-1)/2
+# n even, x**n = (x**2)**n/2
+# x is called the base, and the number n is called the exponent.
+# 0
+# neg
+
+# recursive
+# https://leetcode.com/problems/powx-n/discuss/19560/Shortest-Python-Guaranteed
+
+# 时间复杂度：O(logn)，即为递归的层数。
+# 空间复杂度：O(logn)，即为递归的层数。这是由于递归的函数调用会使用栈空间。
+Time complexity : O(logn). Each time we apply the formula , n is reduced by half. Thus we need at most O(logn) computations to get the result. - similar to binary search
+Space complexity : O(logn). 
+
+python中lru_cache的基本原理是构建一个字典，字典的key为调用参数，value就是该参数的计算结果。
+
+"""
