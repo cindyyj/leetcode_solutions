67fb4c2b8c38eaa136b8f096887a2eb8a241845a
Jing <jyang.ustc@gmail.com>
2022-02-16 21:31:12 -0600

    Time: 32 ms (87.39%), Space: 13.9 MB (94.51%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,31 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-
-        # Initialize a stack
         stack = []
+        for p in path.split('/'):
+            if stack and p == '..':
+                stack.pop()
+            elif p not in " ..":
+                stack.append(p)
+        return '/' + '/'.join(stack)
 
-        # Split the input string on "/" as the delimiter
-        # and process each portion one by one
-        for portion in path.split("/"):
-
-            # If the current component is a "..", then
-            # we pop an entry from the stack if it's non-empty
-            if portion == "..":
-                if stack:
-                    stack.pop()
-            elif portion == "." or not portion:
-                # A no-op for a "." or an empty string
-                continue
-            else:
-                # Finally, a legitimate directory name, so we add it
-                # to our stack
-                stack.append(portion)
-
-        # Stich together all the directory names together
-        final_str = "/" + "/".join(stack)
-        return final_str
-    
-    
+# 作者：qingfengpython
+# 链接：https://leetcode-cn.com/problems/simplify-path/solution/71-jian-hua-lu-jing-biao-zhun-de-zhan-we-0ebf/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。   
 #         stack = []
 #         for p in path.split('/'):
 #             if stack and p == '..':
@@ -35,7 +21,7 @@ class Solution:
 #             else: # a directory name
 #                 stack.append(p)
                 
-#         return '/' + '/'.join(stack)
+# #         return '/' + '/'.join(stack)
         
         
         """
