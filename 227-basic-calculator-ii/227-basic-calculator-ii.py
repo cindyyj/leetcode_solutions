23b98687ce39f181aaade0a55725e19a93e58b4c
Jing <jyang.ustc@gmail.com>
2022-02-16 20:45:19 -0600

    Time: 178 ms (16.30%), Space: 15.7 MB (41.40%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,53 +1,27 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        pre_op = '+'
-        num = 0
-        for i, each in enumerate(s):
-            if each.isdigit():
-                num = 10 * num + int(each)
-            if i == len(s) - 1 or each in '+-*/':
-                if pre_op == '+':
-                    stack.append(num)
-                elif pre_op == '-':
-                    stack.append(-num)
-                elif pre_op == '*':
-                    stack.append(stack.pop() * num)
-                elif pre_op == '/':
-                    top = stack.pop()
-                    if top < 0:
-                        stack.append(int(top / num))
-                    else:
-                        stack.append(top // num)
-                pre_op = each
-                num = 0
-        return sum(stack)
 
-# 作者：fuxuemingzhu
-# 链接：https://leetcode-cn.com/problems/basic-calculator-ii/solution/xian-cheng-chu-zai-jia-jian-yong-zhan-ba-hplr/
-# 来源：力扣（LeetCode）
-# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-#         stack = list()
-#         num = 0
-#         pre = '+'
+        stack = list()
+        num = 0
+        pre = '+'
         
-#         for i, c in enumerate(s):
-#             if c.isnumeric():
-#                 num = num * 10 + int(c)
-#             if c in '+=*/' or i == len(s) - 1:
-#                 if pre == '+':
-#                     stack.append(num)
-#                 elif pre == '-':
-#                     stack.append(-num)
-#                 elif pre == '*':
-#                     stack.append(stack.pop() * num)
-#                 else:
-#                     stack.append(int( stack.pop() / num)) #「浮点除 / 」再取整
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+            if c in '+-*/' or i == len(s) - 1:
+                if pre == '+':
+                    stack.append(num)
+                elif pre == '-':
+                    stack.append(-num)
+                elif pre == '*':
+                    stack.append(stack.pop() * num)
+                else:
+                    stack.append(int( stack.pop() / num)) #「浮点除 / 」再取整
                 
-#                 pre = c
-#                 num = 0
+                pre = c
+                num = 0
                 
-#         return sum(stack)
+        return sum(stack)
 
                     
     """
