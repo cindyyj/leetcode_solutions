74de4926212b62fb218e6778e1f06b73c6e76544
Jing <jyang.ustc@gmail.com>
2022-02-17 08:56:15 -0600

    Time: 280 ms (9.43%), Space: 20.7 MB (47.80%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -1,3 +1,24 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        return sum(i < c for i, c in enumerate(citations[::-1]))
+        
+        # citations[i] >= n - i
+        # H-index is n - i (n - i) paper cited at least c times
+        
+        n = len(citations)
+        l, r = 0, n - 1
+        
+        while l <= r:
+            mid = l + ((r - l) >> 1)
+            
+            if citations[mid] == n - mid:
+                return n - mid
+            elif citations[mid] < n - mid:
+                l = mid + 1
+            else:
+                r = mid - 1
+            
+            # We didn't find an exact match, so there's exactly (n - left) papers that have citations
+            # greater than or equal to citations[left] and that is our answer            
+        return n - l
+        
+        # return sum(i < c for i, c in enumerate(citations[::-1]))
