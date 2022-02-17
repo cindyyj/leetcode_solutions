04262674b8c0dd96333574fd01b1c93652bf8762
Jing <jyang.ustc@gmail.com>
2022-02-17 11:32:50 -0600

    Time: 50 ms (50.03%), Space: 13.7 MB (99.95%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -1,5 +1,10 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        d = {char : i for i, char in enumerate(order)}        
-        words = [[d[c] for c in w] for w in words]
-        return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
+        # d = {char : i for i, char in enumerate(order)}        
+        # words = [[d[c] for c in w] for w in words]
+        # return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
+    
+        # one-line 
+        # https://leetcode-cn.com/problems/verifying-an-alien-dictionary/solution/python-yi-xing-by-wang-hao-bi0glutbvu/
+        return words == sorted(words,key=lambda s:''.join(['abcdefghijklmnopqrstuvwxyz'[order.index(i)] for i in s]))
+
