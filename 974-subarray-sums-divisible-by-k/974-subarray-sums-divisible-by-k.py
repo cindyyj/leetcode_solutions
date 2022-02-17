ff6a98ce31e24c711f5e35139dc163daa61fdedf
Jing <jyang.ustc@gmail.com>
2022-02-17 15:53:21 -0600

    Time: 344 ms (60.37%), Space: 18.8 MB (29.11%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
@@ -1,15 +1,19 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        # sum(nums[i:j]) % k == 0 for some i < j, then sum(nums[:j]) % k == sum(nums[:i]) % k. 
+        # So we just need to use a dictionary to keep track of sum(nums[:i]) % k and the corresponding index i
+        # 当前 presum 与 K的关系，余数是几，当被除数为负数时取模结果为负数，需要纠正
+        
         pres = list(accumulate(nums))
         d = Counter()
         d[0] = 1
         cnt = 0
         
         for pre in pres:
-            key = (pre % k + k) % k
+            key = (pre % k + k) % k 
             if key in d:
                 cnt += d[key]
             d[key] += 1
                 
-        return cnt      
+        return cnt 
             
