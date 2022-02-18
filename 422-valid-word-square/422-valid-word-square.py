197a0bd625778aeea36d2e2d02657599062cd101
Jing <jyang.ustc@gmail.com>
2022-02-18 09:41:30 -0600

    Time: 65 ms (80.99%), Space: 14.7 MB (91.11%) - LeetHub

--- 422-valid-word-square/422-valid-word-square.py
+++ 422-valid-word-square/422-valid-word-square.py
@@ -1,17 +1,28 @@
 class Solution:
-    def getColWord(self,i,words):
-        temp = ""
-        for word in words:
-            if i < len(word): temp+=word[i]
-        return temp
+#     def getColWord(self,i,words):
+#         temp = ""
+#         for word in words:
+#             if i < len(word): temp+=word[i]
+#         return temp
         
+#     def validWordSquare(self, words):
+#         """
+#         :type words: List[str]
+#         :rtype: bool
+#         """      
+#         for i,rowWord in enumerate(words):
+#             colWord = self.getColWord(i,words)
+#             if colWord != rowWord:
+#                 return False
+#         return True
+    
+    
     def validWordSquare(self, words):
-        """
-        :type words: List[str]
-        :rtype: bool
-        """      
-        for i,rowWord in enumerate(words):
-            colWord = self.getColWord(i,words)
-            if colWord != rowWord:
-                return False
-        return True
+        try:
+            for i in range(len(words)):
+                for j in range(len(words[i])):
+                    if words[i][j] != words[j][i]:
+                        return False
+            return True
+        except IndexError:
+            return False
