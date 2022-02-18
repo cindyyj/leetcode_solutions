b190df12c7fe712d15b724f7cc953ce2a12c7184
Jing <jyang.ustc@gmail.com>
2022-02-17 20:42:47 -0600

    Time: 840 ms (56.87%), Space: 20.4 MB (27.09%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,7 +1,22 @@
+from heapq import heappush, heappop 
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        points.sort(key= lambda x: x[0]**2 + x[1]**2)
-        return points[:k]
+        # minheap, find the closest to origin
+        queue = []
+        n = len(points)
+        for i in range(n):
+            heappush(queue, (points[i][0]**2 + points[i][1]**2, points[i]))
+        res = []
+        for i in range(k):
+            res.append(heappop(queue)[1])
+        return res
+
+        # 作者：jalan
+        # 链接：https://leetcode-cn.com/problems/k-closest-points-to-origin/solution/tou-lan-yu-kuai-pai-by-jalan/
+
+        # points.sort(key= lambda x: x[0]**2 + x[1]**2)
+        # return points[:k]
         
         
