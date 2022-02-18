12cafed80592e0937d0bc5f3bf4deaf6d03efcd4
Jing <jyang.ustc@gmail.com>
2022-02-17 21:44:40 -0600

    Time: 672 ms (84.57%), Space: 15.5 MB (24.29%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        n = len(grid)
+        ans = 0
+        
+        # edge cases
+        if grid[0][0] or grid[n-1][n-1]:
+            return -1
+        
+        dirs = [ (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        
+        queue = collections.deque([(0,0,1)]) # row, col, path steps (distance)
+        seen = set()
+        seen.add((0,0))
+        
+        while queue:
+            row, col, d = queue.popleft()
+            if (row, col) == (n-1, n-1):
+                return d
+            for dx, dy in dirs:
+                r, c = row + dx, col + dy
+                if 0 <= r < n and 0 <= c < n:
+                    if (r, c) not in seen and grid[r][c] == 0:
+                        seen.add((r,c))
+                        queue.append((r, c, d+1))
+        
+        return -1 
+        
+  
+        
+        """
+        You can recognize that this is a typical shortest path problem that can be solved with a Breadth-first search (BFS).
+        You can correctly implement a BFS to solve it.
+        For bonus points, you know that the solution could be optimized using the A* algorithm.
+        """
