77173a1f9b7d52deacd89151f36e6c49a8388586
Jing <jyang.ustc@gmail.com>
2022-02-19 09:22:52 -0600

    Time: 1421 ms (64.13%), Space: 13.9 MB (90.55%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.py
+++ 611-valid-triangle-number/611-valid-triangle-number.py
@@ -26,6 +26,12 @@ b+c>a
 
 均成立。如果我们将三条边进行升序排序，使它们满足 a≤b≤c，那么 a + c > b 和 b + c > a 使一定成立的，我们只需要保证 a + b > c
 
+方法三的精髓其实是排序后的倒序判断（由后往前扫）。
+固定一条最长边 c 之后，剩下的两边都是在比 c 小的数里找的。只要剩余的两边满足  a + b > c，则一定满足 a + c > b 和 b + c > a 
+
+双指针其实我是很快就想到了的。但是我是正向扫描一直做不对。楼主拓宽了我的对双指针的视野，谢谢！
+
+
 作者：LeetCode-Solution
 链接：https://leetcode-cn.com/problems/valid-triangle-number/solution/you-xiao-san-jiao-xing-de-ge-shu-by-leet-t2td/
 来源：力扣（LeetCode）
