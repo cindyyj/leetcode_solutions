328c2822826661545965cfed1f3133571cad4a71
Jing <jyang.ustc@gmail.com>
2022-03-05 09:29:43 -0600

    Time: 36 ms (67.85%), Space: 13.9 MB (48.47%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.py
+++ 266-palindrome-permutation/266-palindrome-permutation.py
@@ -1,14 +1,16 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
         
+        return sum(v % 2 for v in Counter(s).values()) < 2
+    
+    
+    
+# # --------------------------- METHOD 2 ---------------------------        
         
-        counts = collections.Counter(s)
-        odds = [cnt for cnt in counts.values() if cnt & 1]  # num & 1 same as num % 2 == 1
+#         counts = collections.Counter(s)
+#         odds = [cnt for cnt in counts.values() if cnt & 1]  # num & 1 same as num % 2 == 1
         
-        return len(odds) <= 1
-        
-        
-        
+#         return len(odds) <= 1
         
 # # --------------------------- METHOD 1 ---------------------------        
 #         counts = collections.Counter(s)
