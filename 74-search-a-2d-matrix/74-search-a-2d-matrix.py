cc358ce04df18c404021dba911b753d2c5ca070a
Jing <jyang.ustc@gmail.com>
2022-03-05 11:01:16 -0600

    Time: 48 ms (80.06%), Space: 14.4 MB (94.52%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,32 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        # Brute Force
-        # https://leetcode-cn.com/problems/search-a-2d-matrix/solution/fu-xue-ming-zhu-liu-chong-fang-fa-bang-n-e20z/
         
-        # return any([target in row for row in matrix])
+# --------------------------- METHOD 1, Brute Force, one liner!  ---------------------------
+        # https://leetcode-cn.com/problems/search-a-2d-matrix/solution/fu-xue-ming-zhu-liu-chong-fang-fa-bang-n-e20z/       
+        return any([target in row for row in matrix])
+
+# # --------------------------- METHOD 2, Binary Search  ---------------------------    
+#         # binary search!!!, time O(log(mn))
+#         m = len(matrix)
+#         n = len(matrix[0])
         
-        # binary search!!!, time O(log(mn))
-        m = len(matrix)
-        n = len(matrix[0])
+#         if not m or not n:
+#             return False
         
-        if not m or not n:
-            return False
+#         l, r = 0, m * n - 1
+#         while l <= r:
+#             mid = l + (r - l) // 2
+#             val = matrix[mid // n][mid % n]
+#             if val == target:
+#                 return True
+#             elif val < target:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
         
-        l, r = 0, m * n - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            val = matrix[mid // n][mid % n]
-            if val == target:
-                return True
-            elif val < target:
-                l = mid + 1
-            else:
-                r = mid - 1
-        
-        return False     
-        
-#         # time complexity, O(mn)
+#         return False     
+
+# --------------------------- METHOD 3, not recommend  ---------------------------        
+#         # time complexity, O(mn), didn't take advantage of the sorted property! do not recommend
 #         # search from upper right or bottom left
 #         rows = len(matrix)
 #         cols = len(matrix[0])
