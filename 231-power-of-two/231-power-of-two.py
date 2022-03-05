c862f1396f01207ed2e178f1e4ea7f8094ae5958
Jing <jyang.ustc@gmail.com>
2022-03-05 14:28:00 -0600

    Time: 34 ms (82.17%), Space: 13.8 MB (73.56%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -1,5 +1,12 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        
+        
+        
+        
+        # power of 2, just has one 1-bit
+        # n & (n-1) sets 1-bit to zero.  n - 1: change rightmost 1-bit to 0 and all lower bits to 1
+        # turn off rightmost 1-bit 
         if n == 0:
             return False
         return n & (n - 1) == 0
