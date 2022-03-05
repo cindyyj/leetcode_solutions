1621ec91d675c3e81c910d714814587221e37398
Jing <jyang.ustc@gmail.com>
2022-03-04 21:19:23 -0600

    Time: 40 ms (83.09%), Space: 14.1 MB (47.14%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -2,16 +2,35 @@ class Solution:
     def isMajorityElement(self, nums: List[int], target: int) -> bool:
         # return nums.count(target) > len(nums) / 2
         
-        """
-        bisect_left returns the largest index to insert the element w.r.t. <
-        bisect_right returns the largest index to insert the element w.r.t. <=
-        """
+#         """
+#         bisect_left returns the largest index to insert the element w.r.t. <
+#         bisect_right returns the largest index to insert the element w.r.t. <=
+#         """
+#         n = len(nums)
+#         if nums[n // 2] != target:
+#             return False
+        
+#         low = bisect.bisect_left(nums, target)
+#         high = bisect.bisect_right(nums, target)
+        
+#         return high - low > n / 2
+        
+        
+        # smart binary search!
         n = len(nums)
         if nums[n // 2] != target:
             return False
         
-        low = bisect.bisect_left(nums, target)
-        high = bisect.bisect_right(nums, target)
-        
-        return high - low > n / 2
-        
+        l = 0 
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1 
+            else:
+                r = mid - 1
+                
+        try:
+            return nums[l + n//2] == target
+        except IndexError:
+            return False
