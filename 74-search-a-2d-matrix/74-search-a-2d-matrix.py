a1c4e69b2bef7506952e80387f30b89339a57bcb
Jing <jyang.ustc@gmail.com>
2022-03-05 11:05:24 -0600

    Time: 44 ms (90.81%), Space: 14.4 MB (94.52%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -2,30 +2,27 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         
-# --------------------------- METHOD 1, Brute Force, one liner!  ---------------------------
-        # https://leetcode-cn.com/problems/search-a-2d-matrix/solution/fu-xue-ming-zhu-liu-chong-fang-fa-bang-n-e20z/       
-        return any([target in row for row in matrix])
+# # --------------------------- METHOD 1, Brute Force, one liner!  ---------------------------
+#         # https://leetcode-cn.com/problems/search-a-2d-matrix/solution/fu-xue-ming-zhu-liu-chong-fang-fa-bang-n-e20z/       
+#         return any([target in row for row in matrix])
 
-# # --------------------------- METHOD 2, Binary Search  ---------------------------    
-#         # binary search!!!, time O(log(mn))
-#         m = len(matrix)
-#         n = len(matrix[0])
+# --------------------------- METHOD 2, Binary Search  ---------------------------    
+        # binary search!!!, time O(log(mn))
+        m, n = len(matrix), len(matrix[0])
         
-#         if not m or not n:
-#             return False
+        l, r = 0, m * n - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            val = matrix[mid//n][mid%n]
+            if val == target:
+                return True
+            elif val < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         
-#         l, r = 0, m * n - 1
-#         while l <= r:
-#             mid = l + (r - l) // 2
-#             val = matrix[mid // n][mid % n]
-#             if val == target:
-#                 return True
-#             elif val < target:
-#                 l = mid + 1
-#             else:
-#                 r = mid - 1
-        
-#         return False     
+        return False
+    
 
 # --------------------------- METHOD 3, not recommend  ---------------------------        
 #         # time complexity, O(mn), didn't take advantage of the sorted property! do not recommend
@@ -46,4 +43,23 @@ class Solution:
                     
 #             else:
 #                 return False       
+
+
+#         m = len(matrix)
+#         n = len(matrix[0])
         
+#         if not m or not n:
+#             return False
+        
+#         l, r = 0, m * n - 1
+#         while l <= r:
+#             mid = l + (r - l) // 2
+#             val = matrix[mid // n][mid % n]
+#             if val == target:
+#                 return True
+#             elif val < target:
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+        
+#         return False 
