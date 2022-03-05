f9a09c3eb0ee3234dc03ca948c65a9f51dbbfde2
Jing <jyang.ustc@gmail.com>
2022-03-04 21:15:15 -0600

    Time: 58 ms (42.61%), Space: 13.9 MB (98.93%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -1,4 +1,17 @@
 class Solution:
     def isMajorityElement(self, nums: List[int], target: int) -> bool:
-        return nums.count(target) > len(nums) / 2
+        # return nums.count(target) > len(nums) / 2
+        
+        """
+        bisect_left returns the largest index to insert the element w.r.t. <
+        bisect_right returns the largest index to insert the element w.r.t. <=
+        """
+        n = len(nums)
+        if nums[n // 2] != target:
+            return False
+        
+        low = bisect.bisect_left(nums, target)
+        high = bisect.bisect_right(nums, target)
+        
+        return high - low > n / 2
         
