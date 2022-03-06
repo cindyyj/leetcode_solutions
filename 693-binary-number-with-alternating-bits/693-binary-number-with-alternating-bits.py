d0432a7666862a139a1e053426e9d9a2443fd9ca
Jing <jyang.ustc@gmail.com>
2022-03-06 17:59:12 -0600

    Time: 24 ms (98.71%), Space: 13.9 MB (76.09%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
@@ -1,5 +1,11 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        s = bin(n)
-        return ('00' not in s) and ('11' not in s)
+        # s = bin(n)
+        # return ('00' not in s) and ('11' not in s)
         
+        n, cur_digit = divmod(n, 2)
+        while n > 0:
+            if cur_digit == n % 2:
+                return False
+            n, cur_digit = divmod(n, 2)
+        return True
