03fd9ca57c7d6cf9c90b5d135c2e42b6cbc278da
Jing <jyang.ustc@gmail.com>
2022-03-06 14:19:43 -0600

    Time: 44 ms (47.97%), Space: 13.7 MB (97.86%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -5,10 +5,14 @@ class Solution:
         # create bitmask of 1111...1 of same length of binary num 
         # bin(1) ->0b1, len(bin(num)) - 2 is the same as bit length
         
-#         x = '1' * num.bit_length()
-#         # x = '1'*(len(bin(num)) - 2)
+        x = '1'*(len(bin(num)) - 2)  # x = '1' * num.bit_length()        
+        return int(x, 2) ^ num # much faster than int(x, 2) - num
         
-#         return int(x, 2) ^ num # much faster than int(x, 2) - num
-        # return int(x, 2) - num 
-        
-        return ((1 << num.bit_length()) - 1) ^ num
+        # return ((1 << num.bit_length()) - 1) ^ num
+    
+        """
+        def bit_length(self):
+            s = bin(self)       # binary representation:  bin(-37) --> '-0b100101'
+            s = s.lstrip('-0b') # remove leading zeros and minus sign
+            return len(s)       # len('100101') --> 6
+        """
