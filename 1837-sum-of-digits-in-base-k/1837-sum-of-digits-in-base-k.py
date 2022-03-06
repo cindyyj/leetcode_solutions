de52f5cd23d1fc61e416d647864bef75150c6cfc
Jing <jyang.ustc@gmail.com>
2022-03-06 16:53:52 -0600

    Time: 35 ms (69.73%), Space: 13.9 MB (78.35%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
@@ -1,5 +1,10 @@
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
+        
+        """
+        Time: O(logn), space: O(1).
+        Note: O(log(n base k)) = O(logn).
+        """
         total = 0
         while n > 0:
             total += n % k
