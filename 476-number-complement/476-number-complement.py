e2ace719f991bc719ed7b2db2ee1256b071f9e1f
Jing <jyang.ustc@gmail.com>
2022-03-06 14:11:47 -0600

    Time: 24 ms (98.65%), Space: 13.8 MB (72.20%) - LeetHub

---
+++ 476-number-complement/476-number-complement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        
+        # same as 1009
+        # create bitmask of 1111...1 of same length of binary num 
+        # bin(1) ->0b1, len(bin(num)) - 2 is the same as bit length
+        x = '1'*(len(bin(num)) - 2)
+        return int(x, 2) ^ num 
