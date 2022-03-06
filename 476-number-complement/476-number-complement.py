b6ecf1ee803019e4a5c23607a785b4833902f418
Jing <jyang.ustc@gmail.com>
2022-03-06 14:12:54 -0600

    Time: 29 ms (88.80%), Space: 13.9 MB (42.97%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -4,6 +4,8 @@ class Solution:
         # same as 1009
         # create bitmask of 1111...1 of same length of binary num 
         # bin(1) ->0b1, len(bin(num)) - 2 is the same as bit length
+        
         x = '1'*(len(bin(num)) - 2)
-        # return int(x, 2) ^ num 
-        return int(x, 2) - num 
+        return int(x, 2) ^ num # much faster than int(x, 2) - num
+    
+        # return int(x, 2) - num 
