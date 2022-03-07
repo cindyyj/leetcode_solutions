9de7437f223dd0dd8502710e55e50343a6fb4f68
Jing <jyang.ustc@gmail.com>
2022-03-06 19:18:25 -0600

    Time: 41 ms (71.19%), Space: 13.9 MB (87.08%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -10,12 +10,17 @@ class Solution:
         """
         n = len(matrix)
              
-        # reflect 水平翻转, 上下
-        for i in range(n // 2):
-            for j in range(n):
-                matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
+        # # flip 水平翻转, 上下
+        # for i in range(n // 2):
+        #     for j in range(n):
+        #         matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
    
         # transpose 主对角线翻转,  Reverse on Diagonal 
         for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # reflect 左右翻转
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
