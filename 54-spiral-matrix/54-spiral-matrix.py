671e1352051924527d91c074d4939c458843bb7c
Jing <jyang.ustc@gmail.com>
2022-03-06 21:53:00 -0600

    Time: 32 ms (86.68%), Space: 13.8 MB (99.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -8,36 +8,38 @@ class Solution:
         move left: (row, col - 1)
         move upwards: (row - 1, col)
         """
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+
         
-        m, n = len(matrix), len(matrix[0])
+#         m, n = len(matrix), len(matrix[0])
         
-        if not m or not n:
-            return list()
+#         if not m or not n:
+#             return list()
         
-        order = list()
-        left, right, top, bottom = 0, n - 1, 0, m - 1
-        while left <= right and top <= bottom:
-            for col in range(left, right + 1):
-                order.append(matrix[top][col])
-            top += 1
-            if top > bottom:
-                break
+#         order = list()
+#         left, right, top, bottom = 0, n - 1, 0, m - 1
+#         while left <= right and top <= bottom:
+#             for col in range(left, right + 1):
+#                 order.append(matrix[top][col])
+#             top += 1
+#             if top > bottom:
+#                 break
                 
-            for row in range(top, bottom + 1):
-                order.append(matrix[row][right])
-            right -= 1
-            if right < left:
-                break
+#             for row in range(top, bottom + 1):
+#                 order.append(matrix[row][right])
+#             right -= 1
+#             if right < left:
+#                 break
             
-            for col in range(right, left - 1, -1):
-                order.append(matrix[bottom][col])
-            bottom -= 1
-            if bottom < top:
-                break
+#             for col in range(right, left - 1, -1):
+#                 order.append(matrix[bottom][col])
+#             bottom -= 1
+#             if bottom < top:
+#                 break
             
-            for row in range(bottom, top - 1, -1):
-                order.append(matrix[row][left])
-            left += 1
-            if left > right: break
+#             for row in range(bottom, top - 1, -1):
+#                 order.append(matrix[row][left])
+#             left += 1
+#             if left > right: break
         
-        return order                  
+#         return order                  
