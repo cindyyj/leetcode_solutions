4df545b45072f4795981ff33180d2afbe9de7dad
Jing <jyang.ustc@gmail.com>
2022-03-06 18:01:16 -0600

    Time: 53 ms (21.59%), Space: 13.8 MB (97.43%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.py
@@ -3,9 +3,12 @@ class Solution:
         # s = bin(n)
         # return ('00' not in s) and ('11' not in s)
         
-        n, cur_digit = divmod(n, 2)
-        while n > 0:
-            if cur_digit == n % 2:
-                return False
-            n, cur_digit = divmod(n, 2)
-        return True
+        # n, cur_digit = divmod(n, 2)
+        # while n > 0:
+        #     if cur_digit == n % 2:
+        #         return False
+        #     n, cur_digit = divmod(n, 2)
+        # return True
+        
+        bits = bin(n)
+        return all( bits[i] != bits[i+1] for i in range(len(bits) - 1))
