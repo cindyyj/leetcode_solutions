22dbcc48bc1b8c1f232732e85239dfc7a453573f
Jing <jyang.ustc@gmail.com>
2022-03-08 18:30:24 -0600

    Time: 108 ms (65.92%), Space: 14.4 MB (45.49%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -1,4 +1,6 @@
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
         
-        return sorted(set(arr1) & set(arr2) & set(arr3))
+        # return sorted(set(arr1) & set(arr2) & set(arr3))
+    
+        return [k for k, v in Counter(arr1 + arr2 + arr3).items() if v == 3]
