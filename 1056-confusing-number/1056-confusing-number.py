af13b360a0390684867f4bc13784e70e23b55fd6
Jing <jyang.ustc@gmail.com>
2022-03-09 14:36:52 -0600

    Time: 32 ms (83.14%), Space: 13.8 MB (82.90%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -1,12 +1,26 @@
 class Solution:
     def confusingNumber(self, n: int) -> bool:
-        rotation = {"0": 0, "1": 1, "6": 9, "8": 8, "9": 6}
-        num = 0
-
-        for i, ch in enumerate(str(n)):
-            if ch not in rotation:
+        
+        d = {0:0, 1:1, 6:9, 9:6, 8:8}
+        nums = list(map(int, str(n)))
+        new_n = list()
+        
+        for num in nums:
+            if num not in d:
                 return False
             else:
-                num += rotation[ch] * 10 ** i
+                new_n.append(d[num])
+        
+        return new_n[::-1] != nums
+        
+        
+#         rotation = {"0": 0, "1": 1, "6": 9, "8": 8, "9": 6}
+#         num = 0
 
-        return num != n
+#         for i, ch in enumerate(str(n)):
+#             if ch not in rotation:
+#                 return False
+#             else:
+#                 num += rotation[ch] * 10 ** i
+
+#         return num != n
