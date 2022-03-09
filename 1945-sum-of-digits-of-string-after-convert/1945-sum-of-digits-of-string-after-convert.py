389626c6b2bb364f70befbfc1e7a28e580fcabe3
Jing <jyang.ustc@gmail.com>
2022-03-08 20:12:57 -0600

    Time: 40 ms (77.54%), Space: 13.8 MB (81.01%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -1,6 +1,17 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         
+        """
+        immutable does not mean you can't reassign the variable, it means you cannot edit its content, by which I mean the below is ok :
+
+        s = 'bla'
+        s= 'blb'
+        but this is not ok:
+
+        s = 'bla'
+        s[2] ='b'
+        """
+        
         nums = "".join([str(ord(ch) - ord('a') + 1) for ch in s])
         while k:
             nums = str(sum(list(map(int, nums))))            
