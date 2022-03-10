0c11cf34d69803eebd502dd1144c6b0be3b14e63
Jing <jyang.ustc@gmail.com>
2022-03-10 13:51:53 -0600

    Time: 46 ms (42.30%), Space: 14 MB (31.20%) - LeetHub

--- 836-rectangle-overlap/836-rectangle-overlap.py
+++ 836-rectangle-overlap/836-rectangle-overlap.py
@@ -3,8 +3,9 @@ class Solution:
         
         # https://leetcode-cn.com/problems/rectangle-overlap/solution/tu-jie-jiang-ju-xing-zhong-die-wen-ti-zhuan-hua-we/
         # change 2d overlap to 1d interval overlap
-        # only not overlap if 
+        # only not overlap if x_overlap and y_overlap
         # 假设两个区间分别是 [s1, e1] 和 [s2, e2] 的话，区间不重叠的两种情况就是 e1 <= s2 和 e2 <= s1。
+        # 分离轴定理 二维情况是最简单的
         
         x_overlap = not(rec1[2] <= rec2[0] or rec2[2] <= rec1[0])
         y_overlap = not(rec1[3] <= rec2[1] or rec2[3] <= rec1[1])
