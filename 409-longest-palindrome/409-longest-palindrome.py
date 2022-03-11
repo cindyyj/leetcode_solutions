801b35512cdb89e2652d0d6a9cff4f8639cb497e
Jing <jyang.ustc@gmail.com>
2022-03-10 20:30:32 -0600

    Time: 69 ms (8.19%), Space: 13.9 MB (79.04%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        
+        # https://leetcode.com/problems/longest-palindrome/solution/
+        """
+        对于每个字符 ch，假设它出现了 v 次，我们可以使用该字符 v / 2 * 2 次，在回文串的左侧和右侧分别放置 v / 2 个字符 ch，其中 / 为整数除法。例如若 "a" 出现了 5 次，那么我们可以使用 "a" 的次数为 4，回文串的左右两侧分别放置 2 个 "a"。
+
+如果有任何一个字符 ch 的出现次数 v 为奇数（即 v % 2 == 1），那么可以将这个字符作为回文中心，注意只能最多有一个字符作为回文中心。在代码中，我们用 ans 存储回文串的长度，由于在遍历字符时，ans 每次会增加 v / 2 * 2，因此 ans 一直为偶数。但在发现了第一个出现次数为奇数的字符后，我们将 ans 增加 1，这样 ans 变为奇数，在后面发现其它出现奇数次的字符时，我们就不改变 ans 的值了。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/longest-palindrome/solution/zui-chang-hui-wen-chuan-by-leetcode-solution/
+        """
+        
+        ans = 0
+        cnt =  collections.Counter(s)
+        for v in cnt.values():
+            ans += v // 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+                
+        return ans            
