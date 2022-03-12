86af1180f12f379836bb6df85124ae1e14b72442
Jing <jyang.ustc@gmail.com>
2022-03-12 12:47:36 -0600

    Time: 65 ms (67.15%), Space: 13.9 MB (99.21%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,13 +1,25 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
         
-        for i in arr:
-            if i <= k:
-                k += 1
-        return k
+        l, r = 0, len(arr) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            missing = arr[mid] - (mid + 1) # arr[i] should have i + 1 if no missing value
+            
+            # find lower bound (left boundary)
+            if missing < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l + k
 
-# --------------------------- METHOD 2, binary search ---------------------------        
-
+# # --------------------------- METHOD 2  ---------------------------        
+#         # 缺失的正整数一定 >= k
+#         # 数组中每出现一个 <= k 的数字, 意味着少了一个缺失的数字, 此时k+1        
+#         for i in arr:
+#             if i <= k:
+#                 k += 1
+#         return k
     
 # # --------------------------- METHOD 1 set ---------------------------
 #         # use set to check if it contains a number (much faster!)
