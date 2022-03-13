139a7e294a55040e84dfee8ad438bc42e8cda222
Jing <jyang.ustc@gmail.com>
2022-03-12 20:21:32 -0600

    Time: 36 ms (98.20%), Space: 14 MB (52.25%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.py
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -1,16 +1,37 @@
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        d = defaultdict(int)
-        for born, died in logs:
-            for year in range(born, died):
-                d[year] += 1
         
-        max_alive = 0
-        res = 0
+        # https://leetcode-cn.com/problems/maximum-population-year/solution/ren-kou-zui-duo-de-nian-fen-by-leetcode-5m7r4/
+        # 「差分」+「前缀和」
+        delta = defaultdict(int)
+        for born, died in logs:
+            delta[born] += 1
+            delta[died] -= 1
+        
+        maxp = 0    # 人口数量最大值
+        res = 0     # 最大值对应的最小下标
+        curr = 0    # 每一年的人口数量
+        
+        # 前缀和
         for year in range(1950, 2051):
-            alive = d[year]
-            if alive > max_alive:
-                res = year
-                max_alive = alive
-            
+            curr += delta[year]
+            if curr > maxp:
+                maxp = curr
+                res = year        
         return res
+        
+
+#         d = defaultdict(int)
+#         for born, died in logs:
+#             for year in range(born, died):
+#                 d[year] += 1
+        
+#         max_alive = 0
+#         res = 0
+#         for year in range(1950, 2051):
+#             alive = d[year]
+#             if alive > max_alive:
+#                 res = year
+#                 max_alive = alive
+            
+#         return res
