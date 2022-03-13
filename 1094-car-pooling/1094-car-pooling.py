df091f3d39b64c2c248c7374d5515e4adb229328
Jing <jyang.ustc@gmail.com>
2022-03-12 21:43:14 -0600

    Time: 90 ms (70.19%), Space: 14.4 MB (63.24%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -7,6 +7,26 @@ class Solution:
         # 1109. Corporate Flight Bookings
         # 218. The Skyline Problem
         # https://leetcode.com/problems/car-pooling/discuss/867628/3-different-approaches-in-Python.
+
+        # Meeting Rooms II (Sorting all intervals)4
+        lst = []
+        for n, start, end in trips:
+            lst.append((start, n))
+            lst.append((end, -n))
+            
+        # The sort() method is used to sort the elements in ascending order and by default
+        # for list of tuples, it will sort the first element. 
+        # If two tuples have the same first element then it will sort them according to the second element.
+        lst.sort()
+        pas = 0
+        for loc in lst:
+            pas += loc[1]
+            if pas > capacity:
+                return False
+        return True
+    
+#         stops = sorted([k for n, a, b in trips for k in ((a, n), (b, -n))])
+#         return not any(n > capacity for n in accumulate(map(lambda x: x[1], stops)))        
         
 #         # bucket sort
 #         people = [0] * 1001
@@ -20,22 +40,22 @@ class Solution:
 #         return True
 
         
-        # 前缀和 prefix sum
-        d = defaultdict(int)
+#         # 前缀和 prefix sum
+#         d = defaultdict(int)
         
-        for num, start, end in trips:
-            d[start] += num
-            d[end] -= num
+#         for num, start, end in trips:
+#             d[start] += num
+#             d[end] -= num
         
-        maxp = 0
-        total = 0
+#         maxp = 0
+#         total = 0
         
-        #  sorted(d) -> sort dict by key, defaultdict do not have attribute sort! 
-        for pos in sorted(d):
-            total += d[pos]
-            d[pos] = total
-            maxp = max(d[pos], maxp)
-            if maxp > capacity:
-                return False
+#         #  sorted(d) -> sort dict by key, defaultdict do not have attribute sort! 
+#         for pos in sorted(d):
+#             total += d[pos]
+#             d[pos] = total
+#             maxp = max(d[pos], maxp)
+#             if maxp > capacity:
+#                 return False
         
-        return True # maxp <= capacity
+#         return True # maxp <= capacity
