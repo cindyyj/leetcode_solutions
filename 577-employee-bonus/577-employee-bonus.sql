55d8af6b14f3a6dfd2727d47891c3a9408674e90
Jing <jyang.ustc@gmail.com>
2022-03-12 19:50:46 -0600

    Time: 825 ms (61.81%), Space: 0B (100.00%) - LeetHub

--- 577-employee-bonus/577-employee-bonus.sql
+++ 577-employee-bonus/577-employee-bonus.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+# "Select all employee's name and bonus whose bonus is < 1000. 
+# (People have no bonus records are also needed to be considered)"
 
 select name, bonus
 from Employee left join Bonus 
