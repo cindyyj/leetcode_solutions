b6ce232ca902c4740c9c79b6a7f8ac7ab1f3972d
Jing <jyang.ustc@gmail.com>
2022-03-12 21:46:54 -0600

    Time: 72 ms (93.24%), Space: 14.5 MB (63.24%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -7,29 +7,33 @@ class Solution:
         # 1109. Corporate Flight Bookings
         # 218. The Skyline Problem
         # https://leetcode.com/problems/car-pooling/discuss/867628/3-different-approaches-in-Python.
+         
 
-        # Meeting Rooms II (Sorting all intervals)4
-        lst = []
-        for n, start, end in trips:
-            lst.append((start, n))
-            lst.append((end, -n))
+# # --------------------------- METHOD 1 ---------------------------        
+#         # Meeting Rooms II (Sorting all intervals)
+#         lst = []
+#         for n, start, end in trips:
+#             lst.append((start, n))
+#             lst.append((end, -n))
             
-        # The sort() method is used to sort the elements in ascending order and by default
-        # for list of tuples, it will sort the first element. 
-        # If two tuples have the same first element then it will sort them according to the second element.
-        lst.sort()
-        return not any(n > capacity for n in accumulate(map(lambda x:x[1], lst)))
+#         # The sort() method is used to sort the elements in ascending order and by default
+#         # for list of tuples, it will sort the first element. 
+#         # If two tuples have the same first element then it will sort them according to the second element.
+#         lst.sort()
+#         return not any(n > capacity for n in accumulate(map(lambda x:x[1], lst)))
         
-        # pas = 0
-        # for loc in lst:
-        #     pas += loc[1]
-        #     if pas > capacity:
-        #         return False
-        # return True
+#         # pas = 0
+#         # for loc in lst:
+#         #     pas += loc[1]
+#         #     if pas > capacity:
+#         #         return False
+#         # return True
     
-#         stops = sorted([k for n, a, b in trips for k in ((a, n), (b, -n))])
-#         return not any(n > capacity for n in accumulate(map(lambda x: x[1], stops)))        
+# #         stops = sorted([k for n, a, b in trips for k in ((a, n), (b, -n))])
+# #         return not any(n > capacity for n in accumulate(map(lambda x: x[1], stops)))        
         
+
+# --------------------------- METHOD 2 ---------------------------            
 #         # bucket sort
 #         people = [0] * 1001
 #         for n, a, b in trips:
@@ -41,7 +45,7 @@ class Solution:
 #                 return False
 #         return True
 
-        
+# --------------------------- METHOD 3 ---------------------------                    
 #         # 前缀和 prefix sum
 #         d = defaultdict(int)
         
@@ -61,3 +65,22 @@ class Solution:
 #                 return False
         
 #         return True # maxp <= capacity
+
+
+        # heap
+        # sort by start
+        trips.sort(key=lambda x: x[1])
+        dest = []
+        cur_cap = 0
+        
+        for n, a, b in trips:
+            while dest and dest[0][0] <= a:
+                cur_cap -= heappop(dest)[1]
+                
+            heappush(dest, (b, n))
+            cur_cap += n
+            if cur_cap > capacity:
+                return False
+            
+        return True        
+        
