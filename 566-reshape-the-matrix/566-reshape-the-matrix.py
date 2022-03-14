875dab555432e496fd4a0e0f2e0eae4dc178b104
Jing <jyang.ustc@gmail.com>
2022-03-13 20:06:51 -0500

    Time: 390 ms (5.01%), Space: 32.6 MB (5.15%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -3,6 +3,7 @@ import numpy as np
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         
+        # https://leetcode.com/problems/reshape-the-matrix/discuss/1317320/Python-One-Line-Two-Line-Yield-Generator-Circular-Index-Numpy-with-explaination(556)
         # numpy
         try:
             return np.array(mat).reshape(r, c)
