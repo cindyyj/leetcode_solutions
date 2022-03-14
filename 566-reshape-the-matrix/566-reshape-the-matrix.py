045ee8d002ac8397e9696b2736e3c2357f470c1c
Jing <jyang.ustc@gmail.com>
2022-03-13 20:01:45 -0500

    Time: 152 ms (29.36%), Space: 32.8 MB (5.15%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -3,16 +3,24 @@ import numpy as np
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         
-        m, n = len(mat), len(mat[0])
-        
-        # invalid
-        if r * c != m * n: 
+        # numpy
+        try:
+            return np.array(mat).reshape(r, c)
+        except:
             return mat
         
-        new = [[0] * c for _ in range(r)]
-        for i in range(m*n):
-            new[i // c][i % c] = mat[i // n][i % n]        
-        return new
+        
+#         # matrix[index / width][index % width] for both the input and the output matrix.
+#         m, n = len(mat), len(mat[0])
+        
+#         # invalid
+#         if r * c != m * n: 
+#             return mat
+        
+#         new = [[0] * c for _ in range(r)]
+#         for i in range(m*n):
+#             new[i // c][i % c] = mat[i // n][i % n]        
+#         return new
             
         
         # return np.array(mat).reshape(r, c)
