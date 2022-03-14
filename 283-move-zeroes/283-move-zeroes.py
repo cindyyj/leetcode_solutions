afdbb54b76b2731193a2e4589325cadcdd82b178
Jing <jyang.ustc@gmail.com>
2022-03-13 20:43:56 -0500

    Time: 291 ms (33.15%), Space: 15.5 MB (80.65%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -2,21 +2,18 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        # // If the current element is not 0, then we need to
+        # // append it just in front of last non 0 element we found. 
+        # // After we have finished processing new elements,
+        # // all the non-zero elements are already at beginning of array.
+        # // We just need to fill remaining array with 0's.                  
         """
         # S: O(1), T: O(n)  
         
-        slow = fast = 0
-    # // If the current element is not 0, then we need to
-    # // append it just in front of last non 0 element we found. 
-    
+        slow = 0
         for fast in range(len(nums)):
             if nums[fast] != 0:
                 nums[slow] = nums[fast]
                 slow += 1
-                
- 	# // After we have finished processing new elements,
- 	# // all the non-zero elements are already at beginning of array.
- 	# // We just need to fill remaining array with 0's.        
-    
         for i in range(slow, len(nums)):
             nums[i] = 0
