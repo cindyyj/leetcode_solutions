7d5132bbafc366153d5e41e5bf677d3d869e3002
Jing <jyang.ustc@gmail.com>
2022-03-13 20:17:03 -0500

    Time: 312 ms (5.01%), Space: 32.5 MB (5.15%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -4,11 +4,15 @@ class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
         
         # https://leetcode.com/problems/reshape-the-matrix/discuss/1317320/Python-One-Line-Two-Line-Yield-Generator-Circular-Index-Numpy-with-explaination(556)
-        # numpy
-        try:
-            return np.array(mat).reshape(r, c)
-        except:
-            return mat
+        # generator expression (also called a generator comprehension), which has a very similar syntax to list comprehensions
+        gen = (val for row in mat for val in row)
+        return [[next(gen) for _ in range(c)] for _ in range(r)] if len(mat) * len(mat[0]) == r * c else mat        
+    
+        # # numpy
+        # try:
+        #     return np.array(mat).reshape(r, c)
+        # except:
+        #     return mat
         
         
 #         # matrix[index / width][index % width] for both the input and the output matrix.
