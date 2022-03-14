c92bccde80a39b6375dd1e9845a5016d7dae56f2
Jing <jyang.ustc@gmail.com>
2022-03-14 18:14:20 -0500

    Time: 699 ms (68.64%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -3,5 +3,4 @@
 # https://www.mysqltutorial.org/mysql-delete-duplicate-rows/
 DELETE t1 FROM Person t1
 INNER JOIN Person t2 
-WHERE 
-    t1.id > t2.id AND t1.email = t2.email    
+WHERE t1.id > t2.id AND t1.email = t2.email    
