334ff20815584830d7b1805cddd08e46a55d07cc
Jing <jyang.ustc@gmail.com>
2022-03-13 20:41:37 -0500

    Time: 753 ms (74.48%), Space: 16.7 MB (85.30%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -6,6 +6,8 @@ class Solution:
         # 2D dp, 2 pass: 
         # update based on its left and top neighbors;
         # then update based on its right and bottom neighbors.
+        # 首先从左上角开始递推 是由其 「左方」和 「左上方」的最优子状态决定的；
+        # 然后从右下角开始递推 是由其 「右方」和 「右下方」的最优子状态决定的；
 
         m, n = len(mat), len(mat[0])
 
