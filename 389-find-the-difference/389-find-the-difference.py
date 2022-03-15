fe243a4c079a1af9f7509105a95d7ea112eccd27
Jing <jyang.ustc@gmail.com>
2022-03-15 16:05:39 -0500

    Time: 36 ms (87.71%), Space: 14 MB (37.04%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,5 +1,18 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        for char in t:
-            if s.count(char) != t.count(char):
-                return char
+        
+        # bit manipulation, 0 ^ x = x (xor)
+        ch = 0 
+        for c in s:
+            ch ^= ord(c)
+        for c in t:
+            ch ^= ord(c)
+        return chr(ch)
+        
+        
+        
+        # return (Counter(t) - Counter(s)).keys()
+        
+        # for char in t:
+        #     if s.count(char) != t.count(char):
+        #         return char
