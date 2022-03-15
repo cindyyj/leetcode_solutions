f1784b9d8f5053b13f45735f90f718a6355f2dfe
Jing <jyang.ustc@gmail.com>
2022-03-15 16:14:07 -0500

    Time: 49 ms (34.45%), Space: 13.9 MB (66.09%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,4 +1,16 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace("()", "o").replace("(al)", "al")
+        
+        replace_map = {
+            '()':'o',
+            '(al)':'al'
+        }
+        
+        def replace_with_map(command: str, pattern: str) -> str:
+            return command.replace(pattern, replace_map[pattern])
+        
+        return reduce(replace_with_map, replace_map, command)
+        
+        
+        # return command.replace("()", "o").replace("(al)", "al")
         
