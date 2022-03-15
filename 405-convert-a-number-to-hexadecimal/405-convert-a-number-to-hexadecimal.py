7081dfc4d6a9c27519bc66fe052656bfa642d912
Jing <jyang.ustc@gmail.com>
2022-03-14 20:51:40 -0500

    Time: 48 ms (39.73%), Space: 13.9 MB (36.09%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
@@ -6,8 +6,10 @@ class Solution:
         # two’s complement
         if num < 0:
             num += 2**32
+            
+        # no need to reverse, otherwise, res += s[num % 16], return res[::-1]            
         while num:
-            res = s[num % 16] + res
+            res = s[num % 16] + res 
             num >>= 4
         
         return res or '0'
