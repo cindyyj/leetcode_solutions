9e79d47ba9f0298c318fb5483fa91f49e0a6a678
Jing <jyang.ustc@gmail.com>
2022-03-14 19:53:28 -0500

    Time: 412 ms (16.81%), Space: 14.5 MB (88.95%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,11 +1,13 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         
-        total_len = 0
-        cnt = Counter(chars)
+        return sum(len(w) for w in words if not Counter(w) - Counter(chars))
         
-        for w in words:
-            if Counter(w) <= cnt:
-                total_len += len(w)
+#         total_len = 0
+#         cnt = Counter(chars)
         
-        return total_len
+#         for w in words:
+#             if Counter(w) <= cnt:
+#                 total_len += len(w)
+        
+#         return total_len
