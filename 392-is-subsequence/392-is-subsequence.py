d4f901c1f7d8079cdd0f5f74bb32d65cd7ff4d5c
Jing <jyang.ustc@gmail.com>
2022-03-14 21:13:57 -0500

    Time: 38 ms (72.14%), Space: 13.9 MB (86.83%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n, m = len(s), len(t)
+        if n > m:
+            return False
+        
+        i, j = 0, 0
+        while i < n and j < m:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == n
+        
+""" 
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/is-subsequence/solution/pan-duan-zi-xu-lie-by-leetcode-solution/
+"""
