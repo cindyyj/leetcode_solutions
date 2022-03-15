a27731bebd77725cd94373e52eaa0ee37e579b06
Jing <jyang.ustc@gmail.com>
2022-03-15 15:55:19 -0500

    Time: 175 ms (5.02%), Space: 14.3 MB (60.19%) - LeetHub

--- 1086-high-five/1086-high-five.py
+++ 1086-high-five/1086-high-five.py
@@ -1,12 +1,25 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        scores = defaultdict(list)
         
-        for i, score in items:
-            scores[i].append(score)
+        # bisect.insort(arr, x, lo=0, hi=len(a), *, key=None)
+        # Similar to insort_left(), but inserting x in arr after any existing entries of x.
+        # The insort() functions are O(n) because the logarithmic search step is dominated by the linear time insertion step!!!
         
-        ans = []
-        for i in sorted(scores):
-            ans.append([i, sum( sorted(scores[i], reverse=True)[:5] ) // 5])
+        d = collections.defaultdict(list)
+        for student, score in items:
+            bisect.insort(d[student], score)
+        return [[i, sum(sorted(d[i])[-5:]) // 5] for i in sorted(d)]        
         
-        return ans
+        
+        
+        
+#         scores = defaultdict(list)
+        
+#         for i, score in items:
+#             scores[i].append(score)
+        
+#         ans = []
+#         for i in sorted(scores):
+#             ans.append([i, sum( sorted(scores[i], reverse=True)[:5] ) // 5])
+        
+#         return ans
