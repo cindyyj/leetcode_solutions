5d696fba2864c9c55f5817c2f0941fe3009cf86e
Jing <jyang.ustc@gmail.com>
2022-03-15 18:52:34 -0500

    Time: 476 ms (52.45%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -6,6 +6,15 @@
 # order by count(*) desc
 # limit 1
 
+
+# generic version 
+# Follow up: What if more than one customer have the largest number of orders, can you find all the customer_number in this case?
+# Do not use triangle join, because it is bad scalibility and performance
+
+# Step:
+# Find the number of the largest count on order number
+# Select the customer who has same number of order as the largest count on order number
+
 select customer_number
 from orders
 group by customer_number
