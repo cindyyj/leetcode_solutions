93879030f932efec962a07ffece319df238a2864
Jing <jyang.ustc@gmail.com>
2022-03-14 20:10:38 -0500

    Time: 24 ms (99.11%), Space: 13.9 MB (75.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,5 +1,15 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        """
+        index, out of bounds --> index error
+        string/list slicing, out of bounds --> empty results
+        
+        The slicing operation doesn't raise an error if both your start and stop indices are larger than the sequence length. 
+        This is in contrast to simple indexing—if you index an element that is out of bounds, Python will throw an index out of bounds error. 
+        However, with slicing it simply returns an empty sequence.
+        """
+        
         n = min(len(word1), len(word2))
         res =""
         for i in range(n):
