5901f0d9ff04dc21a19388a0070588b477c45ebc
Jing <jyang.ustc@gmail.com>
2022-03-14 21:09:47 -0500

    Time: 44 ms (56.40%), Space: 13.8 MB (97.61%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,14 +1,28 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        week, day = 0, 1
-        res = 0
-        for i in range(n):
-            res += week + day
-            day += 1
-            if day == 8:
-                day = 1
-                week += 1
-        return res
+        # 等差数列求和 arithmetic sequence
+        # 所有完整的周存的钱
+        weekNumber = n // 7
+        firstWeekMoney = (1 + 7) * 7 // 2
+        lastWeekMoney = firstWeekMoney + 7 * (weekNumber - 1)
+        weekMoney = (firstWeekMoney + lastWeekMoney) * weekNumber // 2
+        # 剩下的不能构成一个完整的周的天数里存的钱
+        dayNumber = n % 7
+        firstDayMoney = 1 + weekNumber
+        lastDayMoney = firstDayMoney + dayNumber - 1
+        dayMoney = (firstDayMoney + lastDayMoney) * dayNumber // 2
+        return weekMoney + dayMoney  
+        
+        # # simulation
+        # week, day = 0, 1
+        # res = 0
+        # for i in range(n):
+        #     res += week + day
+        #     day += 1
+        #     if day == 8:
+        #         day = 1
+        #         week += 1
+        # return res
 
 """
 作者：LeetCode-Solution
