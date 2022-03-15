a25c50db4c9ceae28a53f86e58a4426285cc1ea3
Jing <jyang.ustc@gmail.com>
2022-03-15 16:09:23 -0500

    Time: 36 ms (87.71%), Space: 13.7 MB (98.69%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,17 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        # bit manipulation, 0 ^ x = x (xor)
-        ch = 0 
-        for c in s:
-            ch ^= ord(c)
-        for c in t:
-            ch ^= ord(c)
-        return chr(ch)
+        return list((Counter(t) - Counter(s)).keys())[0]
         
+        # # bit manipulation, 0 ^ x = x (xor)
+        # ch = 0 
+        # for c in s:
+        #     ch ^= ord(c) # ord is ASCII value, an integer representing the Unicode character.
+        # for c in t:
+        #     ch ^= ord(c)
+        # return chr(ch) #chr = convert ASCII into character
         
-        
-        # return (Counter(t) - Counter(s)).keys()
         
         # for char in t:
         #     if s.count(char) != t.count(char):
