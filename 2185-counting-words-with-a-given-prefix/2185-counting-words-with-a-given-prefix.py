715dbe2d6f934ad8dbbdd79b2a0d0754803117d1
Jing <jyang.ustc@gmail.com>
2022-03-15 14:59:30 -0500

    Time: 50 ms (71.37%), Space: 13.9 MB (73.70%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -1,7 +1,11 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
         
-        return sum(w.find(pref) == 0 for w in words)
+        #  follow-up question from Google: what if the words are sorted lexicographically?
+        
+        return sum(w.startswith(pref) for w in words)
+        
+        # return sum(w.find(pref) == 0 for w in words)
         
 #         cnt = 0
 #         for w in words:
