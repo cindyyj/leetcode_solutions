c46f73dbe6eb22025c197896486bcf0590ae429f
Jing <jyang.ustc@gmail.com>
2022-03-15 16:32:25 -0500

    Time: 60 ms (95.23%), Space: 26.4 MB (57.35%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -3,6 +3,10 @@ class Solution:
         
         # Linear-time Slice Using Substring + HashSet
         # much fast look up in set vs. in list
+        # time & space, O((n-l)*l), n the loop executed N - L + 1 one builds a substring of length L. 
+        
+        # rabin-karp for multiple pattern search
+        # Constant-time Slice Using Rolling Hash
         l, n = 10, len(s)
         seen, ans = set(), set()
         
