6fa42c4c976ef8176ea1c762784cd46d237947d5
Jing <jyang.ustc@gmail.com>
2022-03-16 16:23:08 -0500

    Time: 60 ms (10.87%), Space: 13.9 MB (28.56%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -1,7 +1,9 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
+        return "".join([chr(ord(c) + ord("a") - ord("A")) if "A" <= c <= "Z" else c for c in s])
+
         
-        d = { chr(ord("A") + i) : chr(ord("a") + i) for i in range(26)}
-        return "".join([d[c] if c in d else c for c in s])
+        # d = { chr(ord("A") + i) : chr(ord("a") + i) for i in range(26)}
+        # return "".join([d[c] if c in d else c for c in s])
         
         # return s.lower()
