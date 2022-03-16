c1674c8349706a0027ee62a865b6f2c41cfa75f4
Jing <jyang.ustc@gmail.com>
2022-03-15 22:45:57 -0500

    Time: 52 ms (89.94%), Space: 13.8 MB (75.80%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,11 +1,19 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        # In Python, the shortcut row[~i] = row[-i-1] = row[len(row) - 1 - i]
+        # ~, not, Inverts all the bits
+        # helps us find the i-th value of the row, counting from the right.
         
-        # reverse & flip
-        res = []
+#         # reverse & flip
+#         res = []
+#         for row in image:
+#             row = row[::-1]
+#             res.append(list(map(lambda x : 0 if x == 1 else 1, row)))
+        
+#         return res
+        
+        # in-place change!
         for row in image:
-            row = row[::-1]
-            res.append(list(map(lambda x : 0 if x == 1 else 1, row)))
-        
-        return res
-        
+            for i in range((len(row) + 1) // 2):
+                row[len(row) - 1 - i], row[i] = row[i]^1, row[len(row) - 1 - i] ^ 1        
+        return image
