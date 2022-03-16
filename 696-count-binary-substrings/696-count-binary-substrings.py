dc27db19bb0f6a50cadcfaeb83e022d3e6c60a63
Jing <jyang.ustc@gmail.com>
2022-03-16 11:50:23 -0500

    Time: 393 ms (13.86%), Space: 14.6 MB (21.80%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -1,23 +1,13 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        ans, prev, cur = 0, 0, 1
+
+        groups = [1]
         for i in range(1, len(s)):
-            if s[i-1] != s[i]:
-                ans += min(prev, cur)
-                prev, cur = cur, 1
+            if s[i - 1] != s[i]:
+                groups.append(1)
             else:
-                cur += 1
-        return ans + min(prev, cur)
-        
-        
-        
-        # groups = [1]
-        # for i in range(1, len(s)):
-        #     if s[i - 1] != s[i]:
-        #         groups.append(1)
-        #     else:
-        #         groups[-1] += 1
-        # return sum(min(a, b) for a, b in zip(groups, groups[1:]))
+                groups[-1] += 1
+        return sum(min(a, b) for a, b in zip(groups, groups[1:]))
         
         
         # # itertools.groupby
