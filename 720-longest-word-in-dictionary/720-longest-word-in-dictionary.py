214921c6645f10a62337a73a7609beffae082e14
Jing <jyang.ustc@gmail.com>
2022-03-16 17:30:39 -0500

    Time: 92 ms (89.69%), Space: 14.6 MB (60.61%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -1,5 +1,22 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
+        # hashmap
+        
+        words.sort(key=lambda w: (-len(w), w), reverse = True)
+        longest = ""
+        seen = set([""])
+        
+        for word in words:
+            if word[:-1] in seen:
+                longest = word
+                seen.add(word)
+        return longest
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode-cn.com/problems/longest-word-in-dictionary/solution/ci-dian-zhong-zui-chang-de-dan-ci-by-lee-k5gj/
+
+        
+        # trie! (to do!) 字典树 「208. 实现 Trie (前缀树) 的官方题解」，
         words.sort(key = lambda word : (-len(word), word))
         
         for word in words:
