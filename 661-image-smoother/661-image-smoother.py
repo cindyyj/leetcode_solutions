ff8cd94fc285ddd35b72eea56bbd5b79380413bd
Jing <jyang.ustc@gmail.com>
2022-03-15 23:01:45 -0500

    Time: 696 ms (79.06%), Space: 14.7 MB (49.51%) - LeetHub

--- 661-image-smoother/661-image-smoother.py
+++ 661-image-smoother/661-image-smoother.py
@@ -7,12 +7,12 @@ class Solution:
         
         for i in range(m*n):            
             r, c = i//n, i%n
-            total = cnt = 0
+            cnt = 0
             for nr in (r-1, r, r+1):
                 for nc in (c-1, c, c+1):
                     if 0<= nr < m and 0 <= nc < n:
-                        total += img[nr][nc]
+                        new[r][c] += img[nr][nc]
                         cnt += 1
-            new[r][c] = total // cnt         
+            new[r][c] //= cnt         
             
         return new        
