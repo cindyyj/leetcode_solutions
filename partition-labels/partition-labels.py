31b9b9b8b098cc9f5a7aa56ddd0a21f6f739f943
Jing <jyang.ustc@gmail.com>
2022-03-15 22:23:28 -0500

    Time: 34 ms, Memory: 14 MB - LeetHub

---
+++ partition-labels/partition-labels.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # Figure out the rightmost index first and use it to denote the start of the next section.
+        # Reset the left pointer at the start of each new section.
+        # Store the difference of right and left pointers + 1 as in the result for each section.
+
+        last = {char : i for i, char in enumerate(s)} # last[char] -> index of S where char occurs last
+        	
+        left, right = 0, 0
+        result = []
+        
+        for i, char in enumerate(s):
+            right = max(right, last[char])
+            
+            if i == right:
+                result.append(right - left + 1)
+                left = i + 1
+        
+        return result
