178fcc1a080f98d17323736a5aa72397cbc85de1
Jing <jyang.ustc@gmail.com>
2022-03-17 16:45:40 -0500

    Time: 60 ms (69.63%), Space: 13.8 MB (98.59%) - LeetHub

--- 728-self-dividing-numbers/728-self-dividing-numbers.py
+++ 728-self-dividing-numbers/728-self-dividing-numbers.py
@@ -1,6 +1,9 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         
+        # https://leetcode.com/problems/self-dividing-numbers/discuss/162578/One-line-Python-(Learn-some-Python-tricks-that-you-might-not-know)
+        # to do: many optimization tricks to read
+        
         def selfdivide(num):           
             digits = list(map(int, str(num)))
             for d in digits:
