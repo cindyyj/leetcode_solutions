0846195e0d840a8f42de2de8d7a4225eddf1e976
Jing <jyang.ustc@gmail.com>
2022-03-17 16:02:52 -0500

    Time: 56 ms (83.67%), Space: 14 MB (60.17%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -4,28 +4,28 @@ class Solution:
         q = collections.deque()
         
         fresh = 0
+        time = 0
         m, n = len(grid), len(grid[0])
         dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 2:
-                    q.append((r, c, 0))
+                    q.append((r, c, time))
                 elif grid[r][c] == 1:
                     fresh += 1
         
-        mins = 0
         while q:
-            i, j, mins = q.popleft()
+            i, j, time = q.popleft()
             for di, dj in dirs:
                 if 0 <= i + di < m and 0 <= j+dj < n and grid[i+di][j+dj] == 1:
                     grid[i+di][j+dj] = 2
-                    q.append((i+di, j+dj, mins + 1))
+                    q.append((i+di, j+dj, time + 1))
                     
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 1:  
                     return -1
         
-        return mins
+        return time
             
