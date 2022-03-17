f9113d441a110122ef839db459f18d46b5ef7b59
Jing <jyang.ustc@gmail.com>
2022-03-17 15:11:14 -0500

    Time: 41 ms (64.27%), Space: 13.8 MB (76.32%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,12 +1,26 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         
-        # brute force
-        for x in range(1, n//2 + 1):
+        f1, f2 = [], []
+        for x in range(1, int(n**0.5) + 1):
             if n % x == 0:
-                k -= 1
-                if k == 0:
-                    return x
+                f1.append(x)
+                f2.append(n // x)
+            
+        # in case n is perfect square
+        if f1[-1] == f2[-1]:
+            f2.pop()
         
-        return n if k == 1 else -1
+        factors = f1 + f2[::-1]
+        return -1 if len(factors) < k else factors[k-1]
+        
+        
+#         # brute force
+#         for x in range(1, n//2 + 1):
+#             if n % x == 0:
+#                 k -= 1
+#                 if k == 0:
+#                     return x
+        
+#         return n if k == 1 else -1
         
