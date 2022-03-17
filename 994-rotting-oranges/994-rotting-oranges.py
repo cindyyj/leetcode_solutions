8fcc6216b646d39d9f16ecc1e45149bacb321369
Jing <jyang.ustc@gmail.com>
2022-03-17 16:19:07 -0500

    Time: 73 ms (54.47%), Space: 14 MB (60.17%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,31 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
+      
+        m, n = len(grid), len(grid[0])
         q = collections.deque()
         
-        fresh = 0
-        time = 0
-        m, n = len(grid), len(grid[0])
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 2:
-                    q.append((r, c, time))
-                elif grid[r][c] == 1:
-                    fresh += 1
-        
-        while q:
-            i, j, time = q.popleft()
-            for di, dj in dirs:
-                if 0 <= i + di < m and 0 <= j+dj < n and grid[i+di][j+dj] == 1:
-                    grid[i+di][j+dj] = 2
-                    q.append((i+di, j+dj, time + 1))
+        t = 0
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if val == 2:
+                    q.append((r, c, t))
                     
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 1:  
-                    return -1
-        
-        return time
-            
+        def move(i, j):
+            dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for di, dj in dirs:
+                if 0 <= i + di < m and 0 <= j + dj < n:
+                    yield i + di, j + dj
+                    
+        while q:
+            r, c, t = q.popleft()
+            for nr, nc in move(r, c):
+                if grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    q.append((nr, nc, t + 1))
+                    
+        if any(1 in row for row in grid):
+            return -1
+        return t
