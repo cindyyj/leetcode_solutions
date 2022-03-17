07a62452c4359e5cc2a4c75f519d36be67416b72
Jing <jyang.ustc@gmail.com>
2022-03-17 13:34:18 -0500

    Time: 349 ms (69.34%), Space: 15.1 MB (60.12%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
@@ -1,10 +1,10 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        for i in range(len(num))[::-1]:
-            k, num[i] = divmod(num[i] + k, 10)
-        return [int(i) for i in str(k)] + num if k else num
+        # for i in range(len(num))[::-1]:
+        #     k, num[i] = divmod(num[i] + k, 10)
+        # return [int(i) for i in str(k)] + num if k else num
         
         
-        # ans = int("".join(map(str, num))) + k
-        # return list(map(int, str(ans)))
+        ans = int("".join(map(str, num))) + k
+        return list(map(int, str(ans)))
         
