8d232ec3888bfdf7ab0f747ddde65700164aaa71
Jing <jyang.ustc@gmail.com>
2022-03-18 10:38:31 -0500

    Time: 117 ms (30.67%), Space: 14.5 MB (32.24%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        op_to_binary_fn = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": lambda x, y: int(x / y),   # 需要注意 python 中负数除法的表现与题目不一致
+        }
+
+        stack = list()
+        for token in tokens:
+            try:
+                num = int(token)
+            except ValueError:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                num = op_to_binary_fn[token](num1, num2)
+            finally:
+                stack.append(num)
+            
+        return stack[0]
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/solution/ni-bo-lan-biao-da-shi-qiu-zhi-by-leetcod-wue9/
