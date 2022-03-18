fbe89729b2e8b5ed21b409583a12dd08ab93bb7b
Jing <jyang.ustc@gmail.com>
2022-03-18 10:51:03 -0500

    Time: 105 ms (42.70%), Space: 14.3 MB (67.85%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,26 @@
+import operator as op
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        op_to_binary_fn = {
+        operators = {
             "+": add,
             "-": sub,
             "*": mul,
+            # `operator.truediv a/b` & `operator.floordiv a//b` are not correct
+            # -3 // 2 = -2
             "/": lambda x, y: int(x / y),   # 需要注意 python 中负数除法的表现与题目不一致
         }
 
         stack = list()
         for token in tokens:
-            try:
-                num = int(token)
-            except ValueError:
+            if token in operators:
                 num2 = stack.pop()
                 num1 = stack.pop()
-                num = op_to_binary_fn[token](num1, num2)
-            finally:
-                stack.append(num)
-            
+                res = operators[token](num1, num2)     
+                stack.append(int(res))
+            else:
+                stack.append(int(token))
+                
         return stack[0]
 
 # 作者：LeetCode-Solution
