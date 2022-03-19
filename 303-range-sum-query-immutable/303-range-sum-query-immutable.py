d35e68a6874c2f57a0759c76cc8a70802f6a5122
Jing <jyang.ustc@gmail.com>
2022-03-19 11:29:39 -0500

    Time: 191 ms (39.41%), Space: 17.7 MB (36.40%) - LeetHub

---
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.py
@@ -0,0 +1,17 @@
+from itertools import accumulate 
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums     
+        self.pres = list(accumulate(nums))
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.pres[right]
+        else:
+            return self.pres[right] - self.pres[left - 1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
