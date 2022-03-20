f72529f2a57ed81f91599c8ee354775603118db8
Jing <jyang.ustc@gmail.com>
2022-03-20 11:38:33 -0500

    Time: 33 ms (92.61%), Space: 14.1 MB (65.85%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,6 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        
+        # KMP for string pattern searching
         if not needle:
             return 0
         return haystack.find(needle)
-        
