2b272ebd13567139e3eb2c8ac826559b9125965a
Jing <jyang.ustc@gmail.com>
2022-03-20 19:59:07 -0500

    Time: 59 ms (29.38%), Space: 14 MB (38.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -2,5 +2,6 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         # To have GCD substring, the length of it has to be a GCD of len(s1) and len(s2)
         # Check if s1 + s2 == s2 + s1 to make sure such a string exists
+        # Unfortunately in Python slicing a list is linear time instead of constant time, O(n1+n2):
         
         return str1[:math.gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
