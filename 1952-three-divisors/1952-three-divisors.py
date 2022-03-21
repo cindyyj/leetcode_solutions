d42c8c9fe77308fa18d33a2d9150fcb5b966ff97
Jing <jyang.ustc@gmail.com>
2022-03-20 19:53:36 -0500

    Time: 72 ms (12.82%), Space: 13.9 MB (26.86%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -1,8 +1,15 @@
 class Solution:
     def isThree(self, n: int) -> bool:
         # 穷举: 三除数的必须条件是质数的平方。并且N<10000,穷举100以内的质数的平方即可。
-        if n == 1: # edge case
+        
+        primes = set([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
+        sqrt = math.sqrt(n)
+        if sqrt in primes:
+            return True
+        else:
             return False
-
-        sqrt = int(n**.5)
-        return sqrt*sqrt == n and all(n%a for a in range(2, sqrt))
+        
+        # if n == 1: # edge case
+        #     return False
+        # sqrt = int(n**.5)
+        # return sqrt*sqrt == n and all(n%a for a in range(2, sqrt))
