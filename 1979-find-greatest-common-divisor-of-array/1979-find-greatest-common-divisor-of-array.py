0b579d4421f54c476bfadecc827af21d54e7ed2e
Jing <jyang.ustc@gmail.com>
2022-03-20 19:20:36 -0500

    Time: 95 ms (29.19%), Space: 14.1 MB (21.31%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -1,7 +1,20 @@
 import math
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        return math.gcd(min(nums), max(nums))
+        
+        # gcd Euclidean algorithm (辗转相除法)
+        # 对于两个非负整数 x, y , (x > y), gcd(x, y) = gcd(y, x mod y)
+        
+        def gcd(x, y):
+            if y == 0:
+                return x
+            return gcd(y, x % y)
+        
+        return gcd(max(nums), min(nums))
+        
+        
+        
+        # return math.gcd(min(nums), max(nums))
         
         
         
