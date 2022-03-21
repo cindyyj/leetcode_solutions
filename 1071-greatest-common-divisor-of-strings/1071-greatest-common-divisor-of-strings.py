8a6c4c13cb498bd6996adbfcab5ec821fe45b5af
Jing <jyang.ustc@gmail.com>
2022-03-20 20:01:27 -0500

    Time: 33 ms (84.44%), Space: 13.9 MB (38.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -4,4 +4,13 @@ class Solution:
         # Check if s1 + s2 == s2 + s1 to make sure such a string exists
         # Unfortunately in Python slicing a list is linear time instead of constant time, O(n1+n2):
         
-        return str1[:math.gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
+        # return str1[:math.gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
+        
+        n1 = len(str1)
+        n2 = len(str2)        
+        # check if both strings contain same repeating vals
+        if str1+str2 != str2+str1:
+            return ""
+        if str1 == str2:
+            return str1
+        return str1[:gcd( n1, n2 )]
