d27ea6307c9798e2e85031f65668b2cc9f1af13b
Jing <jyang.ustc@gmail.com>
2022-03-22 20:42:31 -0500

    Time: 78 ms (48.96%), Space: 14.5 MB (86.45%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -2,6 +2,16 @@ from collections import defaultdict
 from itertools import combinations
 # r-length tuples, in sorted order, no repeated elements
 # All the combination of list in sorted order(without replacement) 
+# .sort() method is only for lists and sorts by mutating the list in
+# place; invoked using dot notation
+# • sorted() function can be used to sort any sequence (strings, lists,
+# tuples). It always returns a new sorted list, and does NOT modify
+# the original sequence
+# Sorting a list of (or a tuple of) tuples with sorted() sorts elements in
+# ascending order by their first item
+# • If there is a tie, Python breaks the tie by comparing the second items
+# • If the second items are also tied, it compares the third items, and so on
+
 
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
@@ -15,7 +25,10 @@ class Solution:
             combs = set(combinations(sites, 3))
             for comb in combs:
                 cnt[comb] += 1
-        
-        ans = sorted(cnt, key=lambda x : (-cnt[x], x))
-        return ans[0]
+
+        return list(min(cnt, key=lambda k: (-cnt[k], k))) 
+    
+        # ans = sorted(cnt, key=lambda x : (-cnt[x], x))  # sort descending by value, then lexicographically
+        # return ans[0]
+    
         # return max(sorted(cnt), key=cnt.get)
