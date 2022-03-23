f6d62ac61d7a16e33173f46cd8179f13ed6a435a
Jing <jyang.ustc@gmail.com>
2022-03-22 20:29:55 -0500

    Time: 52 ms (91.16%), Space: 14.5 MB (69.40%) - LeetHub

---
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+from itertools import combinations
+# r-length tuples, in sorted order, no repeated elements
+# All the combination of list in sorted order(without replacement) 
+
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        
+        d = defaultdict(list)
+        for t, u, w in sorted(zip(timestamp, username, website)):
+            d[u].append(w)
+        
+        cnt = Counter()
+        for sites in d.values():
+            combs = set(combinations(sites, 3))
+            for comb in combs:
+                cnt[comb] += 1
+        
+        return max(sorted(cnt), key=cnt.get)
