870794e8247d51b470fb51d2bfdeb1b8ffdaeaaf
Jing <jyang.ustc@gmail.com>
2022-03-22 20:51:22 -0500

    Time: 55 ms (59.25%), Space: 14 MB (73.41%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.py
@@ -2,6 +2,9 @@ class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
         
         #  follow-up question from Google: what if the words are sorted lexicographically?
+        # You can use binary search twice to locate the lower and upper bounds of the words that have the same prefix.
+        # Therefore, the time cost is still O(klogn).
+        # Assume perf = "abcd", we can search "abcd" and "abcda" respectively.
         
         return sum(w.startswith(pref) for w in words)
         
