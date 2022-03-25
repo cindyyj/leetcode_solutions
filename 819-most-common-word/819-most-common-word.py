99992f277ef9739c4cca8e0276b64c114256d4d9
Jing <jyang.ustc@gmail.com>
2022-03-24 19:09:38 -0500

    Time: 60 ms (35.84%), Space: 13.9 MB (86.35%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -6,16 +6,21 @@ class Solution:
         # This solution doesn't work in the case of contracted words. 
         # So this solution would fail the test case "Don't tell me you don't want to." It would return "don" instead of "don't".
         
-        banned = set(banned)       
-        text = paragraph.lower()
-        t = re.sub(r"[!?',;.]+", " ", text)
+        ban = set(banned)
+        words = re.findall(r'\w+', paragraph.lower())
+        return collections.Counter(w for w in words if w not in ban).most_common(1)[0][0]
         
-        # most_common() return list of tuples [(word1, cnt), (word2, cnt)]
-        cnt = Counter(t.split()).most_common()
         
-        for word, _ in cnt:
-            if word not in banned:
-                return word
+#         banned = set(banned)       
+#         text = paragraph.lower()
+#         t = re.sub(r"[!?',;.]+", " ", text)
+        
+#         # most_common() return list of tuples [(word1, cnt), (word2, cnt)]
+#         cnt = Counter(t.split()).most_common()
+        
+#         for word, _ in cnt:
+#             if word not in banned:
+#                 return word
        
         
         
