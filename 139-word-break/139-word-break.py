ebfa773a4a0a068fcdbd148e9b5293db3f55282a
Jing <jyang.ustc@gmail.com>
2022-03-24 19:38:42 -0500

    Time: 60 ms (44.74%), Space: 13.9 MB (96.37%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,6 +2,10 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
         # https://leetcode.com/problems/concatenated-words/discuss/836924/Python-Template-Word-Break-I-Word-Break-II-Concatenated-Words
+        """
+        Time complexity : O(n^3) There are two nested loops, and substring computation at each iteration. Overall that results in O(n^3) time complexity.
+        Space complexity : O(n). Length of p array is n+1.
+        """
         
         dp = [False] * (len(s) + 1)
         dp[0] = True
