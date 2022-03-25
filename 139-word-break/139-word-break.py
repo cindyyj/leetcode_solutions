e4ca5d63fe0d1583ac5c0a727f91a4e7d870acbf
Jing <jyang.ustc@gmail.com>
2022-03-24 19:34:54 -0500

    Time: 57 ms (50.80%), Space: 14 MB (76.19%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,6 +1,8 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
+        # https://leetcode.com/problems/concatenated-words/discuss/836924/Python-Template-Word-Break-I-Word-Break-II-Concatenated-Words
+        
         dp = [False] * (len(s) + 1)
         dp[0] = True
         wordDict = set(wordDict)
@@ -9,4 +11,5 @@ class Solution:
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
+                    break
         return dp[-1]        
