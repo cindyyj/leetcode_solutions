72b25741053c0a5ffa2db3d2be6e8f1a69bbdf2e
Jing <jyang.ustc@gmail.com>
2022-03-25 09:45:37 -0500

    Time: 59 ms (15.01%), Space: 14 MB (41.35%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -33,27 +33,51 @@ class Solution:
         """
         # return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
+        
         if not matrix or not matrix[0]:
             return list()
         
         rows, columns = len(matrix), len(matrix[0])
-        order = list()
-        left, right, top, bottom = 0, columns - 1, 0, rows - 1
-        while left <= right and top <= bottom:
-            for column in range(left, right + 1):
-                order.append(matrix[top][column])
-            for row in range(top + 1, bottom + 1):
-                order.append(matrix[row][right])
-            if left < right and top < bottom:
-                for column in range(right - 1, left, -1):
-                    order.append(matrix[bottom][column])
-                for row in range(bottom, top, -1):
-                    order.append(matrix[row][left])
-            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
-        return order
+        visited = [[False] * columns for _ in range(rows)]
+        total = rows * columns
+        order = [0] * total
 
-# 作者：LeetCode-Solution
-# 链接：https://leetcode-cn.com/problems/spiral-matrix/solution/luo-xuan-ju-zhen-by-leetcode-solution/
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        row, column = 0, 0
+        directionIndex = 0
+        for i in range(total):
+            order[i] = matrix[row][column]
+            visited[row][column] = True
+            nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
+            if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+                directionIndex = (directionIndex + 1) % 4
+            row += directions[directionIndex][0]
+            column += directions[directionIndex][1]
+        return order
+      
+        
+#         # simulation
+#         if not matrix or not matrix[0]:
+#             return list()
+        
+#         rows, columns = len(matrix), len(matrix[0])
+#         order = list()
+#         left, right, top, bottom = 0, columns - 1, 0, rows - 1
+#         while left <= right and top <= bottom:
+#             for column in range(left, right + 1):
+#                 order.append(matrix[top][column])
+#             for row in range(top + 1, bottom + 1):
+#                 order.append(matrix[row][right])
+#             if left < right and top < bottom:
+#                 for column in range(right - 1, left, -1):
+#                     order.append(matrix[bottom][column])
+#                 for row in range(bottom, top, -1):
+#                     order.append(matrix[row][left])
+#             left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+#         return order
+
+# # 作者：LeetCode-Solution
+# # 链接：https://leetcode-cn.com/problems/spiral-matrix/solution/luo-xuan-ju-zhen-by-leetcode-solution/
 
         
 #         m, n = len(matrix), len(matrix[0])
