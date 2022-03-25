b02ab68ee484af8ad20c87c8fe55926c38412855
Jing <jyang.ustc@gmail.com>
2022-03-24 19:08:37 -0500

    Time: 73 ms (15.09%), Space: 13.9 MB (44.46%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -1,18 +1,35 @@
+import re 
+from collections import Counter
+
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         # This solution doesn't work in the case of contracted words. 
         # So this solution would fail the test case "Don't tell me you don't want to." It would return "don" instead of "don't".
-        text = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
         
-        cnt = Counter()
+        banned = set(banned)       
+        text = paragraph.lower()
+        t = re.sub(r"[!?',;.]+", " ", text)
         
-        banned = set(banned)        
-        for w in text.split():
-            if w not in banned:
-                cnt[w] += 1
+        # most_common() return list of tuples [(word1, cnt), (word2, cnt)]
+        cnt = Counter(t.split()).most_common()
         
-        # sort dict are actually sorting its key!
-        sorted_cnt = sorted(cnt, key=lambda x: -cnt[x])
-        return sorted_cnt[0]
+        for word, _ in cnt:
+            if word not in banned:
+                return word
+       
+        
+        
+#         text = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+        
+#         cnt = Counter()
+        
+#         banned = set(banned)        
+#         for w in text.split():
+#             if w not in banned:
+#                 cnt[w] += 1
+        
+#         # sort dict are actually sorting its key!
+#         sorted_cnt = sorted(cnt, key=lambda x: -cnt[x])
+#         return sorted_cnt[0]
                 
         
