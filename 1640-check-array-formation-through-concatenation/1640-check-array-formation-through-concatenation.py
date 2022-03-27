2f5e62092549cafe72d6cc29808ed7c8c1abea0e
Jing <jyang.ustc@gmail.com>
2022-03-27 16:28:56 -0500

    Time: 83 ms (15.14%), Space: 13.8 MB (81.20%) - LeetHub

---
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        d = defaultdict(list)
+        for piece in pieces:
+            d[piece[0]] = piece
+        # d = {piece[0] : piece for piece in pieces}
+        
+        target = []
+        for num in arr:
+            target += d[num]
+        return target == arr
