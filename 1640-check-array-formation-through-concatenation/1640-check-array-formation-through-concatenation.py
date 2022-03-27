96fed179380b3bc59dc2578f59323ec5bb84149d
Jing <jyang.ustc@gmail.com>
2022-03-27 16:29:25 -0500

    Time: 69 ms (35.94%), Space: 13.9 MB (38.27%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,11 +1,12 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        d = defaultdict(list)
-        for piece in pieces:
-            d[piece[0]] = piece
-        # d = {piece[0] : piece for piece in pieces}
+        # d = defaultdict(list)
+        # for piece in pieces:
+        #     d[piece[0]] = piece
+        d = {piece[0] : piece for piece in pieces}
         
         target = []
         for num in arr:
-            target += d[num]
+            # target += d[num]
+            target += d.get(num, [])
         return target == arr
