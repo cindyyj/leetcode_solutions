72dab5cb231c96bef1241c057d5983383565d94b
Jing <jyang.ustc@gmail.com>
2022-03-27 16:21:07 -0500

    Time: 62 ms (72.35%), Space: 14 MB (72.75%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -1,7 +1,19 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
         # two sum with strings
-        return sum(i + j == target for i, j in permutations(nums, 2))
-
+        # brute force
+        # return sum(i + j == target for i, j in permutations(nums, 2))
         # return sum(''.join(num_lst) == target for num_lst in permutations(nums, 2))
         
+        
+        # optimized with freq table
+        freq = collections.Counter(nums)
+        res = 0
+        for k, v in freq.items():
+            if target.startswith(k):
+                suffix = target[len(k):]
+                res += v * freq[suffix]
+                
+                if k == suffix:
+                    res -= freq[suffix]
+        return res
