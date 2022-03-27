e4fa889e7844c569803dcd04db4643430f2555e7
Jing <jyang.ustc@gmail.com>
2022-03-27 11:48:09 -0500

    Time: 525 ms (39.68%), Space: 33.6 MB (38.97%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -2,4 +2,5 @@ class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         
         d = {num : i + 1 for i, num in enumerate(sorted(set(arr)))}
-        return list(map(d.get, arr))
+        return [d[num] for num in arr]
+        # return list(map(d.get, arr))
