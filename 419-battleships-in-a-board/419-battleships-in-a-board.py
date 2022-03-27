dc3c4cb1ed3cca16aecd16156136dbab3d8869c4
Jing <jyang.ustc@gmail.com>
2022-03-27 16:13:05 -0500

    Time: 128 ms (26.49%), Space: 14.5 MB (70.08%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -1,14 +1,25 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         
-        cnt = 0 
+#         cnt = 0 
         
-        for i, row in enumerate(board):
-            for j, val in enumerate(row):
-                if val == "X" and not ((i > 0 and board[i - 1][j] == 'X') or (j > 0 and board[i][j - 1] == 'X')):
-                    cnt += 1
-        return cnt
-        
+#         for i, row in enumerate(board):
+#             for j, val in enumerate(row):
+#                 if val == "X" and not ((i > 0 and board[i - 1][j] == 'X') or (j > 0 and board[i][j - 1] == 'X')):
+#                     cnt += 1
+#         return cnt
+
+    
+        if len(board) == 0: return 0
+        m, n = len(board), len(board[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X' and (i == 0 or board[i-1][j] == '.') and (j == 0 or board[i][j-1] == '.'):
+                    count += 1
+        return count
+    
+    
         """
         假设矩阵的行数为 \textit{row}row，矩阵的列数 \textit{col}col，矩阵中的位置 (i, j)(i,j) 为战舰的左上顶点，需满足以下条件：
 
