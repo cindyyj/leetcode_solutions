5dc553c91367ff904532351e5068e67ddc902df7
Jing <jyang.ustc@gmail.com>
2022-03-27 10:53:15 -0500

    Time: 205 ms, Memory: 0B - LeetHub

---
+++ second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+
+# common top n, nth problem
+# https://leetcode.com/problems/second-highest-salary/discuss/1168444/Summary-Five-ways-to-solve-the-top-n-nth-problems
+# sql series!
+# 177, 185, 184, 1194(hard, to do), 1341, 586, 1549, 1532, 1369, 1097(hard, to do), 1077, 1070, 602, 1082, 1112, 619, 574 (most voted)
+
+# window function!
+# common table expression (CTE)
+# CTE is a temporary named result set that you can reference within a SELECT, INSERT, UPDATE, or DELETE statement
+
+with CTE as (
+    select salary, rank() over (order by salary desc) as rank_desc
+    from Employee
+    )
+
+select max(salary) as SecondHighestSalary 
+from cte 
+where rank_desc = 2
