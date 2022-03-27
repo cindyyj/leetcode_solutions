31ee88bb15ab188fab5c28c2347c67c28adf7ae6
Jing <jyang.ustc@gmail.com>
2022-03-27 11:30:57 -0500

    Time: 47 ms (42.02%), Space: 13.7 MB (97.15%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,5 +1,14 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
+        
+        # Ignore digits and signs,
+        # only count the current open parentheses cur.
+        # The depth equals to the maximum open parentheses.
+        # Complexity
+        # Time O(N)
+        # Space O(1)
+        # can simplify as it's known to be valid parentheses strings
+        
         res = cur = 0
         for c in s:
             if c == '(':
