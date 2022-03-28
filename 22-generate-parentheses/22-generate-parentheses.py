dc25b9d968a048aa5cad851dcd909d7a18d42c22
Jing <jyang.ustc@gmail.com>
2022-03-27 19:36:52 -0500

    Time: 48 ms (59.45%), Space: 14.3 MB (44.26%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,27 +1,44 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        cur_str = ''
         
-        def dfs (cur_str, left, right):
-            """
-            :param cur_str: 从根结点到叶子结点的路径字符串
-            :param left: 左括号还可以使用的个数
-            :param right: 右括号还可以使用的个数
-            :return:
-            """
-            if left == right == 0:
-                res.append(cur_str)
+        ans = []
+        cur = ""
+        
+        def backtrack(cur, left, right):
+            if len(cur) == 2*n:
+                ans.append(cur)
                 return 
+            if left < n:
+                backtrack(cur + "(", left + 1, right) 
             if right < left:
-                return
-            if left > 0:
-                dfs(cur_str + "(", left - 1, right)
-            if right > 0:
-                dfs(cur_str + ")", left, right - 1)
+                backtrack(cur +")", left, right + 1)
+        
+        backtrack(cur, 0, 0)
+        return ans
+    
+        
+#         res = []
+#         cur_str = ''
+        
+#         def dfs (cur_str, left, right):
+#             """
+#             :param cur_str: 从根结点到叶子结点的路径字符串
+#             :param left: 左括号还可以使用的个数
+#             :param right: 右括号还可以使用的个数
+#             :return:
+#             """
+#             if left == right == 0:
+#                 res.append(cur_str)
+#                 return 
+#             if right < left:
+#                 return
+#             if left > 0:
+#                 dfs(cur_str + "(", left - 1, right)
+#             if right > 0:
+#                 dfs(cur_str + ")", left, right - 1)
                 
-        dfs(cur_str, n, n)
-        return res
+#         dfs(cur_str, n, n)
+#         return res
             
 
 # 作者：liweiwei1419
