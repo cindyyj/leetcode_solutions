9bfd5298e6bca357a951e82abb9e8bd12fbc04ad
Jing <jyang.ustc@gmail.com>
2022-03-29 19:56:12 -0500

    Time: 32 ms (87.40%), Space: 13.8 MB (85.08%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -31,29 +31,29 @@ class Solution:
         = [1, 2, 3, 6, 9, 8, 7, 4, 5]
         
         """
-        # return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
         
-        if not matrix or not matrix[0]:
-            return list()
+#         if not matrix or not matrix[0]:
+#             return list()
         
-        rows, columns = len(matrix), len(matrix[0])
-        visited = [[False] * columns for _ in range(rows)]
-        total = rows * columns
-        order = [0] * total
+#         rows, columns = len(matrix), len(matrix[0])
+#         visited = [[False] * columns for _ in range(rows)]
+#         total = rows * columns
+#         order = [0] * total
 
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        row, column = 0, 0
-        directionIndex = 0
-        for i in range(total):
-            order[i] = matrix[row][column]
-            visited[row][column] = True
-            nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
-            if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
-                directionIndex = (directionIndex + 1) % 4
-            row += directions[directionIndex][0]
-            column += directions[directionIndex][1]
-        return order
+#         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+#         row, column = 0, 0
+#         directionIndex = 0
+#         for i in range(total):
+#             order[i] = matrix[row][column]
+#             visited[row][column] = True
+#             nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
+#             if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+#                 directionIndex = (directionIndex + 1) % 4
+#             row += directions[directionIndex][0]
+#             column += directions[directionIndex][1]
+#         return order
       
         
 #         # simulation
