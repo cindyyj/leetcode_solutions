4c33a776381794eec629fd69d2c87710bae5b6ee
Jing <jyang.ustc@gmail.com>
2022-03-30 12:12:04 -0500

    Time: 45 ms (88.11%), Space: 16.4 MB (11.83%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -17,39 +17,39 @@ class Solution:
             r = dfs(node.right)
             d = max(d, l + r)
             
-            # return max(l, r) + 1
+            return max(l, r) + 1
         
         dfs(root)
         return d
         
         
-    # 错误解答： 最大直径是左子树和右子树的最大深度之和，
-    # 但是万一最大直径没有经过根节点呢？
-    # 所以说对于树中的每一个结点，都要把它视为根节点，然后比较所有结点的左子树和右子树的最大深度之和，取其中的最大值。
+#     # 错误解答： 最大直径是左子树和右子树的最大深度之和，
+#     # 但是万一最大直径没有经过根节点呢？
+#     # 所以说对于树中的每一个结点，都要把它视为根节点，然后比较所有结点的左子树和右子树的最大深度之和，取其中的最大值。
     
-    # classic!!!
+#     # classic!!!
     
-    # https://leetcode-cn.com/problems/diameter-of-binary-tree/solution/hot-100-9er-cha-shu-de-zhi-jing-python3-di-gui-ye-/
+#     # https://leetcode-cn.com/problems/diameter-of-binary-tree/solution/hot-100-9er-cha-shu-de-zhi-jing-python3-di-gui-ye-/
 
-    def __init__(self):
-        self.diameter = 0
+#     def __init__(self):
+#         self.diameter = 0
         
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.depth(root)
-        return self.diameter
+#     def diameterOfBinaryTree(self, root: TreeNode) -> int:
+#         self.depth(root)
+#         return self.diameter
 
         
-    def depth(self, root):
+#     def depth(self, root):
         
-        if not root:
-            return 0
+#         if not root:
+#             return 0
         
-        l = self.depth(root.left)
-        r = self.depth(root.right)
-        self.diameter = max(self.diameter, l+r)
+#         l = self.depth(root.left)
+#         r = self.depth(root.right)
+#         self.diameter = max(self.diameter, l+r)
 
-        '''每个结点都要去判断左子树+右子树的高度是否大于self.max，更新最大值'''
+#         '''每个结点都要去判断左子树+右子树的高度是否大于self.max，更新最大值'''
         
-        # 返回的是高度
-        return max(l,r) + 1
+#         # 返回的是高度
+#         return max(l,r) + 1
   
